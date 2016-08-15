417d90a668b3aa560b41e0e72fbf7245b833f70c
Rick Bergfalk <rick.bergfalk@gmail.com>
2016-08-15 16:48:51 -0500

    update tags route to use api

--- routes/tags.js
+++ routes/tags.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 var router = require('express').Router();
 var Query = require('../models/Query.js');
 
-router.get('/tags', function (req, res) {
+router.get('/api/tags', function (req, res) {
     Query.findAll(function (err, queries) {
         var tags = _.uniq(_.flatten(_.map(queries, 'tags'))).sort();
         res.json(tags);
