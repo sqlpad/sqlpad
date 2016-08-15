c53195d331b06b720469310dec38bc87f932a57b
Rick Bergfalk <rick.bergfalk@gmail.com>
2016-08-15 16:31:26 -0500

    make add/update queries api routes

--- routes/queries.js
+++ routes/queries.js
@@ -10,27 +10,24 @@ var Query = require('../models/Query.js');
 var noop = function () {};
 const BASE_URL = config.get('baseUrl');
 
-function getQueryFilterData(req, res, next) {
-    Connection.findAll(function (err, connections) {
-        var connectionsById = _.keyBy(connections, '_id');
-        Query.findAll(function (err, queries) {
-            var tags = _.uniq(_.flatten(_.map(queries, 'tags'))).sort();
-            var createdBys = _.uniq(_.map(queries, 'createdBy')).sort();
-            connections = _.sortBy(connections, 'name');
-            res.locals.connectionsById = connectionsById;
-            res.locals.connections = connections;
-            res.locals.createdBys = createdBys;
-            res.locals.tags = tags;
-            next();
-        });
-    });
-}
 
-router.get('/react/queries', function (req, res) {
-    res.render('react-queries', {
+/*  react-applet get routes
+============================================================================= */
+router.get('/queries', function (req, res) {
+    return res.render('react-applet', {
         pageTitle: "Queries"
     });
-})
+});
+
+router.get('/queries/:_id', function (req, res) {
+    return res.render('react-applet', {
+        pageTitle: "Query"
+    });
+});
+
+
+/*  API routes
+============================================================================= */
 
 router.get('/api/queries', function (req, res) {
     /*
@@ -62,112 +59,92 @@ router.get('/api/queries', function (req, res) {
     })
 })
 
-router.get('/queries', getQueryFilterData, function (req, res) {
-    return res.render('react-applet', {
-        pageTitle: "Queries"
-    });
-});
-
-function getControlKeyText (req, res, next) {
-    var ua = req.headers['user-agent'];
-    var os = uaParser.parseOS(ua).toString();
-    res.locals.isMac = (os.search(/mac/i) >= 0);
-    if (res.locals.isMac) {
-        res.locals.controlKeyText = 'Command';
-    } else {
-        res.locals.controlKeyText = 'Ctrl';
-    }
-    next();
-}
-
-router.get('/queries/:_id', getControlKeyText, function (req, res) {
+router.get('/api/queries/:_id', function (req, res) {
     Connection.findAll(function (err, connections) {
-        res.locals.queryMenu = true;
-        res.locals.cacheKey = uuid.v1();
-        res.locals.navbarConnections = _.sortBy(connections, 'name');
-        res.locals.allowDownload = config.get('allowCsvDownload');
-        var format = req.query && req.query.format;
-        if (req.params._id === 'new') {
-            res.render('query', {
-                query: {
-                    name: ""
-                },
-                format: format
-            });
-        } else {
-            Query.findOneById(req.params._id, function (err, query) {
-                // TODO: render error if this fails?
-                query.logAccess(noop);
-                if (query && query.tags) query.tags = query.tags.join(', ');
-                if (format === 'json') {
-                    // send JSON of query object
-                    res.json(query);
-                } else {
-                    // render page
-                    res.render('query', {
-                        query: query, 
-                        // format may be set to 'chart' or 'table'
-                        format: format,
-                        fullscreenContent: ['chart', 'table'].indexOf(format) > -1 ? true: false
-                    });
-                }
-            })
-        }
-    });
-});
-
-router.post('/queries/:_id', function (req, res) {
-    // save the query, to the query db
-    if (req.params._id == "new") {
-        var query = new Query({
-            name: req.body.name || "No Name Query",
-            tags: req.body.tags,
-            connectionId: req.body.connectionId,
-            queryText: req.body.queryText,
-            chartConfiguration: req.body.chartConfiguration,
-            createdBy: req.user.email,
-            modifiedBy: req.user.email
-        });
-        query.save(function (err, newQuery) {
-            if (err) {
-                console.log(err);
-                res.send({err: err, success: false});
-            } else {
-                // push query to slack if set up. 
-                // this is async, but save operation doesn't care about when/if finished
-                newQuery.pushQueryToSlackIfSetup(); 
-                res.send({success:true, query: newQuery});
-            }
-        });
-    } else {
         Query.findOneById(req.params._id, function (err, query) {
             if (err) {
-                return res.send({
+                return res.json({
                     err: err,
-                    success: false
+                    connections: connections,
+                    query: null
                 });
             }
             if (!query) {
-                return res.send({
-                    err: "No query found for that Id",
-                    success: false
+                return res.json({
+                    connections: connections,
+                    query: {
+                        // defaults here?
+                    }
                 });
             }
-            query.name = req.body.name || "";
-            query.tags = req.body.tags;
-            query.connectionId = req.body.connectionId;
-            query.queryText = req.body.queryText;
-            query.chartConfiguration = req.body.chartConfiguration;
-            query.modifiedBy = req.user.email;
-            query.save(function (err, newQuery) {
-                if (err) {
-                    console.log(err);
-                    return res.send({success: false, err: err});
-                }
-                return res.send({success: true, query: newQuery});
+            return res.json({
+                connections: connections,
+                query: query
             });
         });
-    }
+    });
+});
+
+// create new
+router.post('/api/queries', function (req, res) {
+    // previously posted to api/queries/:_id, req.params._id would have been "new"
+    // now though we know its new because the client did that for us 
+    var query = new Query({
+        name: req.body.name || "No Name Query",
+        tags: req.body.tags,
+        connectionId: req.body.connectionId,
+        queryText: req.body.queryText,
+        chartConfiguration: req.body.chartConfiguration,
+        createdBy: req.user.email,
+        modifiedBy: req.user.email
+    });
+    query.save(function (err, newQuery) {
+        if (err) {
+            console.log(err);
+            res.send({
+                err: err, 
+                success: false
+            });
+        } else {
+            // push query to slack if set up. 
+            // this is async, but save operation doesn't care about when/if finished
+            newQuery.pushQueryToSlackIfSetup(); 
+            res.send({
+                success:true, 
+                query: newQuery
+            });
+        }
+    });
+})
+
+router.put('/api/queries/:_id', function (req, res) {
+    Query.findOneById(req.params._id, function (err, query) {
+        if (err) {
+            return res.send({
+                err: err,
+                success: false
+            });
+        }
+        if (!query) {
+            return res.send({
+                err: "No query found for that Id",
+                success: false
+            });
+        }
+        query.name = req.body.name || "";
+        query.tags = req.body.tags;
+        query.connectionId = req.body.connectionId;
+        query.queryText = req.body.queryText;
+        query.chartConfiguration = req.body.chartConfiguration;
+        query.modifiedBy = req.user.email;
+        query.save(function (err, newQuery) {
+            if (err) {
+                console.log(err);
+                return res.send({success: false, err: err});
+            }
+            return res.send({success: true, query: newQuery});
+        });
+    });
 });
 
 router.delete('/queries/:_id', function (req, res) {
