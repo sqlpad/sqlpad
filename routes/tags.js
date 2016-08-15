aa0ae5c0cd60dfe1408603c189a3d5cae1e178c5
Rick Bergfalk <rick.bergfalk@gmail.com>
2016-08-15 16:53:07 -0500

    api tags json response to follow convention

--- routes/tags.js
+++ routes/tags.js
@@ -5,7 +5,10 @@ var Query = require('../models/Query.js');
 router.get('/api/tags', function (req, res) {
     Query.findAll(function (err, queries) {
         var tags = _.uniq(_.flatten(_.map(queries, 'tags'))).sort();
-        res.json(tags);
+        res.json({
+            err: err,
+            tags: tags    
+        });
     });
 });
 
