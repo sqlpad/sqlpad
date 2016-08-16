0c1541e7c4b191c5089a3cd3059941c4003f3ae0
Rick Bergfalk <rick.bergfalk@gmail.com>
2016-08-15 21:45:58 -0500

    update main.js to handle newer react component

--- client-js/main.js
+++ client-js/main.js
@@ -1,41 +1,26 @@
-//  This is where all the client side js stuff is required so it can be bundled 
-//  via Browserify. 
-//  All the heavy old-school javascript libraries are exposed as browserify globals
-//  because its easy and they don't play well with browserify without some work.
-//
-//  So far, as a general pattern I've been puting jQuery javascript
-//  in smaller files. Then I just require them  here and execute the function to 
-//  bind any events and whatever else to the page. 
+//  This is the client side js entry file to be browserified
 
+// old jquery stuff
 require('./connection.js')();
 require('./connection-admin.js')();
 require('./user-admin.js')();
 
-// used on queries.ejs for reading the query filter form and doing the ajax
-require('./query-filter-form.js')();
 
-// All the stuff that happens when viewing/working with a single query happens here
-//require('./query-editor.js')();
-
-
-// stuff below is gradually being converted into react
-// as more react components are available these can be 
-// managed in a more elegant way 
-// for now page.js is gradually being worked in. 
+// stuff below is gradually being converted into react applets
+// for now page.js is being used to provide a bit of a router 
 var page = require('page');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 
-var ConfigValues = require('./ConfigValues.js');
 
+var ConfigValues = require('./ConfigValues.js');
 if (document.getElementById('config-values')) {
     ReactDOM.render(
         <ConfigValues/>,
         document.getElementById('config-values')
     );
 }
-
 
 var FilterableQueryList = require('./FilterableQueryList.js');
 page.base(baseUrl);
@@ -45,12 +30,51 @@ page('/queries', function (ctx) {
         document.getElementById('react-applet')
     )
 })
-page({click: false});
-/*
-if (document.getElementById('filterable-query-list')) {
+
+var QueryEditor = require('./QueryEditor.js');
+page('/queries/:queryId', getConfig, getTags, function (ctx) {
     ReactDOM.render(
-        <FilterableQueryList/>,
-        document.getElementById('filterable-query-list')
+        <QueryEditor 
+            queryId={ctx.params.queryId}
+            availableTags={ctx.tags} 
+            config={ctx.config}/>,
+        document.getElementById('react-applet')
     )
+})
+
+// init page router
+page({click: false});
+
+
+// client-side middleware
+function getConfig (ctx, next) {
+    fetch(baseUrl + "/api/config", {credentials: 'same-origin'})
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            ctx.config = json.config;
+        })
+        .catch(function (ex) {
+            console.error(ex.toString());
+        })
+        .then(() => {
+            next();
+        })
 }
-*/
+
+function getTags (ctx, next) {
+    fetch(baseUrl + "/api/tags", {credentials: 'same-origin'})
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            ctx.tags = json.tags;
+        })
+        .catch(function (ex) {
+            console.error(ex.toString());
+        })
+        .then(() => {
+            next();
+        })
+}
