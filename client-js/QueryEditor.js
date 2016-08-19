7ba91a25c1d5d8b3921e01c867b4a66af0772bbf
Rick Bergfalk <rick.bergfalk@gmail.com>
2016-08-19 12:32:52 -0500

    disable run button when running

--- client-js/QueryEditor.js
+++ client-js/QueryEditor.js
@@ -422,8 +422,8 @@ var QueryEditor = React.createClass({
                                 disabled={this.state.isSaving}>
                                 <span className="shortcut-letter">S</span>{this.state.isSaving ? 'aving' : 'ave'}
                             </Button>
-                            <Button className="QueryEditorSubheaderItem" onClick={this.runQuery}>
-                                <span className="shortcut-letter">R</span>un
+                            <Button className="QueryEditorSubheaderItem" onClick={this.runQuery} disabled={this.state.isRunning}>
+                                <span className="shortcut-letter">R</span>{this.state.isRunning ? 'unning' : 'un'}
                             </Button>
                             <ControlLabel onClick={this.openQueryDetailsModal} className="QueryEditorSubheaderItem QueryEditorQueryName">{(this.state.query.name ? this.state.query.name : "(click to name query)")}</ControlLabel>
                             <QueryDetailsModal 
