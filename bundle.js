21cd40b42c7a7334b4dcf925418d3c0cb95d0f45
Rick Bergfalk <rick.bergfalk@gmail.com>
2016-09-07 12:14:32 -0500

    standard style to bundle.js

--- bundle.js
+++ bundle.js
@@ -1,48 +1,47 @@
-var fs = require("fs");
-var browserify = require('browserify');
-var watchify = require('watchify');
-var uglifyify = require('uglifyify');
-var minimist = require('minimist');
-var config = minimist(process.argv.slice(2));
+var fs = require('fs')
+var browserify = require('browserify')
+var watchify = require('watchify')
+var minimist = require('minimist')
+var config = minimist(process.argv.slice(2))
 
-var exposeConfig = { 
-    expose: { 
-        jquery: '$', 
-        tauCharts: 'tauCharts',
-        _: '_'
-    } 
-};
-
-var b;
-
-if (config.dev) {
-    b = browserify({ 
-        entries: ['./client-js/main.js'],
-        cache: {}, 
-        packageCache: {}, 
-        debug: true, 
-        fullPaths: true
-    });
-    b.plugin(watchify);
-    b.transform("babelify", {presets: ["es2015", "react"]});
-    b.transform("exposify", exposeConfig);
-    b.on('log', function (msg) {
-        console.log(msg);
-    });
-    b.on('update', bundle);
-    bundle();
-} else {
-    process.env.NODE_ENV = "production";
-    b = browserify({
-        entries: ['./client-js/main.js'],
-        fullPaths: true
-    });
-    b.transform("babelify", {presets: ["es2015", "react"]});
-    b.transform("exposify", exposeConfig);
-    b.transform("uglifyify", {global: true});
-    bundle();
+var exposeConfig = {
+  expose: {
+    jquery: '$',
+    tauCharts: 'tauCharts',
+    _: '_'
+  }
 }
 
-function bundle() {
-    b.bundle().pipe(fs.createWriteStream("./public/javascripts/browserified.js"));
+var b
+
+if (config.dev) {
+  b = browserify({
+    entries: ['./client-js/main.js'],
+    cache: {},
+    packageCache: {},
+    debug: true,
+    fullPaths: true
+  })
+  b.plugin(watchify)
+  b.transform('babelify', {presets: ['es2015', 'react']})
+  b.transform('exposify', exposeConfig)
+  b.on('log', function (msg) {
+    console.log(msg)
+  })
+  b.on('update', bundle)
+  bundle()
+} else {
+  process.env.NODE_ENV = 'production'
+  b = browserify({
+    entries: ['./client-js/main.js'],
+    fullPaths: true
+  })
+  b.transform('babelify', {presets: ['es2015', 'react']})
+  b.transform('exposify', exposeConfig)
+  b.transform('uglifyify', {global: true})
+  bundle()
+}
+
+function bundle () {
+  b.bundle().pipe(fs.createWriteStream('./public/javascripts/browserified.js'))
 }
