1c491db6ef3d9ce81fbc5b6533b5a85931bac6fc
Rick Bergfalk <rick.bergfalk@gmail.com>
2016-09-12 15:29:24 -0500

    add comment why this route is special

--- routes/app.js
+++ routes/app.js
@@ -4,6 +4,9 @@ var getVersion = require('../lib/get-version.js')
 var config = require('../lib/config.js')
 var User = require('../models/User.js')
 
+// NOTE: this route needs a wildcard because it is fetched as a relative url
+// from the front-end. The static SPA does not know if sqlpad is mounted at
+// the root of a domain or if there is a base-url provided in the config
 router.get('*/api/app', function (req, res) {
   User.adminRegistrationOpen(function (err, open) {
     if (err) {
