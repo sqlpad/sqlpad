90387b44e603138e9bdc4c2bcd54e8425c8a60b4
Rick Bergfalk <rick.bergfalk@gmail.com>
2016-09-16 12:42:55 -0500

    add query title and export button to chart-only view

--- client-js/QueryChartOnly.js
+++ client-js/QueryChartOnly.js
@@ -1,6 +1,9 @@
 var React = require('react')
 var fetchJson = require('./fetch-json.js')
 var SqlpadTauChart = require('./SqlpadTauChart.js')
+var DropdownButton = require('react-bootstrap/lib/DropdownButton')
+var MenuItem = require('react-bootstrap/lib/MenuItem')
+import IncompleteDataNotification from './components/IncompleteDataNotification'
 
 var QueryEditor = React.createClass({
   getInitialState: function () {
@@ -44,14 +47,43 @@ var QueryEditor = React.createClass({
   componentDidMount: function () {
     this.runQuery(this.props.queryId)
   },
+  onSaveImageClick: function (e) {
+    if (this.sqlpadTauChart && this.sqlpadTauChart.chart) {
+      this.sqlpadTauChart.chart.fire('exportTo', 'png')
+    }
+  },
   render: function () {
+    var csvDownloadLink
+    var xlsxDownloadLink
+    if (this.state.queryResult) {
+      csvDownloadLink = this.props.config.baseUrl + '/download-results/' + this.state.queryResult.cacheKey + '.csv'
+      xlsxDownloadLink = this.props.config.baseUrl + '/download-results/' + this.state.queryResult.cacheKey + '.xlsx'
+    }
     return (
-      <SqlpadTauChart
-        query={this.state.query}
-        config={this.props.config}
-        queryResult={this.state.queryResult}
-        queryError={this.state.queryError}
-        isRunning={this.state.isRunning} />
+      <div style={{position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}>
+        <h3 style={{marginLeft: 20}}>{(this.state.query ? this.state.query.name : '')}</h3>
+        <div style={{position: 'absolute', top: 20, right: 20}}>
+          <IncompleteDataNotification queryResult={this.state.queryResult} />
+          {(this.state.queryResult ? (
+            <DropdownButton title='Export' id='export-dropdown-button' pullRight>
+              <MenuItem eventKey='1' onClick={this.onSaveImageClick}>png</MenuItem>
+              {(this.props.config.allowCsvDownload ? (<MenuItem eventKey='2' target='_blank' href={csvDownloadLink}>csv</MenuItem>) : null)}
+              {(this.props.config.allowCsvDownload ? (<MenuItem eventKey='3' target='_blank' href={xlsxDownloadLink}>xlsx</MenuItem>) : null)}
+            </DropdownButton>
+          ) : null)}
+        </div>
+        <div style={{position: 'absolute', top: 60, right: 0, bottom: 0, left: 0, padding: 40}}>
+          <SqlpadTauChart
+            query={this.state.query}
+            config={this.props.config}
+            queryResult={this.state.queryResult}
+            queryError={this.state.queryError}
+            isRunning={this.state.isRunning}
+            ref={(ref) => {
+              this.sqlpadTauChart = ref
+            }} />
+        </div>
+      </div>
     )
   }
 })
