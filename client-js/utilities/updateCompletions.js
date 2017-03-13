4c67048c94557336228b3e2bb07a1e2c2058703d
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-03-12 23:38:59 -0400

    Checkpoint

--- client-js/utilities/updateCompletions.js
+++ client-js/utilities/updateCompletions.js
@@ -7,6 +7,9 @@ import 'brace/ext/language_tools'
 
 module.exports = updateCompletions
 
+// TODO rethink algorithm: when table wanted, open to any schema/table
+// when column wanted, look for referenced schemas/tables, and autocomplete off that
+
 // There's stuff below that logs to console a lot
 // documentation on this autocompletion is light
 // and you may find it helpful to print some vars out during dev
@@ -41,37 +44,38 @@ function updateCompletions (schemaInfo) {
   // NOTE sqlpad is also firing autocomplete on every keypress instead of using live autocomplete
   const schemaCompletions = []
   const tableCompletions = []
-  const columnCompletions = []
 
   // we also should create an index of dotted autocompletes.
   // given a precedingtoken as "sometable." or "someschema.table." we should be able to look up relevant completions
   // combos to support are...
-  // schema.
-  // schema.table.
-  // table.
-  const dottedMatches = {}
+  // SCHEMA
+  // TABLE
+  // SCHEMA.TABLE
+  const matchMaps = {
+    schema: {}, // will contain tables
+    table: {},
+    schemaTable: {}
+  }
 
   Object.keys(schemaInfo).forEach(schema => {
     schemaCompletions.push({name: schema, value: schema, score: 0, meta: 'schema'})
-    const schemaDot = schema.toUpperCase() + '.'
-    if (!dottedMatches[schemaDot]) dottedMatches[schemaDot] = []
+    const SCHEMA = schema.toUpperCase()
+    if (!matchMaps.schema[SCHEMA]) matchMaps.schema[SCHEMA] = []
 
     Object.keys(schemaInfo[schema]).forEach(table => {
-      const schemaTableDot = schema.toUpperCase() + '.' + table.toUpperCase() + '.'
-      const tableDot = table.toUpperCase() + '.'
-      if (!dottedMatches[schemaTableDot]) dottedMatches[schemaTableDot] = []
-      if (!dottedMatches[tableDot]) dottedMatches[tableDot] = []
-
-      const tableCompletion = {name: table, value: table, score: 0, meta: schema}
+      const SCHEMA_TABLE = SCHEMA + '.' + table.toUpperCase()
+      const TABLE = table.toUpperCase()
+      if (!matchMaps.table[TABLE]) matchMaps.table[TABLE] = []
+      if (!matchMaps.schemaTable[SCHEMA_TABLE]) matchMaps.schemaTable[SCHEMA_TABLE] = []
+      const tableCompletion = {name: table, value: table, score: 0, meta: schema, schema}
       tableCompletions.push(tableCompletion)
-      dottedMatches[schemaDot].push(tableCompletion)
+      matchMaps.schema[SCHEMA].push(tableCompletion)
 
       const columns = schemaInfo[schema][table]
       columns.forEach(column => {
-        const columnCompletion = {name: schema + table + column.column_name, value: column.column_name, score: 0, meta: table}
-        columnCompletions.push(columnCompletion)
-        dottedMatches[tableDot].push(columnCompletion)
-        dottedMatches[schemaTableDot].push(columnCompletion)
+        const columnCompletion = {name: schema + table + column.column_name, value: column.column_name, score: 0, meta: table, schema, table}
+        matchMaps.table[TABLE].push(columnCompletion)
+        matchMaps.schemaTable[SCHEMA_TABLE].push(columnCompletion)
       })
     })
   })
@@ -80,32 +84,58 @@ function updateCompletions (schemaInfo) {
 
   const myCompleter = {
     getCompletions: function (editor, session, pos, prefix, callback) {
+      // figure out if there are any schemas/tables referenced in query
+      const allTokens = session.getValue().split(/\s+/).map(t => t.toUpperCase())
+      const relevantDottedMatches = {}
+      Object.keys(matchMaps.schemaTable).forEach(schemaTable => {
+        if (allTokens.indexOf(schemaTable) >= 0) {
+          relevantDottedMatches[schemaTable] = matchMaps.schemaTable[schemaTable]
+        }
+      })
+      Object.keys(matchMaps.table).forEach(table => {
+        if (allTokens.indexOf(table) >= 0) {
+          relevantDottedMatches[table] = matchMaps.table[table]
+          // HACK add schemaTable match for this table
+          // we store schema at column match item, so look at first one and use that
+          const firstMatch = matchMaps.table[table][0]
+          const schemaTable = firstMatch.schema.toUpperCase() + '.' + firstMatch.table.toUpperCase()
+          relevantDottedMatches[schemaTable] = {name: firstMatch.table, value: firstMatch.table, score: 0, meta: firstMatch.schema}
+
+        }
+      })
+      debug('matched found: ', Object.keys(relevantDottedMatches))
+
+      // complete for schema and tables already referenced, plus their columns
+      let matches = []
+      Object.keys(relevantDottedMatches).forEach(key => {
+        matches = matches.concat(relevantDottedMatches[key])
+      })
+      const schemas = {}
+      const tables = {}
+      const wantedColumnCompletions = []
+      matches.forEach(match => {
+        if (match.schema) schemas[match.schema] = match.schema
+        if (match.table) tables[match.table] = match.schema
+      })
+      Object.keys(schemas).forEach(schema => {
+        wantedColumnCompletions.push({name: schema, value: schema, score: 0, meta: 'schema'})
+      })
+      Object.keys(tables).forEach(table => {
+        const tableCompletion = {name: table, value: table, score: 0, meta: 'table'}
+        wantedColumnCompletions.push(tableCompletion)
+        const SCHEMA = tables[table].toUpperCase()
+        if (!relevantDottedMatches[SCHEMA]) relevantDottedMatches[SCHEMA] = []
+        relevantDottedMatches[SCHEMA].push(tableCompletion)
+      })
+
       // get tokens leading up to the cursor to figure out context
       // depending on where we are we either want tables or we want columns
       const tableWantedKeywords = ['FROM', 'JOIN']
       const columnWantedKeywords = ['SELECT', 'WHERE', 'GROUP', 'HAVING', 'ON']
 
-      const currentLine = session.getDocument().getLine(pos.row)
-      const currentTokens = currentLine.slice(0, pos.column).split(/\s+/).map(t => t.toUpperCase())
-      const precedingCharacter = currentLine.slice(pos.column - 1, pos.column)
-      const precedingToken = currentTokens[currentTokens.length - 1]
-
-      // if there's no prefix, check to see if precedingCharacter is a .
-      // if so, we need completions matched based on previous token
-      debug('PREFIX: "%s"', prefix)
-      debug('PRECEDING CHAR: "%s"', precedingCharacter)
-      debug('PRECEDING TOKEN: "%s"', precedingToken)
-      if (precedingToken.indexOf('.') >= 0) {
-        let dotTokens = precedingToken.split('.')
-        dotTokens.pop()
-        const dotMatch = dotTokens.join('.') + '.'
-        debug('Completing for "%s" even though we got "%s"', dotMatch, precedingToken)
-        return callback(null, dottedMatches[dotMatch.toUpperCase()])
-      }
-
-      // if we are not dealing with a . match try to figure out where we are in the query
-      // and whether the user would like columns or just tables
+      // find out what is wanted
       // first look at the current line before cursor, then rest of lines beforehand
+      let wanted = ''
       const currentRow = pos.row
       for (let r = currentRow; r >= 0; r--) {
         let line = session.getDocument().getLine(r)
@@ -119,16 +149,53 @@ function updateCompletions (schemaInfo) {
         for (let i = lineTokens.length - 1; i >= 0; i--) {
           const token = lineTokens[i]
           if (columnWantedKeywords.indexOf(token) >= 0) {
-            debug('want column')
-            return callback(null, figureColumns(session, dottedMatches))
+            debug('WANT COLUMN BECAUSE FOUND: ', token)
+            wanted = 'COLUMN'
+            r = 0
+            break
           }
           if (tableWantedKeywords.indexOf(token) >= 0) {
-            debug('want table')
-            return callback(null, tableWantedCompletions)
+            debug('WANT TABLE BECAUSE FOUND: ', token)
+            wanted = 'TABLE'
+            r = 0
+            break
           }
         }
       }
+      debug('WANTED: ', wanted)
 
+      const currentLine = session.getDocument().getLine(pos.row)
+      const currentTokens = currentLine.slice(0, pos.column).split(/\s+/).map(t => t.toUpperCase())
+      const precedingCharacter = currentLine.slice(pos.column - 1, pos.column)
+      const precedingToken = currentTokens[currentTokens.length - 1]
+
+      // if preceding token has a . try to provide completions based on that object
+      debug('PREFIX: "%s"', prefix)
+      debug('PRECEDING CHAR: "%s"', precedingCharacter)
+      debug('PRECEDING TOKEN: "%s"', precedingToken)
+      if (precedingToken.indexOf('.') >= 0) {
+        let dotTokens = precedingToken.split('.')
+        dotTokens.pop()
+        const DOT_MATCH = dotTokens.join('.').toUpperCase()
+        debug('Completing for "%s" even though we got "%s"', DOT_MATCH, precedingToken)
+        if (wanted === 'TABLE') {
+          // if we're in a table place, a completion should only be for tables, not columns
+          return callback(null, matchMaps.schema[DOT_MATCH])
+        }
+        if (wanted === 'COLUMN') {
+          // here we should see show matches for only the tables mentioned in query
+          return callback(null, relevantDottedMatches[DOT_MATCH])
+        }
+      }
+
+      // if we are not dealing with a . match show all relevant objects
+      if (wanted === 'TABLE') {
+        return callback(null, tableWantedCompletions)
+      }
+      if (wanted === 'COLUMN') {
+        // TODO also include alias?
+        return callback(null, matches.concat(wantedColumnCompletions))
+      }
       // No keywords found? User probably wants some keywords
       callback(null, keywordCompletions)
     }
@@ -139,27 +206,4 @@ function updateCompletions (schemaInfo) {
     // Note - later on might be able to set a completer for specific editor like:
     // editor.completers = [staticWordCompleter]
   })
-}
-
-
-
-
-
-function figureColumns (session, dottedMatches) {
-  const allTokens = session.getValue().split(/\s+/).map(t => t.toUpperCase())
-  // remove last character from the dotmatch keys
-  // the query will have occurences of schema.table not schema.table.
-  const relevantDottedMatches = {}
-  Object.keys(dottedMatches).map(dotKey => dotKey.slice(0, -1)).forEach(dotKey => {
-    if (allTokens.indexOf(dotKey) >= 0) {
-      relevantDottedMatches[dotKey] = dottedMatches[dotKey + '.']
-    }
-  })
-  debug(relevantDottedMatches)
-  let matches = []
-  Object.keys(relevantDottedMatches).forEach(key => {
-    matches = matches.concat(relevantDottedMatches[key])
-  })
-  debug(matches)
-  return matches
 }
