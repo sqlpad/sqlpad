581aa95a4c5a77d518835b4e6c3e47744ef70ed3
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-08-27 01:33:48 -0400

    Separate schema getting from schema-info route (#217)
    This is in preparation for future refactoring, as well as making it
    easier for future db drivers to add custom schema logic

--- routes/schema-info.js
+++ routes/schema-info.js
@@ -1,22 +1,9 @@
-var runQuery = require('../lib/run-query.js')
 var _ = require('lodash')
-var fs = require('fs')
-var path = require('path')
 var router = require('express').Router()
-var config = require('../lib/config.js')
 var Connection = require('../models/Connection.js')
 var Cache = require('../models/Cache.js')
-var decipher = require('../lib/decipher.js')
+const getSchemaForConnection = require('../lib/get-schema-for-connection.js')
 var mustBeAuthenticated = require('../middleware/must-be-authenticated.js')
-
-var sqldir = path.join(__dirname, '/../resources/')
-
-var sqlSchemaPostgres = fs.readFileSync(sqldir + '/schema-postgres.sql', {encoding: 'utf8'})
-var sqlSchemaMysql = fs.readFileSync(sqldir + '/schema-mysql.sql', {encoding: 'utf8'})
-var sqlSchemaVertica = fs.readFileSync(sqldir + '/schema-vertica.sql', {encoding: 'utf8'})
-var sqlSchemaCrate = fs.readFileSync(sqldir + '/schema-crate.sql', {encoding: 'utf8'})
-var sqlSchemaCrateV0 = fs.readFileSync(sqldir + '/schema-crate.v0.sql', {encoding: 'utf8'})
-var sqlSchemaStandard = fs.readFileSync(sqldir + '/schema-standard.sql', {encoding: 'utf8'})
 
 router.get('/api/schema-info/:connectionId', mustBeAuthenticated,
   function initLocals (req, res, next) {
@@ -24,7 +11,6 @@ router.get('/api/schema-info/:connectionId', mustBeAuthenticated,
     res.locals.tree = {}
     res.locals.cacheKey = null
     res.locals.connection = null
-    res.locals.SHOW_SCHEMA_COPY_BUTTON = config.get('showSchemaCopyButton')
     res.locals.connectionId = req.params.connectionId
     next()
   },
@@ -70,83 +56,17 @@ router.get('/api/schema-info/:connectionId', mustBeAuthenticated,
   },
 
   function runSchemaQuery (req, res, next) {
-    var connection = res.locals.connection
-
-    connection.username = decipher(connection.username)
-    connection.password = decipher(connection.password)
-    connection.maxRows = typeof Number.MAX_SAFE_INTEGER === 'undefined' ? 9007199254740991 : Number.MAX_SAFE_INTEGER
-
-    let primarySchemaSql = ''
-    let secondarySchemaSql
-    if (connection.driver === 'vertica') {
-      primarySchemaSql = sqlSchemaVertica
-    } else if (connection.driver === 'crate') {
-      primarySchemaSql = sqlSchemaCrate
-      secondarySchemaSql = sqlSchemaCrateV0
-    } else if (connection.driver === 'postgres') {
-      primarySchemaSql = sqlSchemaPostgres
-    } else if (connection.driver === 'mysql') {
-      primarySchemaSql = sqlSchemaMysql
-    } else {
-      primarySchemaSql = sqlSchemaStandard
-    }
-
-    runQuery(primarySchemaSql, connection, function (err, queryResult) {
-      if (err && !secondarySchemaSql) {
+    const connection = res.locals.connection
+    getSchemaForConnection(connection, function (err, tree) {
+      if (err) {
         console.error(err)
         return res.json({
           error: 'Problem running schema info query'
         })
       }
-      if (err && secondarySchemaSql) {
-        return runQuery(secondarySchemaSql, connection, function (err, queryResult) {
-          if (err) {
-            console.error(err)
-            return res.json({
-              error: 'Problem running schema info query'
-            })
-          }
-          res.locals.queryResult = queryResult
-          next()
-        })
-      }
-      res.locals.queryResult = queryResult
+      res.locals.tree = tree
       next()
     })
-  },
-
-  function formatResults (req, res, next) {
-    const queryResult = res.locals.queryResult
-    var bySchema = _.groupBy(queryResult.rows, 'table_schema')
-    for (var schema in bySchema) {
-      if (bySchema.hasOwnProperty(schema)) {
-        res.locals.tree[schema] = {}
-        var byTableName = _.groupBy(bySchema[schema], 'table_name')
-        for (var tableName in byTableName) {
-          if (byTableName.hasOwnProperty(tableName)) {
-            res.locals.tree[schema][tableName] = byTableName[tableName]
-          }
-        }
-      }
-    }
-    // TODO get rid of is_nullable since no plans on using it in UI
-    /*
-    At this point, tree should look like this:
-      {
-        "table": {
-          "dbo": {
-            "tablename": [
-              {
-                column_name: "the column name",
-                data_type: "string",
-                is_nullable: "no"
-              }
-            ]
-          }
-        }
-      }
-    */
-    next()
   },
 
   function updateCacheAndRender (req, res, next) {
