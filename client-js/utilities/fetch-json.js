9dfc092dd7bb0a265ba22511121d9860d1a3640f
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-09-04 19:09:47 -0500

    Force no-cache on fetch requests

--- client-js/utilities/fetch-json.js
+++ client-js/utilities/fetch-json.js
@@ -1,17 +1,20 @@
 import 'whatwg-fetch'
 
-export default function fetchJson (verb, url, body) {
-  var opts = {
-    method: 'GET',
+export default function fetchJson (method, url, body) {
+  const opts = {
+    method: method.toUpperCase(),
     credentials: 'same-origin',
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache',
+      'Expires': '-1',
+      'Pragma': 'no-cache'
     }
   }
-  if (!verb) throw new Error('You must supply a verb to the fetch wrapper')
-  opts.method = verb.toUpperCase()
-  if (body) opts.body = JSON.stringify(body)
+  if (body) {
+    opts.body = JSON.stringify(body)
+  }
   return fetch(url, opts).then((response) => {
     if (response.status === 200) {
       return response.json()
