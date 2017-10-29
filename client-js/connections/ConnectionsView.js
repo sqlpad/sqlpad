0c630ac8bb63f8f9674e174640f2f63bfcf0b4df
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-10-29 19:51:41 -0400

    Clean up those function binds (#254)

--- client-js/connections/ConnectionsView.js
+++ client-js/connections/ConnectionsView.js
@@ -5,21 +5,11 @@ import ConnectionList from './ConnectionList'
 import ConnectionForm from './ConnectionForm'
 
 class ConnectionsView extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      connections: [],
-      selectedConnection: null,
-      isTesting: false,
-      isSaving: false
-    }
-    this.handleSelect = this.handleSelect.bind(this)
-    this.handleDelete = this.handleDelete.bind(this)
-    this.onNewConnectionClick = this.onNewConnectionClick.bind(this)
-    this.setConnectionValue = this.setConnectionValue.bind(this)
-    this.loadConnectionsFromServer = this.loadConnectionsFromServer.bind(this)
-    this.testConnection = this.testConnection.bind(this)
-    this.saveConnection = this.saveConnection.bind(this)
+  state = {
+    connections: [],
+    selectedConnection: null,
+    isTesting: false,
+    isSaving: false
   }
 
   componentDidMount() {
@@ -27,64 +17,59 @@ class ConnectionsView extends React.Component {
     this.loadConnectionsFromServer()
   }
 
-  handleSelect(connection) {
+  handleSelect = connection => {
     this.setState({
       selectedConnection: Object.assign({}, connection)
     })
   }
 
-  handleDelete(connection) {
+  handleDelete = connection => {
+    const { selectedConnection } = this.state
     fetchJson('DELETE', '/api/connections/' + connection._id).then(json => {
       if (json.error) return Alert.error('Delete failed')
       Alert.success('Connection deleted')
-      if (
-        this.state.selectedConnection &&
-        connection._id === this.state.selectedConnection._id
-      ) {
+      if (selectedConnection && connection._id === selectedConnection._id) {
         this.setState({ selectedConnection: null })
       }
       this.loadConnectionsFromServer()
     })
   }
 
-  onNewConnectionClick() {
+  onNewConnectionClick = () => {
     this.setState({
       selectedConnection: {}
     })
   }
 
-  setConnectionValue(attribute, value) {
-    var selectedConnection = this.state.selectedConnection
+  setConnectionValue = (attribute, value) => {
+    const { selectedConnection } = this.state
     if (selectedConnection) {
       selectedConnection[attribute] = value
-      this.setState({ selectedConnection: selectedConnection })
+      this.setState({ selectedConnection })
     }
   }
 
-  loadConnectionsFromServer() {
+  loadConnectionsFromServer = () => {
     fetchJson('GET', '/api/connections').then(json => {
       if (json.error) Alert.error(json.error)
       this.setState({ connections: json.connections })
     })
   }
 
-  testConnection() {
+  testConnection = () => {
+    const { selectedConnection } = this.state
     this.setState({ isTesting: true })
-    fetchJson(
-      'POST',
-      '/api/test-connection',
-      this.state.selectedConnection
-    ).then(json => {
+    fetchJson('POST', '/api/test-connection', selectedConnection).then(json => {
       this.setState({ isTesting: false })
       if (json.error) return Alert.error('Test Failed')
       return Alert.success('Test successful')
     })
   }
 
-  saveConnection() {
+  saveConnection = () => {
     const { selectedConnection } = this.state
     this.setState({ isSaving: true })
-    if (this.state.selectedConnection._id) {
+    if (selectedConnection._id) {
       fetchJson(
         'PUT',
         '/api/connections/' + selectedConnection._id,
@@ -97,14 +82,10 @@ class ConnectionsView extends React.Component {
         this.loadConnectionsFromServer()
       })
     } else {
-      fetchJson(
-        'POST',
-        '/api/connections',
-        this.state.selectedConnection
-      ).then(json => {
+      fetchJson('POST', '/api/connections', selectedConnection).then(json => {
         this.setState({
           isSaving: false,
-          selectedConnection: json.connection || this.state.selectedConnection
+          selectedConnection: json.connection || selectedConnection
         })
         if (json.error) return Alert.error('Save failed')
         Alert.success('Connection saved')
@@ -115,22 +96,23 @@ class ConnectionsView extends React.Component {
   }
 
   render() {
+    const { isTesting, isSaving, connections, selectedConnection } = this.state
     return (
       <div className="flex-100">
         <ConnectionList
-          connections={this.state.connections}
-          selectedConnection={this.state.selectedConnection}
+          connections={connections}
+          selectedConnection={selectedConnection}
           handleSelect={this.handleSelect}
           handleDelete={this.handleDelete}
           onNewConnectionClick={this.onNewConnectionClick}
         />
         <ConnectionForm
-          selectedConnection={this.state.selectedConnection}
+          selectedConnection={selectedConnection}
           setConnectionValue={this.setConnectionValue}
           testConnection={this.testConnection}
           saveConnection={this.saveConnection}
-          isTesting={this.state.isTesting}
-          isSaving={this.state.isSaving}
+          isTesting={isTesting}
+          isSaving={isSaving}
         />
       </div>
     )
