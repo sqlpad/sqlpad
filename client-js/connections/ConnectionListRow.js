7e22e24d854c39d050f4f4ceffd777448b49fd84
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-10-29 18:57:05 -0400

    common DeleteButton component from connections

--- client-js/connections/ConnectionListRow.js
+++ client-js/connections/ConnectionListRow.js
@@ -1,68 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Button from 'react-bootstrap/lib/Button'
-import Glyphicon from 'react-bootstrap/lib/Glyphicon'
-import Popover from 'react-bootstrap/lib/Popover'
-import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger'
+import DeleteButton from '../common/DeleteButton'
 
 class ConnectionListRow extends React.Component {
-  constructor(props) {
-    super(props)
-    this.onDelete = this.onDelete.bind(this)
-    this.onSelect = this.onSelect.bind(this)
+  handleDeleteClick = e => {
+    const { handleDelete, connection } = this.props
+    handleDelete(connection)
   }
 
-  onDelete(e) {
-    this.props.handleDelete(this.props.connection)
-  }
-
-  onSelect(e) {
-    this.props.handleSelect(this.props.connection)
+  handleConnectionClick = e => {
+    const { handleSelect, connection } = this.props
+    handleSelect(connection)
   }
 
   render() {
-    var getClassNames = () => {
-      if (
-        this.props.selectedConnection &&
-        this.props.selectedConnection._id === this.props.connection._id
-      ) {
-        return 'list-group-item ListRow ListRowSelected'
-      } else {
-        return 'list-group-item ListRow'
-      }
-    }
-    const popoverClick = (
-      <Popover id="popover-trigger-click" title="Are you sure?">
-        <Button
-          bsStyle="danger"
-          onClick={this.onDelete}
-          style={{ width: '100%' }}
-        >
-          delete
-        </Button>
-      </Popover>
-    )
+    const { connection, selectedConnection } = this.props
+
+    const classNames =
+      selectedConnection && selectedConnection._id === connection._id
+        ? 'list-group-item ListRow ListRowSelected'
+        : 'list-group-item ListRow'
+
     return (
-      <li className={getClassNames()}>
+      <li className={classNames}>
         <h4>
-          <a href="#connection" onClick={this.onSelect}>
-            {this.props.connection.name}
+          <a href="#connection" onClick={this.handleConnectionClick}>
+            {connection.name}
           </a>
         </h4>
         <h5>
-          {this.props.connection.driver} {this.props.connection.host}/{this.props.connection.database}
+          {connection.driver} {connection.host}/{connection.database}
         </h5>
-        <OverlayTrigger
-          trigger="click"
-          placement="left"
-          container={this}
-          rootClose
-          overlay={popoverClick}
-        >
-          <a className="ListRowDeleteButton" href="#delete">
-            <Glyphicon glyph="trash" />
-          </a>
-        </OverlayTrigger>
+        <DeleteButton onClick={this.handleDeleteClick} />
       </li>
     )
   }
