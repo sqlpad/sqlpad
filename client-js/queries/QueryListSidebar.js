f2b5e1c86733258d76ee86b807cd0b52c88ee5c3
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-10-29 15:20:39 -0400

    Split queries components out

---
+++ client-js/queries/QueryListSidebar.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import Form from 'react-bootstrap/lib/Form'
+import FormGroup from 'react-bootstrap/lib/FormGroup'
+import FormControl from 'react-bootstrap/lib/FormControl'
+import ControlLabel from 'react-bootstrap/lib/ControlLabel'
+
+class QueryListSidebar extends React.Component {
+  onSearchChange = e => {
+    this.props.onSearchChange(e.target.value)
+  }
+
+  onConnectionChange = e => {
+    this.props.onConnectionChange(e.target.value)
+  }
+
+  onTagChange = e => {
+    this.props.onTagChange(e.target.value)
+  }
+
+  onCreatedByChange = e => {
+    this.props.onCreatedByChange(e.target.value)
+  }
+
+  onSortByChange = e => {
+    this.props.onSortByChange(e.target.value)
+  }
+
+  render() {
+    var connectionSelectOptions = this.props.connections.map(function(conn) {
+      return (
+        <option key={conn._id} value={conn._id}>
+          {conn.name}
+        </option>
+      )
+    })
+    var createdBySelectOptions = this.props.createdBys.map(function(createdBy) {
+      return (
+        <option key={createdBy} value={createdBy}>
+          {createdBy}
+        </option>
+      )
+    })
+    var tagSelectOptions = this.props.tags.map(function(tag) {
+      return (
+        <option key={tag} value={tag}>
+          {tag}
+        </option>
+      )
+    })
+    return (
+      <div className="QueryListSidebar">
+        <Form>
+          <FormGroup controlId="formControlsSelect">
+            <ControlLabel>Search</ControlLabel>
+            <FormControl type="text" onChange={this.onSearchChange} />
+          </FormGroup>
+          <br />
+          <FormGroup controlId="formControlsSelect">
+            <ControlLabel>Tag</ControlLabel>
+            <FormControl componentClass="select" onChange={this.onTagChange}>
+              <option value="">All</option>
+              {tagSelectOptions}
+            </FormControl>
+          </FormGroup>
+          <br />
+          <FormGroup controlId="formControlsSelect">
+            <ControlLabel>Connection</ControlLabel>
+            <FormControl
+              componentClass="select"
+              onChange={this.onConnectionChange}
+            >
+              <option value="">All</option>
+              {connectionSelectOptions}
+            </FormControl>
+          </FormGroup>
+          <br />
+          <FormGroup controlId="formControlsSelect">
+            <ControlLabel>Created By</ControlLabel>
+            <FormControl
+              value={this.props.selectedCreatedBy}
+              componentClass="select"
+              onChange={this.onCreatedByChange}
+            >
+              <option value="">All</option>
+              {createdBySelectOptions}
+            </FormControl>
+          </FormGroup>
+          <br />
+          <FormGroup controlId="formControlsSelect">
+            <ControlLabel>Sort By</ControlLabel>
+            <FormControl componentClass="select" onChange={this.onSortByChange}>
+              <option value="modifiedDate">Modified Date</option>
+              <option value="name">Name</option>
+            </FormControl>
+          </FormGroup>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default QueryListSidebar
---
+++ client-js/queries/QueryPreview.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import moment from 'moment'
+import ControlLabel from 'react-bootstrap/lib/ControlLabel'
+import chartDefinitions from '../components/ChartDefinitions.js'
+import SqlEditor from '../components/SqlEditor'
+
+class QueryPreview extends React.Component {
+  render() {
+    const { config, selectedQuery } = this.props
+    if (selectedQuery) {
+      const query = selectedQuery
+      const chartTypeLabel = () => {
+        const chartType =
+          query.chartConfiguration && query.chartConfiguration.chartType
+            ? query.chartConfiguration.chartType
+            : null
+
+        const chartDefinition = chartDefinitions.find(
+          def => def.chartType === chartType
+        )
+        return chartDefinition ? (
+          <h4>Chart: {chartDefinition.chartLabel}</h4>
+        ) : null
+      }
+      return (
+        <div className="QueryPreview">
+          <ControlLabel>Preview</ControlLabel>
+          <h4>{selectedQuery.name}</h4>
+          <SqlEditor
+            config={config}
+            height="70%"
+            readOnly
+            value={selectedQuery.queryText}
+          />
+          {chartTypeLabel()}
+          <h4>Modified: {moment(query.modifiedDate).calendar()}</h4>
+          <h4>Created By: {query.createdBy}</h4>
+        </div>
+      )
+    } else {
+      return <div className="QueryPreview" />
+    }
+  }
+}
+
+QueryPreview.propTypes = {
+  config: PropTypes.object.isRequired,
+  selectedQuery: PropTypes.object.isRequired
+}
+
+QueryPreview.defaultProps = {
+  selectedConnection: {}
+}
+
+export default QueryPreview
