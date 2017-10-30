bc53fef00837bb31515631e4c420fbacf93f4c98
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-10-29 23:20:09 -0400

    Use bootstrap from npm

--- client-js/index.js
+++ client-js/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
+import 'bootstrap/dist/css/bootstrap.css'
 import './css/react-select.css'
 import './css/s-alert-default.css'
 import './css/vendorOverrides.css'
