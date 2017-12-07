57bc726d5dd0a4e9a938eb62016cc27f87319ace
Rick Bergfalk <rick.bergfalk@gmail.com>
2017-12-07 08:56:21 -0500

    Tachyons the FullscreenMessage

--- client-js/common/FullscreenMessage.js
+++ client-js/common/FullscreenMessage.js
@@ -1,16 +1,7 @@
 import React from 'react'
 
-const style = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  position: 'absolute',
-  flexDirection: 'column',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  fontSize: 36
-}
-
-export default props => <div style={style}>{props.children}</div>
+export default props => (
+  <div className=" w-100 flex f1 flex-column items-center justify-center">
+    {props.children}
+  </div>
+)
