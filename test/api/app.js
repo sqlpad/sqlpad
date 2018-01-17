527431ec5fdf9e8fd97e94ee399e2e3c6644453f
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-01-16 22:39:55 -0500

    Checkpoint

--- test/api/app.js
+++ test/api/app.js
@@ -9,18 +9,42 @@ const expectedKeys = [
   'passport'
 ]
 
-describe('api/app', function() {
-  describe('get', function() {
-    it('returns expected values', function() {
-      return utils.get('/api/app').then(data => {
-        utils.expectKeys(data, expectedKeys)
-      })
-    })
+const { spawn } = require('child_process')
+const ls = spawn('node', [
+  'server.js',
+  '--dir',
+  './dbtest',
+  '--port',
+  '3010',
+  '--debug'
+])
 
-    it('handles unknown baseUrl', function() {
-      return utils.get('/sqlpad/api/app').then(data => {
-        utils.expectKeys(data, expectedKeys)
+ls.stdout.on('data', data => {
+  console.log(`stdout: ${data}`)
+})
+
+ls.stderr.on('data', data => {
+  console.log(`stderr: ${data}`)
+})
+
+ls.on('close', code => {
+  console.log(`child process exited with code ${code}`)
+})
+
+setTimeout(function() {
+  describe('api/app', function() {
+    describe('get', function() {
+      it('returns expected values', function() {
+        return utils.get('/api/app').then(data => {
+          utils.expectKeys(data, expectedKeys)
+        })
+      })
+
+      it('handles unknown baseUrl', function() {
+        return utils.get('/sqlpad/api/app').then(data => {
+          utils.expectKeys(data, expectedKeys)
+        })
       })
     })
   })
-})
+}, 4000)
