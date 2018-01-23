13a987a7b6006ebb95a956dadbe92c956042a258
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-01-22 20:05:26 -0500

    Use includes its clear

--- test/utils.js
+++ test/utils.js
@@ -2,6 +2,6 @@ const assert = require('assert')
 
 exports.expectKeys = function expectKeys(data, expectedKeys) {
   Object.keys(data).forEach(key =>
-    assert(expectedKeys.indexOf(key) > -1, `expected key ${key}`)
+    assert(expectedKeys.includes(key), `expected key ${key}`)
   )
 }
