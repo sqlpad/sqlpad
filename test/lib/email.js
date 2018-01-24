56acef1264881dfaed26d186ad0191f36a1c1204
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-01-23 22:05:09 -0500

    Add initial email test

---
+++ test/lib/email.js
@@ -0,0 +1,27 @@
+const email = require('../../lib/email.js')
+const config = require('../../lib/config.js')
+
+describe('lib/email.js', function() {
+  if (config.smtpConfigured() && process.env.SQLPAD_TEST_EMAIL) {
+    describe('#send()', function() {
+      it.skip('should send without error', function() {
+        // TODO test other kinds of emails
+        return email.send(
+          process.env.SQLPAD_TEST_EMAIL,
+          'test',
+          'test',
+          '<h1>test</h1>'
+        )
+      })
+    })
+  } else {
+    describe('#send() - set env vars to enable:', function() {
+      it.skip('SQLPAD_SMTP_HOST')
+      it.skip('SQLPAD_SMTP_USER')
+      it.skip('SQLPAD_SMTP_FROM')
+      it.skip('SQLPAD_SMTP_PORT')
+      it.skip('PUBLIC_URL')
+      it.skip('SQLPAD_TEST_EMAIL')
+    })
+  }
+})
