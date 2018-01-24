1164d3f8672f32ff2cff25f03b89afd9898e1ecb
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-01-23 23:38:30 -0500

    Add more instruction for future

--- test/lib/email.js
+++ test/lib/email.js
@@ -15,11 +15,13 @@ describe('lib/email.js', function() {
   } else {
     describe('Set env vars to enable:', function() {
       it.skip('SQLPAD_SMTP_HOST')
-      it.skip('SQLPAD_SMTP_USER')
-      it.skip('SQLPAD_SMTP_FROM')
       it.skip('SQLPAD_SMTP_PORT')
+      it.skip('SQLPAD_SMTP_USER')
+      it.skip('SQLPAD_SMTP_PASSWORD')
+      it.skip('SQLPAD_SMTP_FROM')
       it.skip('PUBLIC_URL')
       it.skip('SQLPAD_TEST_EMAIL')
+      it.skip('SQLPAD_SMTP_SECURE (opt. probably to false)')
     })
   }
 })
