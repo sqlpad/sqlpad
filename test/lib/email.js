31666e70d34cbd18880a6d117728c9f5493c848d
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-01-23 23:20:59 -0500

    Send the real emails during testing

--- test/lib/email.js
+++ test/lib/email.js
@@ -3,19 +3,17 @@ const config = require('../../lib/config.js')
 
 describe('lib/email.js', function() {
   if (config.smtpConfigured() && process.env.SQLPAD_TEST_EMAIL) {
-    describe('#send()', function() {
-      it('should send without error', function() {
-        // TODO test other kinds of emails
-        return email.send(
-          process.env.SQLPAD_TEST_EMAIL,
-          'test',
-          'test',
-          '<h1>test</h1>'
-        )
-      })
+    it('should send invites', function() {
+      return email.sendInvite(process.env.SQLPAD_TEST_EMAIL)
+    })
+    it('should send forgot passwords', function() {
+      return email.sendForgotPassword(
+        process.env.SQLPAD_TEST_EMAIL,
+        '/password-resset/id'
+      )
     })
   } else {
-    describe('#send() - set env vars to enable:', function() {
+    describe('Set env vars to enable:', function() {
       it.skip('SQLPAD_SMTP_HOST')
       it.skip('SQLPAD_SMTP_USER')
       it.skip('SQLPAD_SMTP_FROM')
