40cb359d6c48bad7d1166c1b7009aaea358303f7
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-01-28 18:01:46 -0500

    Add reset testing util

--- test/utils.js
+++ test/utils.js
@@ -1,7 +1,18 @@
 const assert = require('assert')
+const User = require('../models/User')
+const Query = require('../models/Query')
+const Connection = require('../models/Connection')
 
 exports.expectKeys = function expectKeys(data, expectedKeys) {
   Object.keys(data).forEach(key =>
     assert(expectedKeys.includes(key), `expected key ${key}`)
+  )
+}
+
+exports.reset = function reset() {
+  return Promise.all(
+    User._removeAll(),
+    Query._removeAll(),
+    Connection._removeAll()
   )
 }
