4f856dc98cd409a6da20abd1fa452fc5df4d3b8c
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-01-28 18:32:22 -0500

    Test user signup/signin

---
+++ test/api/signup-signin.js
@@ -0,0 +1,61 @@
+const assert = require('assert')
+const request = require('supertest')
+const app = require('../../app')
+const utils = require('../utils')
+
+describe('api signup/signin', function() {
+  before(function() {
+    return utils.reset()
+  })
+
+  describe('signup', function() {
+    it('allows new user signup', function() {
+      return request(app)
+        .post('/api/signup')
+        .send({
+          password: 'admin',
+          passwordConfirmation: 'admin',
+          email: 'admin@test.com'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(response => {
+          assert(!response.body.error, 'Expect no error')
+        })
+    })
+
+    it('prevents duplicate signups', function() {
+      return request(app)
+        .post('/api/signup')
+        .send({
+          password: 'admin',
+          passwordConfirmation: 'admin',
+          email: 'admin@test.com'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(response => {
+          assert(response.body.error, 'Expect error user already signed up')
+        })
+    })
+  })
+
+  describe('signin', function() {
+    it('signs in user', function() {
+      return request(app)
+        .post('/api/signin')
+        .send({
+          password: 'admin',
+          email: 'admin@test.com'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(response => {
+          assert(!response.body.error, 'Expect no error')
+        })
+    })
+  })
+
+  // TODO add signout
+  // TODO check for session once that is changed
+})
--- test/utils.js
+++ test/utils.js
@@ -3,16 +3,38 @@ const User = require('../models/User')
 const Query = require('../models/Query')
 const Connection = require('../models/Connection')
 
-exports.expectKeys = function expectKeys(data, expectedKeys) {
+function expectKeys(data, expectedKeys) {
   Object.keys(data).forEach(key =>
     assert(expectedKeys.includes(key), `expected key ${key}`)
   )
 }
 
-exports.reset = function reset() {
-  return Promise.all(
+function reset() {
+  return Promise.all([
     User._removeAll(),
     Query._removeAll(),
     Connection._removeAll()
-  )
+  ])
+}
+
+function resetWithUser() {
+  return reset().then(() => {
+    const user = new User({
+      email: 'admin@test.com',
+      password: 'admin',
+      role: 'admin'
+    })
+    return new Promise((resolve, reject) => {
+      user.save((err, newUser) => {
+        if (err) return reject(err)
+        return resolve(newUser)
+      })
+    })
+  })
+}
+
+module.exports = {
+  expectKeys,
+  reset,
+  resetWithUser
 }
