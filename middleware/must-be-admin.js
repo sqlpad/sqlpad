592ac54eab5044818efd28309e3174d8c88449d7
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-01-28 19:05:27 -0500

    Add postgres test-connection call

--- middleware/must-be-admin.js
+++ middleware/must-be-admin.js
@@ -1,7 +1,12 @@
-module.exports = function mustBeAdmin(req, res, next) {
-  if (req.user.role === 'admin') {
-    next()
-  } else {
-    next(new Error('You must be an admin to do that'))
+const mustBeAuthenticated = require('./must-be-authenticated')
+
+module.exports = [
+  mustBeAuthenticated,
+  function mustBeAdmin(req, res, next) {
+    if (req.user.role === 'admin') {
+      next()
+    } else {
+      next(new Error('You must be an admin to do that'))
+    }
   }
-}
+]
