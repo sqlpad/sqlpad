d41f428bb21517af034a392dd2d1505200c9c01c
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-02-01 21:22:18 -0500

    Only attempt http basic auth if headers are present

--- middleware/must-be-authenticated.js
+++ middleware/must-be-authenticated.js
@@ -1,9 +1,16 @@
-var passport = require('passport')
+const passport = require('passport')
+const config = require('../lib/config.js')
+const BASE_URL = config.get('baseUrl')
 
+// If authenticated continue
+// If not and auth header is present, try authenticated with http basic
+// Otherwise redirect user to signin
 module.exports = function mustBeAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next()
   }
-  passport.authenticate('basic', { session: false })(req, res, next)
-  // next(new Error('You must be logged in to do that'))
+  if (req.headers.authorization) {
+    return passport.authenticate('basic', { session: false })(req, res, next)
+  }
+  res.redirect(BASE_URL + '/signin')
 }
