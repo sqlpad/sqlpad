78fb6c65bed4267855290551d59ffd0565374a7f
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-02-09 00:19:14 -0500

    Fix paths

--- server/lib/get-version.js
+++ server/lib/get-version.js
@@ -1,4 +1,4 @@
-const packageJson = require('../package.json')
+const packageJson = require('../../package.json')
 const latestVersion = require('latest-version')
 const semverDiff = require('semver-diff')
 const _ = require('lodash')
