8861f463ef8f262a88c5c9386e9f0d1aa3ce5e9c
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-02-10 12:36:24 -0500

    Just default config

---
+++ server/lib/config/default.js
@@ -0,0 +1,22 @@
+/*
+  Gets default config values as defined in configuration definitions file
+  Some of these have special logic behind them, like dbPath
+*/
+const path = require('path')
+const definitions = require('../../resources/configItems')
+
+const userHome =
+  process.platform === 'win32' ? process.env.USERPROFILE : process.env.HOME
+const defaultDbPath = path.join(userHome, 'sqlpad/db')
+
+const defaultMap = {}
+
+definitions.forEach(definition => {
+  if (definition.key === 'dbPath') {
+    defaultMap.dbPath = defaultDbPath
+  } else if (definition.hasOwnProperty('default')) {
+    defaultMap[definition.key] = definition.default
+  }
+})
+
+module.exports = defaultMap
