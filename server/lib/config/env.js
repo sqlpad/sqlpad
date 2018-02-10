b5e016ebe8fe6c8629be061c59ba11d35eeae66e
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-02-10 12:36:13 -0500

    Just env config

---
+++ server/lib/config/env.js
@@ -0,0 +1,16 @@
+/*
+  Gets config values from environment vars as defined in configuration definitions file
+*/
+const definitions = require('../../resources/configItems')
+
+const envConfigMap = definitions
+  .filter(definition => definition.hasOwnProperty('envVar'))
+  .reduce((envMap, definition) => {
+    const { key, envVar } = definition
+    if (process.env[envVar]) {
+      envMap[key] = process.env[envVar]
+    }
+    return envMap
+  }, {})
+
+module.exports = envConfigMap
