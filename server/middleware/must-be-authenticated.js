a9c8beadb3be8caea8666444dc259f241cb4de01
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-02-11 13:45:34 -0500

    Use middleware config for auth middleware

--- server/middleware/must-be-authenticated.js
+++ server/middleware/must-be-authenticated.js
@@ -1,16 +1,15 @@
 const passport = require('passport')
-const config = require('../lib/config.js')
-const BASE_URL = config.get('baseUrl')
 
 // If authenticated continue
 // If not and auth header is present, try authenticated with http basic
 // Otherwise redirect user to signin
 module.exports = function mustBeAuthenticated(req, res, next) {
+  const { config } = req
   if (req.isAuthenticated()) {
     return next()
   }
   if (req.headers.authorization) {
     return passport.authenticate('basic', { session: false })(req, res, next)
   }
-  res.redirect(BASE_URL + '/signin')
+  res.redirect(config.get('baseUrl') + '/signin')
 }
