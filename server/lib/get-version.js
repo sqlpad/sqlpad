1b56ed32a37bfa031ec23f793249a58a61de8b98
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-02-11 19:29:24 -0500

    configUtil getHelper get-version.js

--- server/lib/get-version.js
+++ server/lib/get-version.js
@@ -2,7 +2,9 @@ const packageJson = require('../../package.json')
 const latestVersion = require('latest-version')
 const semverDiff = require('semver-diff')
 const _ = require('lodash')
-const config = require('./config.js')
+const db = require('./db.js')
+const configUtil = require('./config')
+
 const ONE_DAY = 1000 * 60 * 60 * 24
 
 const version = {
@@ -13,25 +15,33 @@ const version = {
 }
 
 function checkForUpdate() {
-  if (!config.get('disableUpdateCheck')) {
-    latestVersion(packageJson.name).then(npmVersion => {
-      version.latest = npmVersion
-      const difference = semverDiff(version.current, npmVersion)
-      if (difference) {
-        version.updateAvailable = true
-        version.updateType = difference
-        console.log('\n' + '='.repeat(70))
-        console.log('update available (%s)', version.updateType)
-        console.log(
-          'current version: %s      latest: %s',
-          version.curent,
-          version.latest
-        )
-        console.log("run 'npm i -g " + packageJson.name + "' to update.")
-        console.log('='.repeat(70) + '\n')
+  return configUtil
+    .getHelper(db)
+    .then(config => {
+      if (config.get('disableUpdateCheck')) {
+        return
       }
+      return latestVersion(packageJson.name).then(npmVersion => {
+        version.latest = npmVersion
+        const difference = semverDiff(version.current, npmVersion)
+        if (difference) {
+          version.updateAvailable = true
+          version.updateType = difference
+          console.log('\n' + '='.repeat(70))
+          console.log('update available (%s)', version.updateType)
+          console.log(
+            'current version: %s      latest: %s',
+            version.curent,
+            version.latest
+          )
+          console.log("run 'npm i -g " + packageJson.name + "' to update.")
+          console.log('='.repeat(70) + '\n')
+        }
+      })
     })
-  }
+    .catch(error => {
+      console.log(error)
+    })
 }
 
 setInterval(checkForUpdate, ONE_DAY)
