3404cd1970966ff3783ca42e1cfb17c3cc12c01b
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-02-11 20:36:32 -0500

    who knew config would be such a headache

--- server/routes/config-values.js
+++ server/routes/config-values.js
@@ -1,40 +1,42 @@
 const ConfigItem = require('../models/ConfigItem.js')
 const router = require('express').Router()
 const mustBeAdmin = require('../middleware/must-be-admin.js')
-const _ = require('lodash')
 
 router.get('/api/config-items', mustBeAdmin, function(req, res) {
-  let configItems = _.cloneDeep(ConfigItem.findAll())
-  configItems = configItems.map(function(item) {
-    if (item.sensitive && item.interface === 'env') {
-      item.effectiveValue = item.effectiveValue ? '**********' : ''
-      item.dbValue = item.dbValue ? '**********' : ''
-      item.default = item.default ? '**********' : ''
-      item.envValue = item.envValue ? '**********' : ''
-      item.cliValue = item.cliValue ? '**********' : ''
-      item.savedCliValue = item.savedCliValue ? '**********' : ''
-    }
-    return item
-  })
-  return res.json({
-    configItems: configItems
-  })
+  return ConfigItem.findAll()
+    .then(configItems => {
+      return res.json({
+        configItems: configItems.map(function(item) {
+          if (item.sensitive && item.interface === 'env') {
+            item.effectiveValue = item.effectiveValue ? '**********' : ''
+            item.dbValue = item.dbValue ? '**********' : ''
+            item.default = item.default ? '**********' : ''
+            item.envValue = item.envValue ? '**********' : ''
+            item.cliValue = item.cliValue ? '**********' : ''
+            item.savedCliValue = item.savedCliValue ? '**********' : ''
+          }
+          return item
+        })
+      })
+    })
+    .catch(error => {
+      return res.json({
+        error: 'Problem getting config items'
+      })
+    })
 })
 
 router.post('/api/config-values/:key', mustBeAdmin, function(req, res) {
   const key = req.params.key
   const value = req.body.value
-  const configItem = ConfigItem.findOneByKey(key)
-  configItem.setDbValue(value)
-  configItem.save(function(err) {
-    if (err) {
-      console.error(err)
+  ConfigItem.save(key, value)
+    .then(() => res.json({}))
+    .catch(error => {
+      console.error(error)
       return res.json({
         error: 'Problem saving config value'
       })
-    }
-    return res.json({})
-  })
+    })
 })
 
 module.exports = router
