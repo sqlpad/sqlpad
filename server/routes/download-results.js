f9e68031c8695c96468532cf216668dde041ad75
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-03-09 08:46:49 -0600

    Promises for routes/download-results

--- server/routes/download-results.js
+++ server/routes/download-results.js
@@ -1,53 +1,55 @@
-var fs = require('fs')
-var router = require('express').Router()
-var Cache = require('../models/Cache.js')
+const fs = require('fs')
+const router = require('express').Router()
+const Cache = require('../models/Cache.js')
 
 router.get('/download-results/:cacheKey.csv', function(req, res, next) {
   const { config } = req
-
   if (config.get('allowCsvDownload')) {
-    Cache.findOneByCacheKey(req.params.cacheKey, function(err, cache) {
-      if (err) {
-        console.error(err)
-        return next(err)
-      }
-      if (!cache) {
-        return next(new Error('Cache not found'))
-      }
-      var filename = cache.queryName + '.csv'
-      res.setHeader(
-        'Content-disposition',
-        'attachment; filename="' + encodeURIComponent(filename) + '"'
-      )
-      res.setHeader('Content-Type', 'text/csv')
-      fs.createReadStream(cache.csvFilePath()).pipe(res)
-    })
+    return Cache.findOneByCacheKey(req.params.cacheKey)
+      .then(cache => {
+        if (!cache) {
+          return next(new Error('Cache not found'))
+        }
+        var filename = cache.queryName + '.csv'
+        res.setHeader(
+          'Content-disposition',
+          'attachment; filename="' + encodeURIComponent(filename) + '"'
+        )
+        res.setHeader('Content-Type', 'text/csv')
+        fs.createReadStream(cache.csvFilePath()).pipe(res)
+      })
+      .catch(error => {
+        console.error(error)
+        // TODO figure out what this sends and set manually
+        return next(error)
+      })
   }
 })
 
 router.get('/download-results/:cacheKey.xlsx', function(req, res, next) {
   const { config } = req
-
   if (config.get('allowCsvDownload')) {
-    Cache.findOneByCacheKey(req.params.cacheKey, function(err, cache) {
-      if (err) {
-        console.error(err)
-        return next(err)
-      }
-      if (!cache) {
-        return next(new Error('Cache not found'))
-      }
-      var filename = cache.queryName + '.xlsx'
-      res.setHeader(
-        'Content-disposition',
-        'attachment; filename="' + encodeURIComponent(filename) + '"'
-      )
-      res.setHeader(
-        'Content-Type',
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-      )
-      fs.createReadStream(cache.xlsxFilePath()).pipe(res)
-    })
+    return Cache.findOneByCacheKey(req.params.cacheKey)
+      .then(cache => {
+        if (!cache) {
+          return next(new Error('Cache not found'))
+        }
+        var filename = cache.queryName + '.xlsx'
+        res.setHeader(
+          'Content-disposition',
+          'attachment; filename="' + encodeURIComponent(filename) + '"'
+        )
+        res.setHeader(
+          'Content-Type',
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+        fs.createReadStream(cache.xlsxFilePath()).pipe(res)
+      })
+      .catch(error => {
+        console.error(error)
+        // TODO figure out what this sends and set manually
+        return next(error)
+      })
   }
 })
 
