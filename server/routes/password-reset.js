116c9819c33f6f3bfda678c820fa205e10d93e81
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-03-11 12:41:07 -0600

    Promises password-reset route

--- server/routes/password-reset.js
+++ server/routes/password-reset.js
@@ -1,50 +1,35 @@
-var router = require('express').Router()
-var User = require('../models/User.js')
+const router = require('express').Router()
+const User = require('../models/User.js')
+const sendError = require('../lib/sendError')
 
 router.get('/api/password-reset/:passwordResetId', function(req, res) {
-  User.findOneByPasswordResetId(req.params.passwordResetId, function(
-    err,
-    user
-  ) {
-    if (err) {
-      console.error(err)
-      return res.json({ error: 'Problem querying user database' })
-    }
-    if (!user) {
-      return res.json({})
-    }
-    return res.json({ passwordResetId: req.params.passwordResetId })
-  })
+  return User.findOneByPasswordResetId(req.params.passwordResetId)
+    .then(user => {
+      if (!user) {
+        return res.json({})
+      }
+      return res.json({ passwordResetId: req.params.passwordResetId })
+    })
+    .catch(error => sendError(res, error, 'Problem querying user database'))
 })
 
 router.post('/api/password-reset/:passwordResetId', function(req, res) {
-  User.findOneByPasswordResetId(req.params.passwordResetId, function(
-    err,
-    user
-  ) {
-    if (err) {
-      console.error(err)
-      return res.json({ error: 'Problem querying user database' })
-    }
-    if (!user) {
-      return res.json({ error: 'Password reset permissions not found' })
-    }
-    if (req.body.email !== user.email) {
-      return res.json({ error: 'Incorrect email address' })
-    }
-    if (req.body.password !== req.body.passwordConfirmation) {
-      return res.json({ error: 'Passwords do not match' })
-    }
-    user.password = req.body.password
-    user.passwordResetId = ''
-    user.save(function(err) {
-      if (err) {
-        console.error(err)
-        return res.json({ error: 'Error saving update to DB' })
+  return User.findOneByPasswordResetId(req.params.passwordResetId)
+    .then(user => {
+      if (!user) {
+        return res.json({ error: 'Password reset permissions not found' })
       }
-      return res.json({})
+      if (req.body.email !== user.email) {
+        return res.json({ error: 'Incorrect email address' })
+      }
+      if (req.body.password !== req.body.passwordConfirmation) {
+        return res.json({ error: 'Passwords do not match' })
+      }
+      user.password = req.body.password
+      user.passwordResetId = ''
+      return user.save().then(() => res.json({}))
     })
-  })
+    .catch(error => sendError(res, error, 'Problem querying user database'))
 })
 
 module.exports = router
