9e8e79c498ad5535953941ec39c38c430d9ae120
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-03-11 12:29:25 -0600

    Promises forgot-password route

--- server/routes/forgot-password.js
+++ server/routes/forgot-password.js
@@ -2,44 +2,39 @@ const router = require('express').Router()
 const uuid = require('uuid')
 const User = require('../models/User.js')
 const email = require('../lib/email')
+const sendError = require('../lib/sendError')
 
 router.post('/api/forgot-password', function(req, res) {
   const { config } = req
 
   if (!req.body.email) {
-    return res.json({ error: 'Email address must be provided' })
+    return sendError(res, null, 'Email address must be provided')
   }
   if (!config.smtpConfigured()) {
-    return res.json({ error: 'Email must be configured' })
+    return sendError(res, null, 'Email must be configured')
   }
-  User.findOneByEmail(req.body.email, function(err, user) {
-    if (err) {
-      console.error(err)
-      return res.json({ error: 'Problem querying user database' })
-    }
-    // If user not found send success regardless
-    // This is not a user-validation service
-    if (!user) {
-      return res.json({})
-    }
 
-    user.passwordResetId = uuid.v4()
-    user.save(function(err) {
-      if (err) {
-        console.error(err)
-        return res.json({ error: 'Problem saving to user database' })
+  return User.findOneByEmail(req.body.email)
+    .then(user => {
+      // If user not found send success regardless
+      // This is not a user-validation service
+      if (!user) {
+        return res.json({})
       }
 
-      const resetPath = `/password-reset/${user.passwordResetId}`
+      user.passwordResetId = uuid.v4()
 
-      // Send email, but do not block response to client
-      email
-        .sendForgotPassword(req.body.email, resetPath)
-        .catch(error => console.error(error))
+      return user.save().then(() => {
+        const resetPath = `/password-reset/${user.passwordResetId}`
+        // Send email, but do not block response to client
+        email
+          .sendForgotPassword(req.body.email, resetPath)
+          .catch(error => console.error(error))
 
-      return res.json({})
+        return res.json({})
+      })
     })
-  })
+    .catch(error => sendError(res, error, 'Problem saving user'))
 })
 
 module.exports = router
