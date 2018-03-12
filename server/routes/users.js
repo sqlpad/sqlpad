7557bdda3a985b0051e5ac893bd0860bc9a3fb9a
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-03-11 21:45:57 -0600

    promise users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,110 +3,80 @@ const User = require('../models/User.js')
 const email = require('../lib/email')
 const mustBeAdmin = require('../middleware/must-be-admin.js')
 const mustBeAuthenticated = require('../middleware/must-be-authenticated.js')
+const sendError = require('../lib/sendError')
 
 router.get('/api/users/current', function(req, res) {
   if (req.isAuthenticated() && req.user) {
-    res.json({
+    return res.json({
       user: {
         _id: req.user.id,
         email: req.user.email,
         role: req.user.role
       }
     })
-  } else {
-    // respond with empty object since this isn't really an error
-    res.json({})
   }
+  // respond with empty object since this isn't really an error
+  return res.json({})
 })
 
 router.get('/api/users', mustBeAuthenticated, function(req, res) {
-  User.findAll(function(err, users) {
-    if (err) {
-      console.error(err)
-      return res.json({
-        error: 'Problem querying user database'
-      })
-    }
-    res.json({
-      users: users
-    })
-  })
+  return User.findAll()
+    .then(users => res.json({ users }))
+    .catch(error => sendError(res, error, 'Problem getting uers'))
 })
 
 // create/whitelist/invite user
 router.post('/api/users', mustBeAdmin, function(req, res) {
   const { config } = req
-  User.findOneByEmail(req.body.email, function(err, user) {
-    if (err) {
-      console.error(err)
-      return res.json({ error: 'Problem querying user database' })
-    }
-    if (user) {
-      return res.json({ error: 'User already exists' })
-    }
-    const newUser = new User({
-      email: req.body.email,
-      role: req.body.role
-    })
-    newUser.save(function(err, user) {
-      if (err) {
-        console.error(err.toString())
-        return res.json({
-          error: 'Problem saving user to database'
-        })
+  return User.findOneByEmail(req.body.email)
+    .then(user => {
+      if (user) {
+        return sendError(res, null, 'User already exists')
       }
-
-      if (config.smtpConfigured()) {
-        email.sendInvite(req.body.email).catch(error => console.error(error))
-      }
-      return res.json({})
+      const newUser = new User({
+        email: req.body.email,
+        role: req.body.role
+      })
+      return newUser.save().then(user => {
+        if (config.smtpConfigured()) {
+          email.sendInvite(req.body.email).catch(error => console.error(error))
+        }
+        return res.json({})
+      })
     })
-  })
+    .catch(error => sendError(res, error, 'Problem saving user'))
 })
 
 router.put('/api/users/:_id', mustBeAdmin, function(req, res) {
-  if (
-    req.user._id === req.params._id &&
-    req.user.role === 'admin' &&
-    req.body.role != null
-  ) {
-    return res.json({ error: "You can't unadmin yourself" })
+  const { params, body, user } = req
+  if (user._id === params._id && user.role === 'admin' && body.role != null) {
+    return sendError(res, null, "You can't unadmin yourself")
   }
-  User.findOneById(req.params._id, function(err, user) {
-    if (err) {
-      console.error(err)
-      return res.json({ error: 'Problem querying user database' })
-    }
-    if (!user) return res.json({ error: 'user not found' })
-    // this route could handle potentially different kinds of updates
-    // only update user properties that are explicitly provided in body
-    if (req.body.role != null) user.role = req.body.role
-    if (req.body.passwordResetId != null) {
-      user.passwordResetId = req.body.passwordResetId
-    }
-    user.save(function(err) {
-      if (err) {
-        console.error(err)
-        return res.json({ error: 'Problem saving user to database' })
+  return User.findOneById(params._id)
+    .then(user => {
+      if (!user) {
+        return sendError(res, null, 'user not found')
       }
-      return res.json({})
+      // this route could handle potentially different kinds of updates
+      // only update user properties that are explicitly provided in body
+      if (body.role != null) {
+        user.role = body.role
+      }
+      if (body.passwordResetId != null) {
+        user.passwordResetId = body.passwordResetId
+      }
+      return user.save().then(() => res.json({}))
     })
-  })
+    .catch(error => sendError(res, error, 'Problem saving user'))
 })
 
 router.delete('/api/users/:_id', mustBeAdmin, function(req, res) {
   if (req.user._id === req.params._id) {
-    return res.json({ error: "You can't delete yourself" })
+    return sendError(res, null, "You can't delete yourself")
   }
-  User.removeOneById(req.params._id, function(err) {
-    if (err) {
-      console.error(err)
-      return res.json({
-        error: 'Problem deleting user in database'
-      })
-    }
-    return res.json({})
-  })
+  return User.removeOneById(req.params._id)
+    .then(() => res.json({}))
+    .catch(error => sendError(res, error, 'Problem deleting user'))
 })
 
 module.exports = router
