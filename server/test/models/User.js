ba7a290bc30d6091e82695969bb4119ad6cd47b6
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-03-11 22:28:05 -0600

    Promises user test

--- server/test/models/User.js
+++ server/test/models/User.js
@@ -2,9 +2,9 @@ const assert = require('assert')
 const User = require('../../models/User.js')
 
 describe('models/User.js', function() {
-  const regularUser = new User({ email: 'regular@test.com', role: 'editor' })
-  const adminUser = new User({ email: 'admin@test.com', role: 'admin' })
-  const signedUpUser = new User({
+  let regularUser = new User({ email: 'regular@test.com', role: 'editor' })
+  let adminUser = new User({ email: 'admin@test.com', role: 'admin' })
+  let signedUpUser = new User({
     email: 'signedUp@test.com',
     role: 'editor',
     password: '1234'
@@ -15,99 +15,88 @@ describe('models/User.js', function() {
   })
 
   describe('.openAdminRegistration()', function() {
-    it('should return true if no admins exist', function(done) {
-      User.adminRegistrationOpen(function(err, open) {
-        assert.ifError(err)
+    it('should return true if no admins exist', function() {
+      return User.adminRegistrationOpen().then(open => {
         assert.equal(open, true, 'open=true')
-        done()
       })
     })
-    it('should return false if admin exist', function(done) {
-      adminUser.save(function(err) {
-        assert.ifError(err)
-        User.adminRegistrationOpen(function(err, open) {
-          assert.ifError(err)
-          assert.equal(open, false, 'open=false')
-          done()
+    it('should return false if admin exist', function() {
+      return adminUser
+        .save()
+        .then(user => {
+          adminUser = user
+          return User.adminRegistrationOpen()
         })
-      })
+        .then(open => {
+          assert.equal(open, false, 'open=false')
+        })
     })
   })
 
   describe('new User', function() {
-    it('should save without error', function(done) {
-      regularUser.save(function(err) {
-        assert.ifError(err)
-        adminUser.save(function(err) {
-          assert.ifError(err)
-          signedUpUser.save(done)
+    it('should save without error', function() {
+      return regularUser
+        .save()
+        .then(user => {
+          regularUser = user
+          return signedUpUser.save()
         })
-      })
+        .then(user => {
+          signedUpUser = user
+        })
     })
   })
 
   describe('.findAll()', function() {
-    it('should return all the existing users', function(done) {
-      User.findAll(function(err, users) {
-        assert.ifError(err)
+    it('should return all the existing users', function() {
+      return User.findAll().then(users => {
         assert.equal(users.length, 3, 'users.length')
         assert(users[0] instanceof User, 'instanceof User')
-        done()
       })
     })
   })
 
   describe('.findOneByEmail()', function() {
-    it('should return requested user', function(done) {
-      User.findOneByEmail('admin@test.com', function(err, user) {
-        assert.ifError(err)
+    it('should return requested user', function() {
+      return User.findOneByEmail('admin@test.com').then(user => {
         assert(user instanceof User, 'instanceof User')
         assert.equal(user.email, 'admin@test.com', 'user.email')
         assert.equal(user.role, 'admin', 'user.role')
-        done()
       })
     })
   })
 
   describe('new User', function() {
-    it('should save without error', function(done) {
+    it('should save without error', function() {
       const user = new User({ email: '2@test.com' })
-      user.save(done)
+      return user.save()
     })
-    it('should have defaults populated', function(done) {
-      User.findOneByEmail('regular@test.com', function(err, user) {
-        assert.ifError(err)
+    it('should have defaults populated', function() {
+      return User.findOneByEmail('regular@test.com').then(user => {
         assert(user)
         assert(user.email)
         assert.equal(user.role, 'editor', 'user.role')
         assert(user instanceof User, 'instanceOf User')
         assert(user.createdDate instanceof Date)
         assert(user.modifiedDate instanceof Date)
-        done()
       })
     })
   })
 
   describe('.comparePasswordToHash()', function() {
-    it('should return true if password is a match', function(done) {
-      User.findOneByEmail('signedUp@test.com', function(err, user) {
-        assert.ifError(err)
-        user.comparePasswordToHash('1234', function(err, isMatch) {
-          assert.ifError(err)
+    it('should return true if password is a match', function() {
+      return User.findOneByEmail('signedUp@test.com')
+        .then(user => user.comparePasswordToHash('1234'))
+        .then(isMatch => {
           assert.equal(isMatch, true, 'isMatch')
-          done()
         })
-      })
     })
-    it('should return false if password is not a match', function(done) {
-      User.findOneByEmail('signedUp@test.com', function(err, user) {
-        assert.ifError(err)
-        user.comparePasswordToHash('wrongpassword', function(err, isMatch) {
-          assert.ifError(err)
+    it('should return false if password is not a match', function() {
+      return User.findOneByEmail('signedUp@test.com')
+        .then(user => user.comparePasswordToHash('wrongpassword'))
+        .then(isMatch => {
           assert.equal(isMatch, false, 'isMatch')
-          done()
         })
-      })
     })
   })
 })
