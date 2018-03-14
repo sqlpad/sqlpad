7e35b3ec553db28ebf3dbd1401cedd1380bd5a90
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-03-14 15:50:07 -0600

    promises homepage route

--- server/routes/homepage.js
+++ server/routes/homepage.js
@@ -1,5 +1,6 @@
-var router = require('express').Router()
-var Connection = require('../models/Connection.js')
+const router = require('express').Router()
+const Connection = require('../models/Connection.js')
+const sendError = require('../lib/sendError')
 
 // TODO FIXME - This was meant to redirect user to appropriate page depending on state of setup
 // I do not think it works anymore and should be revisited (this can be done client-side too)
@@ -7,19 +8,17 @@ router.get('/', function(req, res, next) {
   const { config } = req
   const BASE_URL = config.get('baseUrl')
 
-  Connection.findAll(function(err, connections) {
-    if (err) {
-      console.error(err)
-      return next(err)
-    }
-    if (!req.user) {
-      return res.redirect(BASE_URL + '/signin')
-    }
-    if (connections.length === 0 && req.user.role === 'admin') {
-      return res.redirect(BASE_URL + '/connections')
-    }
-    res.redirect(BASE_URL + '/queries')
-  })
+  return Connection.findAll()
+    .then(connections => {
+      if (!req.user) {
+        return res.redirect(BASE_URL + '/signin')
+      }
+      if (connections.length === 0 && req.user.role === 'admin') {
+        return res.redirect(BASE_URL + '/connections')
+      }
+      return res.redirect(BASE_URL + '/queries')
+    })
+    .catch(error => sendError(res, error))
 })
 
 module.exports = router
