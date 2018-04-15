dda80cb1d789bcb23a16374b08499c27bde6eb6f
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-04-15 15:15:06 -0400

    Update mssql to 4.1.0 & test (#345)
    * use rimraf to clean before test
    
    * move sqlserver driver to folder
    
    * Remove unnecessary postgres param
    
    * Add sqlserver tests
    
    * Udpate mssql to 4.1
    
    * Update sqlserver implementation for mssql 4.1
    
    * Update runQuery comments

--- server/drivers/sqlserver/index.js
+++ server/drivers/sqlserver/index.js
@@ -1,5 +1,5 @@
 const mssql = require('mssql')
-const { formatSchemaQueryResults } = require('./utils')
+const { formatSchemaQueryResults } = require('../utils')
 
 const id = 'sqlserver'
 const name = 'SQL Server'
@@ -28,17 +28,23 @@ const SCHEMA_SQL = `
  * @param {object} connection
  */
 function runQuery(query, connection) {
-  const sqlconfig = {
+  const config = {
     user: connection.username,
     password: connection.password,
     server: connection.host,
     port: connection.port ? connection.port : 1433,
     database: connection.database,
     domain: connection.domain,
-    stream: true,
-    requestTimeout: 1000 * 60 * 60, // one hour
+    // Set timeout to 1 hour for long running query support
+    requestTimeout: 1000 * 60 * 60,
     options: {
+      appName: 'SQLPad',
       encrypt: connection.sqlserverEncrypt
+    },
+    pool: {
+      max: 1,
+      min: 0,
+      idleTimeoutMillis: 1000
     }
   }
 
@@ -46,71 +52,55 @@ function runQuery(query, connection) {
   const rows = []
 
   return new Promise((resolve, reject) => {
-    const mssqlConnection = new mssql.Connection(sqlconfig, err => {
+    const pool = new mssql.ConnectionPool(config, err => {
       if (err) {
         return reject(err)
       }
-      let rowCounter = 0
-      let queryError
-      let resultsSent = false
-      let tooManyHandled = false
 
-      // For SQL Server, this can be called more than once safely
-      const continueOn = function() {
-        if (!resultsSent) {
-          resultsSent = true
-          if (queryError) {
-            return reject(queryError)
-          }
-          return resolve({ rows, incomplete })
-        }
-      }
-
-      const request = new mssql.Request(mssqlConnection)
+      const request = new mssql.Request(pool)
+      // Stream set a config level doesn't seem to work
+      request.stream = true
       request.query(query)
 
-      request.on('row', function(row) {
-        // special handling if columns were not given names
+      request.on('row', row => {
+        // Special handling if columns were not given names
         if (row[''] && row[''].length) {
           for (let i = 0; i < row[''].length; i++) {
             row['UNNAMED COLUMN ' + (i + 1)] = row[''][i]
           }
           delete row['']
         }
-        rowCounter++
-        if (rowCounter <= connection.maxRows) {
-          // if we haven't hit the max yet add row to results
-          rows.push(row)
-        } else {
-          if (!tooManyHandled) {
-            tooManyHandled = true
-            // Too many rows!
-            incomplete = true
-            continueOn()
-            console.log('Row limit hit - Attempting to cancel query...')
-            request.cancel() // running this will yeild a cancel error
-          }
+        if (rows.length < connection.maxRows) {
+          return rows.push(row)
+        }
+        // If reached it means we received a row event for more than maxRows
+        // If we haven't flagged incomplete yet, flag it,
+        // Resolve what we have and cancel request
+        // Note that this will yield a cancel error
+        if (!incomplete) {
+          incomplete = true
+          resolve({ rows, incomplete })
+          request.cancel()
         }
       })
 
-      request.on('error', function(err) {
-        // May be emitted multiple times
-        // for now I guess we just set queryError to be the most recent error?
-        if (err.code === 'ECANCEL') {
-          console.log('Query cancelled successfully')
-        } else {
-          console.log('mssql query error:')
-          console.log(err)
-          queryError = err
+      // Error events may fire multiple times
+      // If we get an ECANCEL error and too many rows were handled it was intentional
+      request.on('error', err => {
+        if (err.code === 'ECANCEL' && incomplete) {
+          return
         }
+        return reject(err)
       })
 
-      request.on('done', function(returnValue) {
-        // Always emitted as the last one
-        continueOn()
-        mssqlConnection.close() // I don't think this does anything using the tedious driver. but maybe someday it will
+      // Always emitted as the last one
+      request.on('done', () => {
+        resolve({ rows, incomplete })
+        pool.close()
       })
     })
+
+    pool.on('error', err => reject(err))
   })
 }
 
--- server/drivers/sqlserver/test.sh
+++ server/drivers/sqlserver/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose down
 docker-compose up -d
-sleep 5
+sleep 15
 npx mocha ./test.js
 docker-compose down
