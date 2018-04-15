a577f881c65871f7995500d6d03ec16d394fc18c
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-04-15 15:57:53 -0400

    Use cdn for bootstrap css/fonts (#346)

--- client-js/index.js
+++ client-js/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
-import 'bootstrap/dist/css/bootstrap.css'
 import './css/react-select.css'
 import './css/s-alert-default.css'
 import './css/react-split-pane.css'
