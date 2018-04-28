50ef6f6b4c8deaf26d0584bd6f8e7038bb1a2441
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-04-28 13:01:51 -0400

    Implement data table using ReactVirtualized table
    TODO - explore what a grid is like
    TODO - fix widths to be percentage of gridWidth

--- client-js/common/QueryResultDataTable.js
+++ client-js/common/QueryResultDataTable.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Table, Column, Cell } from 'fixed-data-table-2'
+import { Column, Table } from 'react-virtualized'
+import Draggable from 'react-draggable'
 import SpinKitCube from './SpinKitCube.js'
 import moment from 'moment'
-import 'fixed-data-table-2/dist/fixed-data-table.css'
+import 'react-virtualized/styles.css' // only needs to be imported once
 
 const renderValue = (input, fieldMeta) => {
   if (input === null || input === undefined) {
@@ -38,13 +39,31 @@ class QueryResultDataTable extends React.PureComponent {
     }
   }
 
-  handleColumnResizeEnd = (newColumnWidth, columnKey) => {
-    this.setState(({ columnWidths }) => ({
-      columnWidths: {
-        ...columnWidths,
-        [columnKey]: newColumnWidth
-      }
-    }))
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const { queryResult } = nextProps
+    const { columnWidths } = prevState
+
+    if (queryResult && queryResult.fields) {
+      queryResult.fields.forEach(field => {
+        if (!columnWidths[field]) {
+          const fieldMeta = queryResult.meta[field]
+          let valueLength = fieldMeta.maxValueLength
+
+          if (field.length > valueLength) {
+            valueLength = field.length
+          }
+          let columnWidthGuess = valueLength * 20
+          if (columnWidthGuess < 200) {
+            columnWidthGuess = 200
+          } else if (columnWidthGuess > 350) {
+            columnWidthGuess = 350
+          }
+
+          columnWidths[field] = columnWidthGuess
+        }
+      })
+    }
+    return { columnWidths }
   }
 
   componentDidMount() {
@@ -56,8 +75,121 @@ class QueryResultDataTable extends React.PureComponent {
     window.removeEventListener('resize', this.handleResize)
   }
 
+  renderColumns() {
+    const { queryResult } = this.props
+    const { columnWidths } = this.state
+
+    return queryResult.fields.map(field => {
+      return (
+        <Column
+          headerRenderer={this.headerRenderer}
+          cellRenderer={this.cellRenderer}
+          key={field}
+          dataKey={field}
+          label={field}
+          width={columnWidths[field]}
+        />
+      )
+    })
+  }
+
+  headerRenderer = ({
+    columnData,
+    dataKey,
+    disableSort,
+    label,
+    sortBy,
+    sortDirection
+  }) => {
+    return (
+      <React.Fragment key={dataKey}>
+        <div className="ReactVirtualized__Table__headerTruncatedText">
+          {label}
+        </div>
+        <Draggable
+          axis="x"
+          defaultClassName="DragHandle"
+          defaultClassNameDragging="DragHandleActive"
+          onDrag={(event, { deltaX }) =>
+            this.resizeRow({
+              dataKey,
+              deltaX
+            })
+          }
+          position={{ x: 0 }}
+          zIndex={999}
+        >
+          <span className="DragHandleIcon">⋮</span>
+        </Draggable>
+      </React.Fragment>
+    )
+  }
+
+  cellRenderer = ({
+    cellData,
+    columnData,
+    columnIndex,
+    dataKey,
+    isScrolling,
+    rowData,
+    rowIndex
+  }) => {
+    const { queryResult } = this.props
+    const fieldMeta = queryResult.meta[dataKey]
+
+    const value = rowData[dataKey]
+    let barStyle
+    let numberBar
+    if (fieldMeta.datatype === 'number') {
+      const valueNumber = Number(value)
+      const range = fieldMeta.max - (fieldMeta.min < 0 ? fieldMeta.min : 0)
+      let left = 0
+      if (fieldMeta.min < 0 && valueNumber < 0) {
+        left = Math.abs(fieldMeta.min - valueNumber) / range * 100 + '%'
+      } else if (fieldMeta.min < 0 && valueNumber >= 0) {
+        left = Math.abs(fieldMeta.min) / range * 100 + '%'
+      }
+      barStyle = {
+        position: 'absolute',
+        left: left,
+        bottom: 0,
+        height: '2px',
+        width: Math.abs(valueNumber) / range * 100 + '%',
+        backgroundColor: '#555'
+      }
+      numberBar = <div style={barStyle} />
+    }
+    return (
+      <div className="relative" style={{ lineHeight: '30px' }}>
+        {numberBar}
+        <div style={{ whiteSpace: 'nowrap' }}>
+          {renderValue(value, fieldMeta)}
+        </div>
+      </div>
+    )
+  }
+
+  rowClassName = ({ index }) => {
+    return index % 2 === 0 ? 'bg-near-white' : ''
+  }
+
+  resizeRow = ({ dataKey, deltaX }) => {
+    this.setState(prevState => {
+      const prevWidths = prevState.columnWidths
+      return {
+        columnWidths: {
+          ...prevWidths,
+          [dataKey]: prevWidths[dataKey] + deltaX
+        }
+      }
+    })
+  }
+
   render() {
-    if (this.props.isRunning) {
+    const { isRunning, queryError, queryResult } = this.props
+    const { gridHeight, gridWidth } = this.state
+
+    if (isRunning) {
       return (
         <div
           id="result-grid"
@@ -66,95 +198,38 @@ class QueryResultDataTable extends React.PureComponent {
           <SpinKitCube />
         </div>
       )
-    } else if (this.props.queryError) {
+    }
+
+    if (queryError) {
       return (
         <div
           id="result-grid"
           className={`aspect-ratio--object flex items-center justify-center f2 pa4 tc bg-light-red`}
         >
-          {this.props.queryError}
+          {queryError}
         </div>
       )
-    } else if (this.props.queryResult && this.props.queryResult.rows) {
-      const { columnWidths } = this.state
-      const queryResult = this.props.queryResult
-      const columnNodes = queryResult.fields.map(function(field) {
-        const fieldMeta = queryResult.meta[field]
-        let valueLength = fieldMeta.maxValueLength
+    }
 
-        if (field.length > valueLength) {
-          valueLength = field.length
-        }
-        let columnWidthGuess = valueLength * 20
-        if (columnWidthGuess < 200) {
-          columnWidthGuess = 200
-        } else if (columnWidthGuess > 350) {
-          columnWidthGuess = 350
-        }
-
-        const columnWidth = columnWidths[field] || columnWidthGuess
-
-        return (
-          <Column
-            columnKey={field}
-            key={field}
-            isResizable
-            header={<Cell>{field}</Cell>}
-            cell={({ rowIndex }) => {
-              const value = queryResult.rows[rowIndex][field]
-              let barStyle
-              let numberBar
-              if (fieldMeta.datatype === 'number') {
-                const valueNumber = Number(value)
-                const range =
-                  fieldMeta.max - (fieldMeta.min < 0 ? fieldMeta.min : 0)
-                let left = 0
-                if (fieldMeta.min < 0 && valueNumber < 0) {
-                  left =
-                    Math.abs(fieldMeta.min - valueNumber) / range * 100 + '%'
-                } else if (fieldMeta.min < 0 && valueNumber >= 0) {
-                  left = Math.abs(fieldMeta.min) / range * 100 + '%'
-                }
-                barStyle = {
-                  position: 'absolute',
-                  left: left,
-                  bottom: 0,
-                  height: '2px',
-                  width: Math.abs(valueNumber) / range * 100 + '%',
-                  backgroundColor: '#555'
-                }
-                numberBar = <div style={barStyle} />
-              }
-              return (
-                <Cell>
-                  {numberBar}
-                  <div style={{ whiteSpace: 'nowrap' }}>
-                    {renderValue(value, fieldMeta)}
-                  </div>
-                </Cell>
-              )
-            }}
-            width={columnWidth}
-          />
-        )
-      })
+    if (queryResult && queryResult.rows) {
       return (
         <div id="result-grid" className="aspect-ratio--object">
           <Table
-            rowHeight={30}
-            rowsCount={queryResult.rows.length}
-            width={this.state.gridWidth}
-            height={this.state.gridHeight}
+            width={gridWidth}
+            height={gridHeight}
             headerHeight={30}
-            onColumnResizeEndCallback={this.handleColumnResizeEnd}
+            rowHeight={30}
+            rowCount={queryResult.rows.length}
+            rowGetter={({ index }) => queryResult.rows[index]}
+            rowClassName={this.rowClassName}
           >
-            {columnNodes}
+            {this.renderColumns()}
           </Table>
         </div>
       )
-    } else {
-      return <div id="result-grid" className="aspect-ratio--object" />
     }
+
+    return <div id="result-grid" className="aspect-ratio--object" />
   }
 }
 
