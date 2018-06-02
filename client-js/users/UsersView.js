aee0df7f6ca3f00be6bd44da20f7f6a2a20f5bdd
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-06-02 12:08:54 -0400

    Use antd for table

--- client-js/users/UsersView.js
+++ client-js/users/UsersView.js
@@ -3,10 +3,15 @@ import fetchJson from '../utilities/fetch-json.js'
 import uuid from 'uuid'
 import message from 'antd/lib/message'
 import InviteUserForm from './InviteUserForm'
-import SimpleTable from '../common/SimpleTable'
-import SimpleTh from '../common/SimpleTableTh'
-import UserListRow from './UserListRow'
 import Modal from '../common/Modal'
+import moment from 'moment'
+import { Link } from 'react-router-dom'
+
+import Table from 'antd/lib/table'
+import 'antd/lib/table/style/css'
+
+import Popconfirm from 'antd/lib/popconfirm'
+import 'antd/lib/popconfirm/style/css'
 
 import Layout from 'antd/lib/layout'
 import 'antd/lib/layout/style/css'
@@ -14,7 +19,12 @@ import 'antd/lib/layout/style/css'
 import Button from 'antd/lib/button'
 import 'antd/lib/button/style/css'
 
+import Select from 'antd/lib/select'
+import 'antd/lib/select/style/css'
+
 const { Header, Content } = Layout
+const { Column } = Table
+const { Option } = Select
 
 class UsersView extends React.Component {
   state = {
@@ -93,9 +103,100 @@ class UsersView extends React.Component {
     this.setState({ showAddUser: false })
   }
 
+  createdRender = (text, record) => {
+    return !record.signupDate ? (
+      <em>not signed up yet</em>
+    ) : (
+      moment(record.signupDate).calendar()
+    )
+  }
+
+  roleRender = (text, record) => {
+    const { currentUser } = this.props
+    return (
+      <Select
+        className="w5"
+        name="role"
+        disabled={currentUser._id === record._id}
+        value={record.role || ''}
+        onChange={value => {
+          record.role = value
+          return this.updateUserRole(record)
+        }}
+      >
+        <Option value="editor">Editor</Option>
+        <Option value="admin">Admin</Option>
+      </Select>
+    )
+  }
+
+  resetButtonRender = (text, record) => {
+    if (record.passwordResetId) {
+      return (
+        <span>
+          <Button
+            className="w5 mr3"
+            onClick={() => this.removePasswordResetLink(record)}
+          >
+            Remove
+          </Button>
+          <Link className="w5" to={'/password-reset/' + record.passwordResetId}>
+            Reset Link
+          </Link>
+        </span>
+      )
+    }
+    return (
+      <Button
+        className="w5"
+        onClick={() => this.generatePasswordResetLink(record)}
+      >
+        Generate Link
+      </Button>
+    )
+  }
+
+  renderTable() {
+    const { users } = this.state
+    return (
+      <Table
+        locale={{ emptyText: 'No users found' }}
+        dataSource={users}
+        pagination={false}
+        className="w-100"
+      >
+        <Column title="Email" key="email" dataIndex="email" />
+        <Column title="Role" key="role" render={this.roleRender} />
+        <Column title="Created" key="created" render={this.createdRender} />
+        <Column
+          title="Password reset"
+          key="resetButton"
+          render={this.resetButtonRender}
+        />
+        <Column
+          title="Delete"
+          key="delete"
+          render={(text, record) => {
+            return (
+              <Popconfirm
+                title="Delete user?"
+                onConfirm={e => this.handleDelete(record)}
+                onCancel={() => {}}
+                okText="Yes"
+                cancelText="No"
+              >
+                <Button icon="delete" type="danger" />
+              </Popconfirm>
+            )
+          }}
+        />
+      </Table>
+    )
+  }
+
   render() {
-    const { config, currentUser } = this.props
-    const { users, showAddUser } = this.state
+    const { config } = this.props
+    const { showAddUser } = this.state
 
     return (
       <Layout
@@ -114,37 +215,7 @@ class UsersView extends React.Component {
           </div>
         </Header>
         <Content className="ma4">
-          <div className="bg-white">
-            <SimpleTable
-              className="w-100"
-              renderHeader={() => {
-                return (
-                  <tr>
-                    <SimpleTh>Email</SimpleTh>
-                    <SimpleTh>Role</SimpleTh>
-                    <SimpleTh>Created</SimpleTh>
-                    <SimpleTh>Password reset</SimpleTh>
-                    <SimpleTh>Delete</SimpleTh>
-                  </tr>
-                )
-              }}
-              renderBody={() =>
-                users.map(user => {
-                  return (
-                    <UserListRow
-                      key={user._id}
-                      user={user}
-                      currentUser={currentUser}
-                      handleDelete={this.handleDelete}
-                      updateUserRole={this.updateUserRole}
-                      generatePasswordResetLink={this.generatePasswordResetLink}
-                      removePasswordResetLink={this.removePasswordResetLink}
-                    />
-                  )
-                })
-              }
-            />
-          </div>
+          <div className="bg-white">{this.renderTable()}</div>
           <Modal
             title="New user"
             show={showAddUser}
