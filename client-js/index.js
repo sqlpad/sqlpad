df4e7c8c557de97f0fff86a8f9624f61980733f4
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-06-03 14:35:30 -0400

    Remove old vendor overrides

--- client-js/index.js
+++ client-js/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
-import './css/react-select.css'
-import './css/s-alert-default.css'
 import './css/react-split-pane.css'
 import './css/vendorOverrides.css'
 import './css/index.css'
