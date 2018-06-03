11e01fba4cba45d941c6c6807dc7df8a362560fa
Rick Bergfalk <rick.bergfalk@gmail.com>
2018-06-02 21:36:51 -0400

    Use antd table for env config documentation

--- client-js/configuration/ConfigEnvDocumentation.js
+++ client-js/configuration/ConfigEnvDocumentation.js
@@ -1,72 +1,51 @@
 import React from 'react'
-import Col from 'react-bootstrap/lib/Col'
-import ControlLabel from 'react-bootstrap/lib/ControlLabel'
-import Form from 'react-bootstrap/lib/Form'
-import FormControl from 'react-bootstrap/lib/FormControl'
-import HelpBlock from 'react-bootstrap/lib/HelpBlock'
-import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger'
-import Popover from 'react-bootstrap/lib/Popover'
-import Row from 'react-bootstrap/lib/Row'
+
+import Table from 'antd/lib/table'
+import 'antd/lib/table/style/css'
+
+const { Column } = Table
 
 class ConfigEnvDocumentation extends React.Component {
-  render() {
-    var configNodes = this.props.configItems
-      .filter(config => config.interface === 'env')
-      .map(function(config) {
-        var defaultValue = () => {
-          if (config.default === '') {
-            return <em style={{ color: '#999' }}>empty</em>
-          }
-          return <span>{config.default.toString()}</span>
-        }
-        var currentValue =
-          config.value === '' ? '<empty>' : config.effectiveValue.toString()
-        var cliFlag =
-          config.cliFlag && config.cliFlag.pop
-            ? config.cliFlag.pop()
-            : config.cliFlag
-        var helpPopover = (
-          <Popover id="popover-trigger-focus" title={config.envVar}>
-            <HelpBlock>
-              <p>{config.description}</p>
-              <p>
-                <strong>Default:</strong> {defaultValue()}
-              </p>
-              {cliFlag ? (
-                <p>
-                  <strong>CLI Flag:</strong> --{cliFlag}
-                </p>
-              ) : null}
-              <p>
-                <strong>Environment Variable:</strong> {config.envVar}
-              </p>
-              <p>
-                <strong>Set By:</strong> {config.effectiveValueSource}
-              </p>
-            </HelpBlock>
-          </Popover>
-        )
-        return (
-          <Row key={config.key} style={{ marginTop: 30 }}>
-            <Col componentClass={ControlLabel} sm={6}>
-              {config.envVar}
-            </Col>
-            <Col sm={6}>
-              <OverlayTrigger
-                trigger={['hover', 'focus']}
-                placement="right"
-                overlay={helpPopover}
-              >
-                <FormControl type="text" value={currentValue} disabled />
-              </OverlayTrigger>
-            </Col>
-          </Row>
-        )
-      })
+  renderValue = (text, record) => {
+    return record.value === '' ? '<empty>' : record.effectiveValue.toString()
+  }
+
+  renderInfo = (text, record) => {
     return (
-      <Form horizontal style={{ marginBottom: 50 }}>
-        {configNodes}
-      </Form>
+      <div style={{ width: '200px' }}>
+        <p>{record.description}</p>
+      </div>
+    )
+  }
+
+  renderCli = (text, record) => {
+    const cliFlag =
+      record.cliFlag && record.cliFlag.pop
+        ? record.cliFlag.pop()
+        : record.cliFlag
+    if (cliFlag) {
+      return '--' + cliFlag
+    }
+  }
+
+  render() {
+    const filteredConfigItems = this.props.configItems.filter(
+      config => config.interface === 'env'
+    )
+
+    return (
+      <Table
+        className="bg-white w-100"
+        locale={{ emptyText: 'No connections found' }}
+        dataSource={filteredConfigItems}
+        pagination={false}
+      >
+        <Column title="Environment variable" key="envVar" dataIndex="envVar" />
+        <Column title="Value" key="value" render={this.renderValue} />
+        <Column title="CLI flag" key="cli" render={this.renderCli} />
+        <Column title="Set by" key="setby" dataIndex="effectiveValueSource" />
+        <Column title="Info" key="info" render={this.renderInfo} />
+      </Table>
     )
   }
 }
