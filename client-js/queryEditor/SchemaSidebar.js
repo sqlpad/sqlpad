3d15369e766b7c9e92db62399822322a9094245d
greigwise <fritalian@gmail.com>
2018-10-16 22:27:50 -0400

    Add column descriptions to schema sidebar for Postgres (#386)
    * Add column descriptions to schema sidebar for Postgres
    
    * Change variable name
    
    * Refactor <li> block

--- client-js/queryEditor/SchemaSidebar.js
+++ client-js/queryEditor/SchemaSidebar.js
@@ -224,6 +224,7 @@ class SchemaInfoTableItem extends React.Component {
               {...this.props}
               column_name={column.column_name}
               data_type={column.data_type}
+              column_description={column.column_description}
               key={column.column_name}
               schema={schema}
               table={table}
@@ -313,7 +314,14 @@ class SchemaInfoColumnItem extends React.Component {
 
   render() {
     const { copyButtonText, showCopyButton } = this.state
-    const { config, column_name, data_type, schema, table } = this.props
+    const {
+      config,
+      column_name,
+      data_type,
+      column_description,
+      schema,
+      table
+    } = this.props
     const copyButtonClassName = showCopyButton
       ? 'right-2 pointer absolute bg-black hover-bg-hot-pink label label-info'
       : 'right-2 pointer absolute bg-black hover-bg-hot-pink label label-info dn'
@@ -335,6 +343,8 @@ class SchemaInfoColumnItem extends React.Component {
         )
       }
     }
+
+    const description = column_description && ` - ${column_description}`
     return (
       <li className="list">
         <span
@@ -345,6 +355,7 @@ class SchemaInfoColumnItem extends React.Component {
         >
           {column_name}
           <span className="silver"> ({data_type})</span>
+          {description}
           {getCopyToClipboard()}
         </span>
       </li>
