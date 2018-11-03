8a1bef8e9122bda7e9e1e0a22c41cd841f5cff50
Rick Bergfalk <rbergfalk@uptycs.com>
2018-11-02 23:18:59 -0400

    Use unstated provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
+import { Provider } from 'unstated'
 import './css/react-split-pane.css'
 import './css/vendorOverrides.css'
 import './css/index.css'
@@ -8,4 +9,9 @@ import 'tachyons/css/tachyons.min.css'
 
 import 'antd/lib/message/style/css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
