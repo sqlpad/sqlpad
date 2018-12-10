8a38454020ad478df0d72e001a213e71873bd287
Charles S. Givre <cgivre@gmail.com>
2018-12-10 15:54:22 -0800

    Support for Apache Drill

--- server/drivers/drill/drill.js
+++ server/drivers/drill/drill.js
@@ -1,18 +1,3 @@
-/*
-Copyright 2017 Charles S. Givre
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 const fetch = require('node-fetch')
 var request = require('request')
 var url = require('url')
@@ -41,6 +26,7 @@ Client.prototype.execute = function(queryString, callback) {
   })
   let headers = {
     'Content-Type': 'application/json; charset=UTF-8',
+    'User-Name': this.user,
     Accept: 'application/json'
   }
   let queryOptions = {
--- server/drivers/drill/index.js
+++ server/drivers/drill/index.js
@@ -1,19 +1,3 @@
-/*
-Copyright 2017 Charles S. Givre
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 const drill = require('./drill.js')
 const { formatSchemaQueryResults } = require('../utils')
 
@@ -62,7 +46,6 @@ function runQuery(query, connection) {
   }
   const client = new drill.Client(drillConfig)
 
-  //return client.query(query, function(error, data, columns) {
   return client.query(drillConfig, query).then(result => {
     if (!result) {
       throw new Error('No result returned')
