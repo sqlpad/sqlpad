bfcbf7070f99cb5fdd6d16e47447c94da3eeba9c
Rick Bergfalk <rbergfalk@uptycs.com>
2019-01-06 17:27:42 -0500

    Uninstall unstated

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import 'antd/dist/antd.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'tachyons/css/tachyons.min.css'
-import { Provider } from 'unstated'
 import App from './App.js'
 import AppContextProvider from './containers/AppContextProvider'
 import './css/index.css'
@@ -10,10 +9,8 @@ import './css/react-split-pane.css'
 import './css/vendorOverrides.css'
 
 ReactDOM.render(
-  <Provider>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-  </Provider>,
+  <AppContextProvider>
+    <App />
+  </AppContextProvider>,
   document.getElementById('root')
 )
