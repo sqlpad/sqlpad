6865f2acd1404e5aaeccc7d8f390c7adc00716d8
Rick Bergfalk <rbergfalk@uptycs.com>
2019-01-17 08:45:07 -0500

    test

--- client/src/common/Button.js
+++ client/src/common/Button.js
@@ -27,6 +27,8 @@ class Button extends React.Component {
   }
 }
 
+let foo;
+
 Button.propTypes = {
   className: PropTypes.string,
   onClick: PropTypes.func,
@@ -36,6 +38,6 @@ Button.propTypes = {
 Button.defaultProps = {
   className: '',
   onClick: () => {}
-}
+};
 
 export default Button
--- server/lib/db.js
+++ server/lib/db.js
@@ -28,6 +28,8 @@ const db = {
   }
 }
 
+let foo
+
 module.exports = db
 
 // Load dbs, migrate data, and apply indexes
