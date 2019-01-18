2da36cfddd1db8898530714b38f5f43cf24512a1
Rick Bergfalk <rbergfalk@uptycs.com>
2019-01-17 21:58:37 -0500

    Consolidate linting to root of project

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,8 +3,5 @@
 # Get directory script is in
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPTS_DIR/..
-SQLPAD_DIR=`pwd`
 
-# run linter
-npm run lint --prefix "$SQLPAD_DIR/client"
-npm run lint --prefix "$SQLPAD_DIR/server"
+npm run lint
