e254bc2723e55d0c88668bb839d8ba4ad2d2f2df
Rick Bergfalk <rbergfalk@uptycs.com>
2019-03-29 22:32:26 -0400

    Allow adding new connection from connection picker

--- client/src/queryEditor/ConnectionDropdown.js
+++ client/src/queryEditor/ConnectionDropdown.js
@@ -1,39 +1,65 @@
 import Select from 'antd/lib/select';
-import React from 'react';
+import Icon from 'antd/lib/icon';
+import React, { useContext, useState } from 'react';
 import { ConnectionsContext } from '../connections/ConnectionsStore';
+import ConnectionEditDrawer from '../connections/ConnectionEditDrawer';
 
 const { Option } = Select;
 
 function ConnectionDropdown() {
+  const connectionsContext = useContext(ConnectionsContext);
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleChange = id => {
+    if (id === 'new') {
+      return setShowEdit(true);
+    }
+    connectionsContext.selectConnection(id);
+  };
+
+  const handleConnectionSaved = connection => {
+    connectionsContext.addUpdateConnection(connection);
+    connectionsContext.selectConnection(connection._id);
+    setShowEdit(false);
+  };
+
   return (
-    <ConnectionsContext.Consumer>
-      {context => (
-        <Select
-          showSearch
-          placeholder="Choose a connection"
-          // TODO className is overridden by antdesign css?
-          // className="w5"
-          style={{ width: 260 }}
-          optionFilterProp="children"
-          value={context.selectedConnectionId}
-          onChange={id => context.selectConnection(id)}
-          filterOption={(input, option) =>
-            option.props.value &&
-            option.props.children.toLowerCase().indexOf(input.toLowerCase()) >=
-              0
-          }
-        >
-          <Option value="">Choose a connection...</Option>
-          {context.connections.map(conn => {
-            return (
-              <Option key={conn._id} value={conn._id}>
-                {conn.name}
-              </Option>
-            );
-          })}
-        </Select>
-      )}
-    </ConnectionsContext.Consumer>
+    <>
+      <Select
+        showSearch
+        placeholder="Choose a connection"
+        // TODO className is overridden by antdesign css?
+        // className="w5"
+        style={{ width: 260 }}
+        optionFilterProp="children"
+        value={connectionsContext.selectedConnectionId}
+        onChange={handleChange}
+        filterOption={(input, option) =>
+          option.props.value &&
+          option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+        }
+      >
+        <Option value="">
+          <em>Choose a connection...</em>
+        </Option>
+        {connectionsContext.connections.map(conn => {
+          return (
+            <Option key={conn._id} value={conn._id}>
+              {conn.name}
+            </Option>
+          );
+        })}
+        <Option value="new">
+          <Icon type="plus-circle" /> <em>New connection</em>
+        </Option>
+      </Select>
+      <ConnectionEditDrawer
+        visible={showEdit}
+        placement="left"
+        onClose={() => setShowEdit(false)}
+        onConnectionSaved={handleConnectionSaved}
+      />
+    </>
   );
 }
 
