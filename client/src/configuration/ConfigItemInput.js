951f807d52918b5caf367a7d00b37b64cc9a9b3c
Rick Bergfalk <rick.bergfalk@gmail.com>
2019-03-30 12:21:52 -0400

    Simplify the configuration UI (#421)
    * Simplify config ui
    
    Remove environment config info, checklist. Move additional config item info into a popover
    
    * Only send UI config items from API
    
    * Make configuration view a drawer
    
    And make inputs fancy
    
    * Remove unnecessary style

--- client/src/configuration/ConfigItemInput.js
+++ client/src/configuration/ConfigItemInput.js
@@ -1,20 +1,30 @@
 import Input from 'antd/lib/input';
 import Select from 'antd/lib/select';
-import React, { useState } from 'react';
+import Form from 'antd/lib/form';
+import Popover from 'antd/lib/popover';
+import Switch from 'antd/lib/switch';
+import React from 'react';
 
 const { Option } = Select;
 
-function ConfigItemInput({ config, saveConfigValue }) {
-  const [value, setValue] = useState(config.effectiveValue);
+function configIsBoolean(config) {
+  const { options } = config;
+  return (
+    typeof config.effectiveValue === 'boolean' &&
+    options &&
+    options.length === 2 &&
+    options.includes(true) &&
+    options.includes(false)
+  );
+}
 
+function ConfigItemInput({ config, onChange }) {
   const handleChange = e => {
-    setValue(e.target.value);
-    saveConfigValue(config.key, e.target.value);
+    onChange(config.key, e.target.value);
   };
 
   const handleSelectChange = value => {
-    setValue(value);
-    saveConfigValue(config.key, value);
+    onChange(config.key, value);
   };
 
   const disabled =
@@ -22,7 +32,49 @@ function ConfigItemInput({ config, saveConfigValue }) {
     config.effectiveValueSource === 'saved cli' ||
     config.effectiveValueSource === 'env';
 
-  if (config.options) {
+  const effectiveValueSourceLabels = {
+    cli: 'Command Line',
+    'saved cli': 'Saved Command Line',
+    env: 'Environment Varialbe'
+  };
+  const overriddenBy = effectiveValueSourceLabels[config.effectiveValueSource];
+
+  const defaultValue =
+    config.default === '' ? (
+      <em>empty</em>
+    ) : (
+      <span>{config.default.toString()}</span>
+    );
+
+  const popoverContent = (
+    <div style={{ maxWidth: 300 }}>
+      <p>{config.description}</p>
+      <p>
+        <span>Default:</span> {defaultValue}
+      </p>
+      {disabled && (
+        <>
+          <p>
+            <span>Set By:</span> {overriddenBy}
+          </p>
+          <p>
+            When set by command line or environment, item is not configurable
+            via UI.
+          </p>
+        </>
+      )}
+    </div>
+  );
+
+  let input;
+  if (configIsBoolean(config)) {
+    input = (
+      <Switch
+        checked={config.effectiveValue}
+        onChange={value => onChange(config.key, value)}
+      />
+    );
+  } else if (config.options) {
     const optionNodes = config.options.map(option => {
       return (
         <Option key={option} value={option}>
@@ -30,27 +82,35 @@ function ConfigItemInput({ config, saveConfigValue }) {
         </Option>
       );
     });
-    return (
+    input = (
       <Select
         className="w-100"
         disabled={disabled}
         onChange={handleSelectChange}
-        value={value}
+        value={config.effectiveValue}
       >
         {optionNodes}
       </Select>
     );
   } else {
-    return (
+    input = (
       <Input
         className="w-100"
         disabled={disabled}
         onChange={handleChange}
         placeholder={config.label}
-        value={value}
+        value={config.effectiveValue}
       />
     );
   }
+
+  return (
+    <Form.Item label={config.label}>
+      <Popover placement="right" content={popoverContent} trigger="hover">
+        {input}
+      </Popover>
+    </Form.Item>
+  );
 }
 
 export default ConfigItemInput;
---
+++ client/src/configuration/ConfigurationDrawer.js
@@ -0,0 +1,110 @@
+import message from 'antd/lib/message';
+import Form from 'antd/lib/form';
+import Button from 'antd/lib/button';
+import Drawer from 'antd/lib/drawer';
+import React, { useState, useEffect, useContext } from 'react';
+import fetchJson from '../utilities/fetch-json.js';
+import ConfigItemInput from './ConfigItemInput';
+import AppContext from '../containers/AppContext';
+
+const formItemLayout = {
+  labelCol: {
+    sm: { span: 12 }
+  },
+  wrapperCol: {
+    sm: { span: 10 }
+  }
+};
+
+const tailFormItemLayout = {
+  wrapperCol: {
+    sm: {
+      span: 10,
+      offset: 12
+    }
+  }
+};
+
+function ConfigurationDrawer({ onClose, visible }) {
+  const [configItems, setConfigItems] = useState([]);
+  const appContext = useContext(AppContext);
+
+  const loadConfigValuesFromServer = async () => {
+    const json = await fetchJson('GET', '/api/config-items');
+    if (json.error) {
+      message.error(json.error);
+    }
+    setConfigItems(json.configItems);
+  };
+
+  useEffect(() => {
+    if (visible === true) {
+      loadConfigValuesFromServer();
+    }
+  }, [visible]);
+
+  async function saveConfigValues() {
+    const changedSaves = configItems
+      .filter(item => item.changed)
+      .map(item => {
+        return fetchJson('POST', `/api/config-values/${item.key}`, {
+          value: item.effectiveValue
+        });
+      });
+
+    const responses = await Promise.all(changedSaves);
+    const errorResponse = responses.find(r => r.error);
+    if (errorResponse) {
+      message.error('Save failed');
+    } else {
+      await appContext.refreshAppContext();
+      onClose();
+    }
+  }
+
+  const handleChange = (key, value) => {
+    const items = configItems.map(item => {
+      if (item.key === key) {
+        return { ...item, effectiveValue: value, changed: true };
+      }
+      return item;
+    });
+    setConfigItems(items);
+  };
+
+  const hasChanges = configItems.filter(config => config.changed);
+  const saveDisabled = hasChanges.length === 0;
+
+  return (
+    <Drawer
+      title={'Configuration'}
+      visible={visible}
+      width={600}
+      destroyOnClose={false}
+      onClose={onClose}
+      placement={'left'}
+    >
+      <Form {...formItemLayout}>
+        <Form.Item {...tailFormItemLayout}>
+          <Button
+            disabled={saveDisabled}
+            className="w-100"
+            type="primary"
+            onClick={saveConfigValues}
+          >
+            Save
+          </Button>
+        </Form.Item>
+        {configItems.map(config => (
+          <ConfigItemInput
+            key={config.key}
+            config={config}
+            onChange={handleChange}
+          />
+        ))}
+      </Form>
+    </Drawer>
+  );
+}
+
+export default React.memo(ConfigurationDrawer);
