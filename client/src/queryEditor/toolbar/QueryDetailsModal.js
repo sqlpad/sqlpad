09fa82bedeb7dbb084b1f606ba9780bb687ca929
Rick Bergfalk <rbergfalk@uptycs.com>
2019-05-03 19:57:57 -0400

    Remove EditableTagGroup

--- client/src/queryEditor/toolbar/QueryDetailsModal.js
+++ client/src/queryEditor/toolbar/QueryDetailsModal.js
@@ -1,16 +1,13 @@
 import Icon from 'antd/lib/icon';
 import Modal from 'antd/lib/modal';
 import React from 'react';
-import EditableTagGroup from '../../common/EditableTagGroup';
 import { Link } from 'react-router-dom';
 import { connect } from 'unistore/react';
 import { actions } from '../../stores/unistoreStore';
 
 function mapStateToProps(state) {
   return {
-    availableTags: state.availableTags,
-    queryId: state.query && state.query._id,
-    queryTags: state.query && state.query.tags
+    queryId: state.query && state.query._id
   };
 }
 
@@ -19,23 +16,7 @@ const ConnectedQueryDetailsModal = connect(
   actions
 )(React.memo(QueryDetailsModal));
 
-function QueryDetailsModal({
-  queryId,
-  queryTags,
-  visible,
-  setQueryState,
-  availableTags,
-  onClose
-}) {
-  const tagOptions = availableTags.slice();
-  if (queryTags) {
-    queryTags.forEach(t => {
-      if (tagOptions.indexOf(t) === -1) {
-        tagOptions.push(t);
-      }
-    });
-  }
-
+function QueryDetailsModal({ queryId, visible, onClose }) {
   const renderNavLink = (href, text) => {
     const saved = !!queryId;
     if (saved) {
@@ -61,11 +42,6 @@ function QueryDetailsModal({
       footer={null}
     >
       <label>Query Tags</label>
-      <EditableTagGroup
-        tags={queryTags}
-        onChange={values => setQueryState('tags', values)}
-        tagOptions={tagOptions}
-      />
       <hr />
       <p>
         <label>Shortcuts</label>
