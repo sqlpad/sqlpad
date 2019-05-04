acc605a60e16c88b8c379e7d59adfcb3cc57d8b3
Rick Bergfalk <rbergfalk@uptycs.com>
2019-05-04 12:29:47 -0400

    Add error style to Select component

--- client/src/common/Select.js
+++ client/src/common/Select.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './Select.module.css';
 
-export default function Select({ children, className, ...rest }) {
+export default function Select({ children, error, className, ...rest }) {
   const classNames = [styles.select];
 
   if (className) {
     classNames.push(className);
+  }
+  if (error) {
+    classNames.push(styles.danger);
   }
 
   return (
