06515ccc66738579659ac1c32c825a2d39fadcdf
Rick Bergfalk <rbergfalk@uptycs.com>
2019-05-12 14:45:03 -0400

    remove unused

--- client/src/queryEditor/toolbar/QueryTagsModal.js
+++ client/src/queryEditor/toolbar/QueryTagsModal.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { connect } from 'unistore/react';
 import { actions } from '../../stores/unistoreStore';
 import Modal from '../../common/Modal';
-import Button from '../../common/Button';
 import MultiSelect from '../../common/MultiSelect';
-import Spacer from '../../common/Spacer';
 
 function mapStateToProps(state) {
   return {
@@ -43,10 +41,6 @@ function QueryTagsModal({
         options={availableTags.map(tag => ({ name: tag, id: tag }))}
         onChange={handleChange}
       />
-      {/* <Spacer />
-      <Button type="primary" onClick={onClose}>
-        OK
-      </Button> */}
     </Modal>
   );
 }
