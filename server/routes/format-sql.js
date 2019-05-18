e4e6025bd1ee91135907ddf827d68e3045825420
Rick Bergfalk <rbergfalk@uptycs.com>
2019-05-17 23:19:42 -0400

    Add format-sql API

---
+++ server/routes/format-sql.js
@@ -0,0 +1,18 @@
+const sqlFormatter = require('sql-formatter');
+const router = require('express').Router();
+const mustBeAuthenticated = require('../middleware/must-be-authenticated.js');
+const sendError = require('../lib/sendError');
+
+/**
+ * Returns formatted query in same object format it was sent
+ */
+router.post('/api/format-sql', mustBeAuthenticated, function(req, res) {
+  const { body } = req;
+  if (!body.query) {
+    return sendError(res, null, 'query property must be provided');
+  }
+  body.query = sqlFormatter.format(body.query);
+  res.send(body);
+});
+
+module.exports = router;
