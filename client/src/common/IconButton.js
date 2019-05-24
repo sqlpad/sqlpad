3ed8c274af93fbf550a00eccd0093733a9bd6e0e
Rick Bergfalk <rbergfalk@uptycs.com>
2019-05-24 12:27:01 -0500

    Add IconButton component

---
+++ client/src/common/IconButton.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './IconButton.module.css';
+import Tooltip from './Tooltip';
+
+const ICON_SIZE = 18;
+
+const IconButton = React.forwardRef(
+  ({ children, to, icon, tooltip, disabled, className, ...rest }, ref) => {
+    const classNames = [styles.btn];
+
+    if (className) {
+      classNames.push(className);
+    }
+
+    let button;
+
+    // If to is supplied this is a link
+    // IMPORTANT: Link is wrapped in <span> to handle tooltip ref passing
+    if (to) {
+      button = (
+        <span>
+          <Link to={to} className={classNames.join(' ')} {...rest}>
+            {React.Children.map(children, child => {
+              return React.cloneElement(child, { size: ICON_SIZE }, null);
+            })}
+          </Link>
+        </span>
+      );
+    } else {
+      button = (
+        <button
+          ref={ref}
+          className={classNames.join(' ')}
+          disabled={disabled}
+          {...rest}
+        >
+          {React.Children.map(children, child => {
+            return React.cloneElement(child, { size: ICON_SIZE }, null);
+          })}
+        </button>
+      );
+    }
+
+    // If the button is disabled the tooltip gets weird on hover
+    if (!tooltip || disabled) {
+      return button;
+    }
+
+    return <Tooltip label={tooltip}>{button}</Tooltip>;
+  }
+);
+
+export default IconButton;
