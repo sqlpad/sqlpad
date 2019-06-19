35a7a2d54b1b763eddf438d4b23e1e603e5dcc55
Rick Bergfalk <rbergfalk@uptycs.com>
2019-06-18 23:58:09 -0400

    Add table link, icon links to query result header

--- client/src/queryEditor/QueryResultHeader.js
+++ client/src/queryEditor/QueryResultHeader.js
@@ -1,47 +1,34 @@
+import OpenInNewIcon from 'mdi-react/OpenInNewIcon';
+import DownloadIcon from 'mdi-react/DownloadIcon';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'unistore/react';
 import IncompleteDataNotification from '../common/IncompleteDataNotification';
 import SecondsTimer from '../common/SecondsTimer.js';
-import Text from '../common/Text';
-
-const barStyle = {
-  height: '30px',
-  borderBottom: '1px solid #ccc',
-  backgroundColor: '#f4f4f4',
-  lineHeight: '30px',
-  paddingLeft: 4
-};
-
-const headerItemStyle = {
-  paddingLeft: 4,
-  paddingRight: 48
-};
+import styles from './QueryResultHeader.module.css';
 
 function QueryResultHeader({
   cacheKey,
   config,
   isRunning,
+  queryId,
   queryResult,
   runQueryStartTime
 }) {
   if (isRunning || !queryResult) {
     return (
-      <div style={barStyle}>
+      <div className={styles.toolbar}>
         {isRunning ? (
-          <span style={headerItemStyle}>
-            <Text type="secondary">Query time: </Text>
-            <SecondsTimer startTime={runQueryStartTime} /> sec.
+          <span className={styles.toolbarItem}>
+            Query time: <SecondsTimer startTime={runQueryStartTime} />
           </span>
         ) : null}
       </div>
     );
   }
 
-  const serverSec = queryResult
-    ? queryResult.queryRunTime / 1000 + ' sec.'
-    : '';
+  const serverSec = queryResult ? queryResult.queryRunTime / 1000 : '';
   const rowCount =
     queryResult && queryResult.rows ? queryResult.rows.length : '';
 
@@ -49,40 +36,51 @@ function QueryResultHeader({
 
   const csvDownloadLink = `/download-results/${cacheKey}.csv`;
   const xlsxDownloadLink = `/download-results/${cacheKey}.xlsx`;
+  const tableLink = `/query-table/${queryId}`;
 
   return (
-    <div style={barStyle}>
-      <span style={headerItemStyle}>
-        <Text type="secondary">Query time: </Text>
-        {serverSec}
-      </span>
-      <span style={headerItemStyle}>
-        <Text type="secondary">Rows: </Text>
-        {rowCount}
-      </span>
-      <span style={headerItemStyle}>
+    <div className={styles.toolbar}>
+      <div className={styles.toolbarItem}>{serverSec} seconds</div>
+      <div className={styles.toolbarItem}>{rowCount} rows</div>
+
+      <div className={styles.toolbarItem}>
         {config.allowCsvDownload && (
-          <span>
-            <Text type="secondary">Download: </Text>
+          <span className={styles.iconLinkWrapper}>
             <Link
-              style={{ marginLeft: 16 }}
+              className={styles.iconLink}
               target="_blank"
               rel="noopener noreferrer"
               to={csvDownloadLink}
             >
+              <DownloadIcon style={{ marginRight: 4 }} size={16} />
               .csv
             </Link>
+
             <Link
-              style={{ marginLeft: 16 }}
+              className={styles.iconLink}
               target="_blank"
               rel="noopener noreferrer"
               to={xlsxDownloadLink}
             >
+              <DownloadIcon style={{ marginRight: 4 }} size={16} />
               .xlsx
             </Link>
           </span>
         )}
-      </span>
+      </div>
+      <div className={styles.toolbarItem}>
+        <span className={styles.iconLinkWrapper}>
+          <Link
+            className={styles.iconLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            to={tableLink}
+            disabled={!Boolean(queryId) || queryId === 'new'}
+          >
+            table <OpenInNewIcon style={{ marginLeft: 4 }} size={16} />
+          </Link>
+        </span>
+      </div>
 
       {incomplete && <IncompleteDataNotification />}
     </div>
@@ -103,10 +101,16 @@ QueryResultHeader.defaultProps = {
   isRunning: false
 };
 
-export default connect([
-  'cacheKey',
-  'config',
-  'isRunning',
-  'queryResult',
-  'runQueryStartTime'
-])(React.memo(QueryResultHeader));
+function mapStateToProps(state) {
+  const { cacheKey, config, isRunning, queryResult, runQueryStartTime } = state;
+  return {
+    cacheKey,
+    config,
+    isRunning,
+    queryId: state.query && state.query._id,
+    queryResult,
+    runQueryStartTime
+  };
+}
+
+export default connect(mapStateToProps)(React.memo(QueryResultHeader));
