69a576626b02e012e05a54677e3a6857b20dc392
Rick Bergfalk <rbergfalk@uptycs.com>
2019-06-23 22:55:26 -0400

    Add localQueryText helpers

---
+++ client/src/utilities/localQueryText.js
@@ -0,0 +1,25 @@
+import localforage from 'localforage';
+
+export function setLocalQueryText(queryId, queryText) {
+  return localforage
+    .setItem(`queryText:${queryId}`, queryText)
+    .catch(error => console.error(error));
+}
+
+export function getLocalQueryText(queryId) {
+  return localforage
+    .getItem(`queryText:${queryId}`)
+    .catch(error => console.error(error));
+}
+
+export function removeLocalQueryText(queryId) {
+  return localforage
+    .removeItem(`queryText:${queryId}`)
+    .catch(error => console.error(error));
+}
+
+export default {
+  setLocalQueryText,
+  getLocalQueryText,
+  removeLocalQueryText
+};
