197bed72585b411b9daac8c2c2d157194f5248a2
Rick Bergfalk <rbergfalk@uptycs.com>
2019-06-30 23:06:21 -0400

    Revert "Code split on route"
    This reverts commit e223c14f731e9ef939cf21953486e9a546429314.

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Suspense, lazy } from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Redirect,
@@ -6,23 +6,19 @@ import {
   Switch
 } from 'react-router-dom';
 import { connect } from 'unistore/react';
+import Authenticated from './Authenticated';
+import ForgotPassword from './ForgotPassword.js';
+import NotFound from './NotFound.js';
+import PasswordReset from './PasswordReset.js';
+import PasswordResetRequested from './PasswordResetRequested.js';
+import QueryChartOnly from './QueryChartOnly.js';
+import QueryEditor from './queryEditor/QueryEditor.js';
+import QueryTableOnly from './QueryTableOnly.js';
+import SignIn from './SignIn.js';
+import SignUp from './SignUp.js';
 import { refreshAppContext } from './stores/config';
 import { initSelectedConnection } from './stores/connections';
 import { initSchema } from './stores/schema';
-import SpinKitCube from './common/SpinKitCube';
-
-const Authenticated = lazy(() => import('./Authenticated'));
-const ForgotPassword = lazy(() => import('./ForgotPassword.js'));
-const NotFound = lazy(() => import('./NotFound.js'));
-const PasswordReset = lazy(() => import('./PasswordReset.js'));
-const PasswordResetRequested = lazy(() =>
-  import('./PasswordResetRequested.js')
-);
-const QueryChartOnly = lazy(() => import('./QueryChartOnly.js'));
-const QueryEditor = lazy(() => import('./queryEditor/QueryEditor.js'));
-const QueryTableOnly = lazy(() => import('./QueryTableOnly.js'));
-const SignIn = lazy(() => import('./SignIn.js'));
-const SignUp = lazy(() => import('./SignUp.js'));
 
 function Routes({
   config,
@@ -42,69 +38,57 @@ function Routes({
 
   return (
     <Router basename={config.baseUrl}>
-      <Suspense
-        fallback={
-          <div style={{ marginTop: 100 }}>
-            <SpinKitCube />
-          </div>
-        }
-      >
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => <Redirect to={'/queries/new'} />}
-          />
-          <Route
-            exact
-            path="/queries"
-            render={() => <Redirect to={'/queries/new'} />}
-          />
-          <Route
-            exact
-            path="/queries/:queryId"
-            render={({ match }) => (
-              <Authenticated>
-                <QueryEditor queryId={match.params.queryId} />
-              </Authenticated>
-            )}
-          />
-          <Route
-            exact
-            path="/query-table/:queryId"
-            render={({ match }) => (
-              <QueryTableOnly queryId={match.params.queryId} />
-            )}
-          />
-          <Route
-            exact
-            path="/query-chart/:queryId"
-            render={({ match }) => (
-              <QueryChartOnly queryId={match.params.queryId} />
-            )}
-          />
-          <Route exact path="/signin" render={() => <SignIn />} />
-          <Route exact path="/signup" render={() => <SignUp />} />
-          <Route
-            exact
-            path="/forgot-password"
-            render={() => <ForgotPassword />}
-          />
-          <Route
-            exact
-            path="/password-reset/:passwordResetId"
-            render={({ match }) => (
-              <PasswordReset passwordResetId={match.params.passwordResetId} />
-            )}
-          />
-          <Route
-            exact
-            path="/password-reset"
-            render={() => <PasswordResetRequested />}
-          />
-          <Route render={() => <NotFound />} />
-        </Switch>
-      </Suspense>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to={'/queries/new'} />} />
+        <Route
+          exact
+          path="/queries"
+          render={() => <Redirect to={'/queries/new'} />}
+        />
+        <Route
+          exact
+          path="/queries/:queryId"
+          render={({ match }) => (
+            <Authenticated>
+              <QueryEditor queryId={match.params.queryId} />
+            </Authenticated>
+          )}
+        />
+        <Route
+          exact
+          path="/query-table/:queryId"
+          render={({ match }) => (
+            <QueryTableOnly queryId={match.params.queryId} />
+          )}
+        />
+        <Route
+          exact
+          path="/query-chart/:queryId"
+          render={({ match }) => (
+            <QueryChartOnly queryId={match.params.queryId} />
+          )}
+        />
+        <Route exact path="/signin" render={() => <SignIn />} />
+        <Route exact path="/signup" render={() => <SignUp />} />
+        <Route
+          exact
+          path="/forgot-password"
+          render={() => <ForgotPassword />}
+        />
+        <Route
+          exact
+          path="/password-reset/:passwordResetId"
+          render={({ match }) => (
+            <PasswordReset passwordResetId={match.params.passwordResetId} />
+          )}
+        />
+        <Route
+          exact
+          path="/password-reset"
+          render={() => <PasswordResetRequested />}
+        />
+        <Route render={() => <NotFound />} />
+      </Switch>
     </Router>
   );
 }
