c252c4288033d406b2180ec1f7149cbeede33422
Rick Bergfalk <rick.bergfalk@gmail.com>
2019-12-30 00:42:37 -0600

    Let decipher errors bubble up

--- server/lib/decipher.js
+++ server/lib/decipher.js
@@ -8,13 +8,6 @@ const passphrase = config.get('passphrase');
  * @returns {string} deciphered value
  */
 module.exports = function decipher(gibberish) {
-  let returnValue = '';
-  try {
-    const myDecipher = crypto.createDecipher(algorithm, passphrase);
-    returnValue =
-      myDecipher.update(gibberish, 'hex', 'utf8') + myDecipher.final('utf8');
-  } catch (e) {
-    console.error(e);
-  }
-  return returnValue;
+  const myDecipher = crypto.createDecipher(algorithm, passphrase);
+  return myDecipher.update(gibberish, 'hex', 'utf8') + myDecipher.final('utf8');
 };
