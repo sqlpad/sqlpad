d1804776982e141e9352eabcf7d4e0e488af9060
MuchAdoAboutNothing <vbagga@gmail.com>
2020-02-05 20:27:03 -0800

    Fix for "TypeError: Do not know how to serialize a BigInt" (#522)
    * Override the toJSON method
    
    * Workaround for limited support of BigInt
    
    * Add comments for eslint

--- server/models/resultCache.js
+++ server/models/resultCache.js
@@ -90,6 +90,17 @@ function writeCsv(cacheKey, queryResult) {
   });
 }
 
+// This is a workaround till BigInt is fully supported by the standard
+// See https://tc39.es/ecma262/#sec-ecmascript-language-types-bigint-type
+// and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
+// If this is not done, then a JSON.stringify(BigInt) throws
+// "TypeError: Do not know how to serialize a BigInt"
+/* global BigInt:writable */
+/* eslint no-extend-native: ["error", { "exceptions": ["BigInt"] }] */
+BigInt.prototype.toJSON = function() {
+  return this.toString();
+};
+
 function writeJson(cacheKey, queryResult) {
   return new Promise(resolve => {
     try {
