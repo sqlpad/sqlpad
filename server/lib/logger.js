0c478d9cea51a1d08ed9e2edf0ef0ee4926fe539
Rick Bergfalk <rbergfalk@uptycs.com>
2020-02-17 11:48:00 -0600

    logger enable and level refinements

--- server/lib/logger.js
+++ server/lib/logger.js
@@ -2,11 +2,13 @@ const config = require('./config');
 
 // Log levels https://github.com/pinojs/pino/issues/123
 const logLevel = config.get('logLevel');
+const logApp = config.get('logApp');
+const debug = config.get('debug');
 
 const logger = require('pino')({
   name: 'sqlpad-app',
-  enabled: true, // TODO turn off for testing?
-  level: logLevel // default 'info' // or debug
+  enabled: logApp,
+  level: debug ? 'debug' : logLevel
 });
 
 module.exports = logger;
