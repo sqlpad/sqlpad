279ca4d87f31712e1c13d8135c7147423fdc58d9
Rick Bergfalk <rbergfalk@uptycs.com>
2020-02-19 00:28:43 -0600

    Move passport basic authentication to own middleware (#538)

--- server/middleware/must-be-authenticated-or-chart-link-noauth.js
+++ server/middleware/must-be-authenticated-or-chart-link-noauth.js
@@ -1,15 +1,9 @@
-const passport = require('passport');
 const config = require('../lib/config');
 
-// If authenticated continue
-// If not and auth header is present, try authenticated with http basic
-// Otherwise redirect user to signin
+// If authenticated or setting allows it continue. Otherwise redirect user to signin
 module.exports = function mustBeAuthenticatedOrChartLinkNoAuth(req, res, next) {
   if (req.isAuthenticated() || !config.get('tableChartLinksRequireAuth')) {
     return next();
-  }
-  if (req.headers.authorization) {
-    return passport.authenticate('basic', { session: false })(req, res, next);
   }
   res.redirect(config.get('baseUrl') + '/signin');
 };
--- server/middleware/must-be-authenticated.js
+++ server/middleware/must-be-authenticated.js
@@ -1,15 +1,9 @@
-const passport = require('passport');
 const config = require('../lib/config');
 
-// If authenticated continue
-// If not and auth header is present, try authenticated with http basic
-// Otherwise redirect user to signin
+// If authenticated continue. otherwise redirect user to signin
 module.exports = function mustBeAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
-  }
-  if (req.headers.authorization) {
-    return passport.authenticate('basic', { session: false })(req, res, next);
   }
   res.redirect(config.get('baseUrl') + '/signin');
 };
