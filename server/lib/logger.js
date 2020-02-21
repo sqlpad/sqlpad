8a61d3db4290ff5fb9e521f39b3ea840608c1a4a
Rick Bergfalk <rbergfalk@uptycs.com>
2020-02-20 23:33:13 -0600

    Implement pino logger wrapper

--- server/lib/logger.js
+++ server/lib/logger.js
@@ -1,11 +1,49 @@
-const config = require('./config');
+const pino = require('pino');
+
+const levels = ['fatal', 'error', 'warn', 'info', 'debug', 'silent'];
 
 // Log levels https://github.com/pinojs/pino/issues/123
-const appLogLevel = config.get('appLogLevel');
+class Logger {
+  constructor(logLevel = 'warn') {
+    if (!levels.includes(logLevel)) {
+      throw new Error(`Unknown log level ${logLevel}`);
+    }
+    this.logLevel = logLevel;
+    this.logger = pino({
+      name: 'sqlpad-app',
+      level: logLevel
+    });
+  }
 
-const logger = require('pino')({
-  name: 'sqlpad-app',
-  level: appLogLevel
-});
+  setLevel(logLevel) {
+    if (!levels.includes(logLevel)) {
+      throw new Error(`Unknown log level ${logLevel}`);
+    }
+    this.logger = pino({
+      name: 'sqlpad-app',
+      level: logLevel
+    });
+  }
 
-module.exports = logger;
+  fatal(...args) {
+    this.logger.fatal(...args);
+  }
+
+  error(...args) {
+    this.logger.error(...args);
+  }
+
+  warn(...args) {
+    this.logger.warn(...args);
+  }
+
+  info(...args) {
+    this.logger.info(...args);
+  }
+
+  debug(...args) {
+    this.logger.debug(...args);
+  }
+}
+
+module.exports = Logger;
