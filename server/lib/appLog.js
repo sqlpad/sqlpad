7d756ef043a43b6304341b0e6e3153637f820dcb
Rick Bergfalk <rbergfalk@uptycs.com>
2020-02-21 00:24:32 -0600

    Set log level when testing

--- server/lib/appLog.js
+++ server/lib/appLog.js
@@ -1,8 +1,4 @@
 const Logger = require('./logger');
-const config = require('./config');
-
-const appLogLevel = config.get('appLogLevel');
-
-const appLog = new Logger(appLogLevel);
+const appLog = new Logger();
 
 module.exports = appLog;
