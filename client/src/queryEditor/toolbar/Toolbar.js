cc7298ab1de68f9c49784957d6b4d1e81572378c
Rick Bergfalk <rbergfalk@uptycs.com>
2020-02-24 16:12:00 -0600

    Format button into own component

--- client/src/queryEditor/toolbar/Toolbar.js
+++ client/src/queryEditor/toolbar/Toolbar.js
@@ -1,12 +1,11 @@
 import UnsavedIcon from 'mdi-react/ContentSaveEditIcon';
 import SaveIcon from 'mdi-react/ContentSaveIcon';
-import FormatIcon from 'mdi-react/FormatAlignLeftIcon';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'unistore/react';
 import Button from '../../common/Button';
 import IconButton from '../../common/IconButton';
-import { formatQuery, runQuery, saveQuery } from '../../stores/queries';
+import { runQuery, saveQuery } from '../../stores/queries';
 import ConnectionDropDown from '../ConnectionDropdown';
 import ChartButton from './ChartButton';
 import QueryListButton from './QueryListButton';
@@ -17,6 +16,7 @@ import ToolbarQueryNameInput from './ToolbarQueryNameInput';
 import ToolbarSpacer from './ToolbarSpacer';
 import ToolbarTagsButton from './ToolbarTagsButton';
 import ToolbarToggleSchemaButton from './ToolbarToggleSchemaButton';
+import ToolbarFormatQueryButton from './ToolbarFormatQueryButton';
 
 function mapStateToProps(state) {
   return {
@@ -27,19 +27,11 @@ function mapStateToProps(state) {
 }
 
 const ConnectedEditorNavBar = connect(mapStateToProps, store => ({
-  formatQuery,
   runQuery: runQuery(store),
   saveQuery: saveQuery(store)
 }))(React.memo(Toolbar));
 
-function Toolbar({
-  formatQuery,
-  isRunning,
-  isSaving,
-  runQuery,
-  saveQuery,
-  unsavedChanges
-}) {
+function Toolbar({ isRunning, isSaving, runQuery, saveQuery, unsavedChanges }) {
   return (
     <div
       style={{
@@ -66,10 +58,7 @@ function Toolbar({
 
         <ToolbarTagsButton />
         <ToolbarCloneButton />
-
-        <IconButton tooltip="Format" onClick={formatQuery}>
-          <FormatIcon />
-        </IconButton>
+        <ToolbarFormatQueryButton />
 
         <IconButton
           tooltip="Save"
@@ -102,7 +91,6 @@ Toolbar.propTypes = {
   isRunning: PropTypes.bool.isRequired,
   saveQuery: PropTypes.func.isRequired,
   runQuery: PropTypes.func.isRequired,
-  formatQuery: PropTypes.func.isRequired,
   unsavedChanges: PropTypes.bool.isRequired
 };
 
