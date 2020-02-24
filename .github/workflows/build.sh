d0152411f3074423157c2c21496c91b8cb43e521
Rémy Garrigue <rgarrigue@wazo.io>
2020-02-24 17:02:02 +0100

    Add build pipeline

---
+++ .github/workflows/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -x
+
+apt-get update
+# apt-get install -y --no-install-recommends jq g++ make python3 python3-setuptools unixodbc-dev
+apt-get install -y --no-install-recommends g++ make python3 python3-setuptools unixodbc-dev
+bash scripts/build.sh
