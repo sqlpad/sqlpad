6d50131da9fec20517553b2621c7ac3dd5cb5804
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-03-04 17:59:42 -0600

    Add Client implementation for ODBC

--- server/drivers/unixodbc/index.js
+++ server/drivers/unixodbc/index.js
@@ -35,101 +35,15 @@ const SCHEMA_SQL_INFORMATION_SCHEMA = `
  */
 
 async function runQuery(query, connection) {
-  const config = {
-    user: connection.username,
-    password: connection.password,
-    connection_string: connection.connection_string
-  };
-
-  let cn = config.connection_string;
-
-  // Not all drivers require auth
-  if (config.user) {
-    cn = cn + ';Uid=' + config.user;
-  }
-  if (config.password) {
-    cn = cn + ';Pwd=' + config.password;
-  }
-
-  let connectionInstance;
+  const client = new Client(connection);
+  await client.connect();
   try {
-    let incomplete = false;
-    let suppressedResultSet = false;
-    connectionInstance = await odbc.connect(cn);
-    const queries = splitSql(query);
-
-    let queryResult;
-    let lastResult;
-
-    for (const query of queries) {
-      // eslint-disable-next-line no-await-in-loop
-      const result = await connectionInstance.query(query);
-
-      // If result has columns it is a candidate for lastQueryResultWithRows
-      // Until SQLPad has capability to show multiple result sets we are showing the last one with results
-      if (result.columns) {
-        // If queryResult was already set we're suppressing a result set
-        // Eventually we'll show all results but not at this point
-        if (queryResult) {
-          suppressedResultSet = true;
-        }
-
-        queryResult = result;
-      }
-
-      // Keep reference to result as last result
-      lastResult = result;
-    }
-
-    // If queryResult was never populated because none of the queries returned results, use last result
-    if (!queryResult) {
-      queryResult = lastResult;
-    }
-
-    await connectionInstance.close();
-
-    // Format data correctly
-    // node-odbc gives a mix of results depending on query type
-    // If columns oject returned with results the query returned rows
-    const { columns } = queryResult;
-    const rows = [];
-
-    if (columns && columns.length > 0) {
-      // iterate over queryResult, which is also an array of rows
-      for (const row of queryResult) {
-        if (connection.maxRows) {
-          if (rows.length < connection.maxRows) {
-            rows.push(row);
-          } else {
-            incomplete = true;
-          }
-        } else {
-          // Just in case maxRows is not defined push the row
-          rows.push(row);
-        }
-      }
-    }
-
-    return { rows, incomplete, suppressedResultSet };
+    const result = await client.runQuery(query);
+    await client.disconnect();
+    return result;
   } catch (error) {
-    appLog.error(error);
-    try {
-      if (connectionInstance && connectionInstance.close) {
-        await connectionInstance.close();
-      }
-    } catch (error) {
-      // Do nothing here.
-      // An error already happened we're just trying to ensure it closed okay
-      appLog.error(error, 'error closing connection after error');
-    }
-    // unixodb error has additional info about why the error occurred
-    // It has an array of objects with messages.
-    // If that exists try to create a message of everything together and throw that
-    // Otherwise throw what we got
-    if (Array.isArray(error.odbcErrors)) {
-      const message = error.odbcErrors.map(e => e.message).join('; ');
-      throw new Error(message);
-    }
+    // try disconnecting just to be sure
+    await client.disconnect();
     throw error;
   }
 }
@@ -143,7 +57,6 @@ function testConnection(connection) {
   return runQuery(query, connection);
 }
 
-// TODO - reviewed no change needed? datatypes need reviewing
 /**
  * Get schema for connection
  * @param {*} connection
@@ -155,6 +68,119 @@ function getSchema(connection) {
   return runQuery(schema_sql, connection).then(queryResult =>
     formatSchemaQueryResults(queryResult)
   );
+}
+
+class Client {
+  constructor(connection) {
+    this.connection = connection;
+    this.client = null;
+  }
+
+  async connect() {
+    if (this.client) {
+      throw new Error('Client already connected');
+    }
+
+    const { username, password, connection_string } = this.connection;
+
+    let cn = connection_string;
+
+    // Not all drivers require auth
+    if (username) {
+      cn = cn + ';Uid=' + username;
+    }
+    if (password) {
+      cn = cn + ';Pwd=' + password;
+    }
+
+    this.client = await odbc.connect(cn);
+  }
+
+  /**
+   * Disconnect the connected client
+   * Does not propagate error up
+   */
+  async disconnect() {
+    try {
+      if (this.client && this.client.close) {
+        await this.client.close();
+      }
+    } catch (error) {
+      appLog.error(error, 'error closing connection after error');
+    }
+    this.client = null;
+  }
+
+  async runQuery(query) {
+    try {
+      let incomplete = false;
+      let suppressedResultSet = false;
+      const queries = splitSql(query);
+
+      let queryResult;
+      let lastResult;
+
+      for (const query of queries) {
+        // eslint-disable-next-line no-await-in-loop
+        const result = await this.client.query(query);
+
+        // If result has columns it is a candidate for lastQueryResultWithRows
+        // Until SQLPad has capability to show multiple result sets we are showing the last one with results
+        if (result.columns) {
+          // If queryResult was already set we're suppressing a result set
+          // Eventually we'll show all results but not at this point
+          if (queryResult) {
+            suppressedResultSet = true;
+          }
+
+          queryResult = result;
+        }
+
+        // Keep reference to result as last result
+        lastResult = result;
+      }
+
+      // If queryResult was never populated because none of the queries returned results, use last result
+      if (!queryResult) {
+        queryResult = lastResult;
+      }
+
+      // Format data correctly
+      // node-odbc gives a mix of results depending on query type
+      // If columns oject returned with results the query returned rows
+      const { columns } = queryResult;
+      const rows = [];
+
+      if (columns && columns.length > 0) {
+        // iterate over queryResult, which is also an array of rows
+        for (const row of queryResult) {
+          if (this.connection.maxRows) {
+            if (rows.length < this.connection.maxRows) {
+              rows.push(row);
+            } else {
+              incomplete = true;
+            }
+          } else {
+            // Just in case maxRows is not defined push the row
+            rows.push(row);
+          }
+        }
+      }
+
+      return { rows, incomplete, suppressedResultSet };
+    } catch (error) {
+      appLog.error(error);
+      // unixodb error has additional info about why the error occurred
+      // It has an array of objects with messages.
+      // If that exists try to create a message of everything together and throw that
+      // Otherwise throw what we got
+      if (Array.isArray(error.odbcErrors)) {
+        const message = error.odbcErrors.map(e => e.message).join('; ');
+        throw new Error(message);
+      }
+      throw error;
+    }
+  }
 }
 
 const fields = [
@@ -183,10 +209,11 @@ const fields = [
 ];
 
 module.exports = {
-  id,
-  name,
+  Client,
   fields,
   getSchema,
+  id,
+  name,
   runQuery,
   testConnection
 };
