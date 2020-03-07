23a8cfb6654f6b266142b83747994fc4fa930ea1
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-03-07 17:41:26 -0600

    Break function out

--- server/routes/schema-info.js
+++ server/routes/schema-info.js
@@ -1,42 +1,49 @@
+require('../typedefs');
 const router = require('express').Router();
 const mustHaveConnectionAccess = require('../middleware/must-have-connection-access.js');
 const sendError = require('../lib/sendError');
 const ConnectionClient = require('../lib/connection-client');
 
+/**
+ * @param {import('express').Request & Req} req
+ * @param {*} res
+ */
+async function getSchemaInfo(req, res) {
+  const { models, user } = req;
+  const { connectionId } = req.params;
+  const reload = req.query.reload === 'true';
+
+  try {
+    const conn = await models.connections.findOneById(connectionId);
+
+    if (!conn) {
+      throw new Error('Connection not found');
+    }
+
+    let schemaInfo = await models.schemaInfo.getSchemaInfo(connectionId);
+
+    if (schemaInfo && !reload) {
+      return res.json({ schemaInfo });
+    }
+
+    const connectionClient = new ConnectionClient(conn, user);
+    schemaInfo = await connectionClient.getSchema();
+    if (Object.keys(schemaInfo).length) {
+      await models.schemaInfo.saveSchemaInfo(connectionId, schemaInfo);
+    }
+    return res.json({ schemaInfo });
+  } catch (error) {
+    if (error.message === 'Connection not found') {
+      return sendError(res, error);
+    }
+    sendError(res, error, 'Problem getting schema info');
+  }
+}
+
 router.get(
   '/api/schema-info/:connectionId',
   mustHaveConnectionAccess,
-  async function(req, res) {
-    const { models, user } = req;
-    const { connectionId } = req.params;
-    const reload = req.query.reload === 'true';
-
-    try {
-      const conn = await models.connections.findOneById(connectionId);
-
-      if (!conn) {
-        throw new Error('Connection not found');
-      }
-
-      let schemaInfo = await models.schemaInfo.getSchemaInfo(connectionId);
-
-      if (schemaInfo && !reload) {
-        return res.json({ schemaInfo });
-      }
-
-      const connectionClient = new ConnectionClient(conn, user);
-      schemaInfo = await connectionClient.getSchema();
-      if (Object.keys(schemaInfo).length) {
-        await models.schemaInfo.saveSchemaInfo(connectionId, schemaInfo);
-      }
-      return res.json({ schemaInfo });
-    } catch (error) {
-      if (error.message === 'Connection not found') {
-        return sendError(res, error);
-      }
-      sendError(res, error, 'Problem getting schema info');
-    }
-  }
+  getSchemaInfo
 );
 
 module.exports = router;
