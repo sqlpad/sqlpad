995bbf5a3feb2e4c16f238f980de57048e3b6c65
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-03-23 11:08:36 -0500

    Test and fix proxy auth

--- server/middleware/passport-proxy-auth.js
+++ server/middleware/passport-proxy-auth.js
@@ -12,69 +12,81 @@ const CustomStrategy = passportCustom.Strategy;
  * @param {function} done
  */
 async function authProxyStrategy(req, done) {
-  const { config, models } = req;
+  try {
+    const { config, models } = req;
 
-  const headerUser = {};
+    const headerUser = {};
 
-  config
-    .get('proxyAuthHeaders')
-    .split(' ')
-    .forEach(pairing => {
-      const [fieldName, headerName] = pairing.split(':').map(v => v.trim());
-      _.set(headerUser, fieldName, req.headers[headerName]);
-    });
+    config
+      .get('proxyAuthHeaders')
+      .split(' ')
+      .forEach(pairing => {
+        const [fieldName, headerName] = pairing.split(':').map(v => v.trim());
+        const value = req.get(headerName);
+        if (value !== null && value !== undefined) {
+          _.set(headerUser, fieldName, req.get(headerName));
+        }
+      });
 
-  // nedb uses user._id for ids, but user.id should also be supported
-  // If id was used, translate it for _id for now
-  if (headerUser.id && !headerUser._id) {
-    headerUser._id = headerUser.id;
-    delete headerUser.id;
-  }
-
-  // If id and email are not provided we don't have anything to look existing user account up with
-  // The request is incomplete and not authorized
-  if (!headerUser._id && !headerUser.email) {
-    return done(null, false);
-  }
-
-  if (!headerUser.role && config.get('proxyAuthDefaultRole')) {
-    headerUser.role = config.get('proxyAuthDefaultRole');
-  }
-
-  // Try to find existing user in SQLPad's db
-  // This tries to find a match on both id and email, with id taking priority
-  let existingUser;
-  if (headerUser._id) {
-    existingUser = await models.users.findOneById(headerUser._id);
-  }
-  if (!existingUser && headerUser.email) {
-    existingUser = await models.users.findOneByEmail(headerUser.email);
-  }
-
-  // If existing user is found, see if there are changes and update it
-  // Only perform update if actual changes though
-  if (existingUser) {
-    // Get a subset of existing user to use for comparison
-    const existingForCompare = {};
-    Object.keys(headerUser).forEach(key => {
-      existingForCompare[key] = existingUser[key];
-    });
-
-    if (!_.isEqual(existingUser, existingForCompare)) {
-      _.merge(existingUser, headerUser);
-      existingUser = await models.users.save(existingUser);
+    // nedb uses user._id for ids, but user.id should also be supported
+    // However .id should always be deleted
+    if (headerUser.id && !headerUser._id) {
+      headerUser._id = headerUser.id;
     }
-    return done(null, existingUser);
-  }
+    delete headerUser.id;
 
-  // If auto sign up is turned on create user
-  if (config.get('proxyAuthAutoSignUp')) {
-    // Auto create the user
-    const newUser = await models.users.save(headerUser);
-    return done(null, newUser);
-  }
+    // If id and email are not provided we don't have anything to look existing user account up with
+    // The request is incomplete and not authorized
+    if (!headerUser._id && !headerUser.email) {
+      return done(null, false);
+    }
 
-  return done(null, false);
+    if (!headerUser.role && config.get('proxyAuthDefaultRole')) {
+      headerUser.role = config.get('proxyAuthDefaultRole');
+    }
+
+    // Try to find existing user in SQLPad's db
+    // This tries to find a match on both id and email, with id taking priority
+    let existingUser;
+    if (headerUser._id) {
+      existingUser = await models.users.findOneById(headerUser._id);
+    }
+    if (!existingUser && headerUser.email) {
+      existingUser = await models.users.findOneByEmail(headerUser.email);
+    }
+
+    // If existing user is found, see if there are changes and update it
+    // Only perform update if actual changes though
+    if (existingUser) {
+      // Get a subset of existing user to use for comparison
+      const existingForCompare = {};
+      Object.keys(headerUser).forEach(key => {
+        existingForCompare[key] = existingUser[key];
+      });
+
+      if (!_.isEqual(headerUser, existingForCompare)) {
+        _.merge(existingUser, headerUser);
+        existingUser = await models.users.save(existingUser);
+      }
+      return done(null, existingUser);
+    }
+
+    // If auto sign up is turned on create user
+    if (config.get('proxyAuthAutoSignUp')) {
+      // If role or email are not provided no auth
+      if (!headerUser.role || !headerUser.email) {
+        return done(null, false);
+      }
+
+      // Auto create the user
+      const newUser = await models.users.save(headerUser);
+      return done(null, newUser);
+    }
+
+    return done(null, false);
+  } catch (error) {
+    done(error);
+  }
 }
 
 // Register the custom auth-proxy strategy implementation with passport
