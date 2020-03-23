e57f3505464a1eb4f54ebf8a3524008805b19d34
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-03-23 11:08:18 -0500

    Add TODO

--- server/middleware/must-be-authenticated.js
+++ server/middleware/must-be-authenticated.js
@@ -3,5 +3,7 @@ module.exports = function mustBeAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
+  // TODO send 401 instead
+  // Sending redirect is old behavior, and signin may not even be available based on auth setup
   res.redirect(req.config.get('baseUrl') + '/signin');
 };
