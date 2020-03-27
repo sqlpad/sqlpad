01f6dfd45ca7ad0d1e0dfcc16b8f371b0202f9c5
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-03-27 17:17:45 -0500

    Move auth proxy strategy out of middleware

--- server/auth-strategies/register-auth-strategies.js
+++ server/auth-strategies/register-auth-strategies.js
@@ -1,3 +1,4 @@
+const authProxy = require('./auth-proxy');
 const basic = require('./basic');
 const google = require('./google');
 const jwtServiceToken = require('./jwt-service-token');
@@ -9,6 +10,7 @@ const saml = require('./saml');
  * @param {object} config
  */
 function registerAuthStrategies(config) {
+  authProxy(config);
   basic(config);
   google(config);
   jwtServiceToken(config);
