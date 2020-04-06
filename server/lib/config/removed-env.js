0793de7b6ccd06b1a22d47bfab8780da7f22d9db
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-04-06 16:53:42 -0500

    Remove debug from config items

---
+++ server/lib/config/removed-env.js
@@ -0,0 +1,3 @@
+// This tracks environment variables that used to be supported but no longer are
+// This is to assist erroring for old config no longer supported
+module.exports = ['SQLPAD_DEBUG'];
