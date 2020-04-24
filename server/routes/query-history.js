5e5703f20c475f00e59055a6e09aaad05489ae87
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-04-24 12:04:10 -0500

    Send array of objects for /api/query-history (#612)
    * Send array of objects for /api/query-history
    
    * Lower default and add comment

--- server/routes/query-history.js
+++ server/routes/query-history.js
@@ -1,50 +1,26 @@
 const router = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const getMeta = require('../lib/get-meta');
 const mustBeAuthenticated = require('../middleware/must-be-authenticated.js');
 const urlFilterToNeDbFilter = require('../lib/url-filter-to-nedb-filter');
 const wrap = require('../lib/wrap');
 
-// TODO v5 - change this to an array of items?
 router.get(
   '/api/query-history',
   mustBeAuthenticated,
   wrap(async function(req, res) {
     const { models } = req;
 
-    const queryHistory = {
-      id: uuidv4(),
-      cacheKey: null,
-      startTime: new Date(),
-      stopTime: null,
-      queryRunTime: null,
-      fields: [],
-      incomplete: false,
-      meta: {},
-      rows: []
-    };
-
     // Convert URL filter to NeDB compatible filter object
     const dbFilter = urlFilterToNeDbFilter(req.query.filter);
     const dbQueryHistory = await models.queryHistory.findByFilter(dbFilter);
 
-    dbQueryHistory.map(q => {
+    const rows = dbQueryHistory.map(q => {
       delete q._id;
       delete q.userId;
       delete q.connectionId;
       return q;
     });
 
-    queryHistory.incomplete =
-      dbQueryHistory.length >= req.config.get('queryHistoryResultMaxRows');
-    queryHistory.rows = dbQueryHistory;
-    queryHistory.stopTime = new Date();
-    queryHistory.queryRunTime =
-      dbQueryHistory.stopTime - dbQueryHistory.startTime;
-    queryHistory.meta = getMeta(dbQueryHistory);
-    queryHistory.fields = Object.keys(queryHistory.meta);
-
-    return res.utils.data(queryHistory);
+    return res.utils.data(rows);
   })
 );
 
