9f3d09030994e19548de9fb1a6a6db2a5a2459ad
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-04-25 23:21:17 -0500

    Remove console.log

--- server/app.js
+++ server/app.js
@@ -160,7 +160,6 @@ function makeApp(config, models) {
     if (res.headersSent) {
       return next(err);
     }
-    console.log(err);
     return res.status(500).json({
       title: 'Internal Server Error'
     });
