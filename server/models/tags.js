fb5d6a7ea2f33e078e603565a01dbedb8e19aad1
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-04-27 11:30:08 -0500

    Use DISTINCT query to pull tags efficiently

---
+++ server/models/tags.js
@@ -0,0 +1,23 @@
+const { QueryTypes } = require('sequelize');
+
+class Tags {
+  /**
+   * @param {import('../sequelize-db')} sequelizeDb
+   * @param {import('../lib/config')} config
+   */
+  constructor(sequelizeDb, config) {
+    this.sequelizeDb = sequelizeDb;
+    this.config = config;
+  }
+
+  async findDistinctTags() {
+    const rows = await this.sequelizeDb.sequelize.query(
+      'SELECT DISTINCT tag FROM query_tags ORDER BY tag',
+      { type: QueryTypes.SELECT }
+    );
+
+    return rows.map(row => row.tag);
+  }
+}
+
+module.exports = Tags;
