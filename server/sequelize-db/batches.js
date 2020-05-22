65035d77c1a94596e4d65d24ff884a39aae28282
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-05-22 10:20:48 -0500

    Add batches and statements model

---
+++ server/sequelize-db/batches.js
@@ -0,0 +1,74 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = function (sequelize) {
+  const Batches = sequelize.define(
+    'Batches',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      queryId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      connectionId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      connectionClientId: {
+        type: DataTypes.UUID,
+      },
+      status: {
+        type: DataTypes.STRING,
+        validate: {
+          isIn: [['running', 'finished', 'error', 'cancelled']],
+        },
+        defaultValue: 'running',
+      },
+      startTime: {
+        type: DataTypes.DATE,
+      },
+      stopTime: {
+        type: DataTypes.DATE,
+      },
+      // Both query_text and selected_query_text is captured,
+      // as user may execute just a portion of what is in their editor
+      // In the future they may want to "restore" back to this version,
+      // in which case we can restore back to everything
+      batchText: {
+        type: DataTypes.TEXT,
+      },
+      selectedText: {
+        type: DataTypes.TEXT,
+      },
+      // Taking a snapshot of the chart config too, because that could change over time
+      chart: {
+        type: DataTypes.JSON,
+      },
+      // Users create them but not update (the system does though)
+      createdBy: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'batches',
+      underscored: true,
+    }
+  );
+
+  return Batches;
+};
---
+++ server/sequelize-db/statements.js
@@ -0,0 +1,65 @@
+const Sequelize = require('sequelize');
+
+module.exports = function (sequelize) {
+  const Statements = sequelize.define(
+    'Statements',
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      batchId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      sequence: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      statementText: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [['queued', 'running', 'finished', 'error', 'cancelled']],
+        },
+        defaultValue: 'queued',
+      },
+      startTime: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      stopTime: {
+        type: Sequelize.DATE,
+      },
+      columns: {
+        type: Sequelize.JSON,
+      },
+      row_count: {
+        type: Sequelize.INTEGER,
+      },
+      error: {
+        type: Sequelize.JSON,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'statements',
+      underscored: true,
+    }
+  );
+
+  return Statements;
+};
