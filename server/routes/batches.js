4c717d66b7bb7945eb6b8b7fcebd52cbec61b7ac
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-05-22 19:25:21 -0500

    Finish initial cut of batch API

--- server/routes/batches.js
+++ server/routes/batches.js
@@ -1,46 +1,13 @@
 require('../typedefs');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
 const router = require('express').Router();
 const mustBeAuthenticated = require('../middleware/must-be-authenticated.js');
 const executeBatch = require('../lib/execute-batch');
 const wrap = require('../lib/wrap');
-
-/**
- * List batches.
- * Restricted to batches for the currently logged in user
- * @param {Req} req
- * @param {Res} res
- */
-async function list(req, res) {
-  const { models, user } = req;
-  const batches = await models.batches.findAllForUser(user);
-  return res.utils.data(batches);
-}
-
-router.get('/api/batches', mustBeAuthenticated, wrap(list));
-
-/**
- * Get batch by id.
- * Only batches created by that user are permitted.
- * Eventually this may need to expand to basing this on whether user has access to that query and/or connection
- * @param {Req} req
- * @param {Res} res
- */
-async function get(req, res) {
-  const { models, user, params } = req;
-  const batch = await models.batches.findOneById(params.id);
-
-  if (!batch) {
-    return res.utils.notFound();
-  }
-
-  if (batch.userId !== user.id) {
-    return res.utils.forbidden();
-  }
-
-  return res.utils.data(batch);
-}
-
-router.get('/api/batches/:id', mustBeAuthenticated, wrap(get));
+const papa = require('papaparse');
+const readFile = promisify(fs.readFile);
 
 /**
  * Create batch
@@ -80,5 +47,144 @@ async function create(req, res) {
 }
 
 router.post('/api/batches', mustBeAuthenticated, wrap(create));
+
+/**
+ * List batches.
+ * Restricted to batches for the currently logged in user
+ * @param {Req} req
+ * @param {Res} res
+ */
+async function list(req, res) {
+  const { models, user } = req;
+  const batches = await models.batches.findAllForUser(user);
+  return res.utils.data(batches);
+}
+
+router.get('/api/batches', mustBeAuthenticated, wrap(list));
+
+async function batchToReq(req, res, next) {
+  try {
+    const { models, user, params } = req;
+    const batch = await models.batches.findOneById(params.batchId);
+
+    if (!batch) {
+      return res.utils.notFound();
+    }
+
+    if (batch.userId !== user.id) {
+      return res.utils.forbidden();
+    }
+
+    req.batch = batch;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+}
+
+/**
+ * Get batch by id.
+ * Only batches created by that user are permitted.
+ * Eventually this may need to expand to basing this on whether user has access to that query and/or connection
+ * @param {Req} req
+ * @param {Res} res
+ */
+async function getBatch(req, res) {
+  return res.utils.data(req.batch);
+}
+
+router.get(
+  '/api/batches/:batchId',
+  mustBeAuthenticated,
+  batchToReq,
+  wrap(getBatch)
+);
+
+/**
+ * Get statements for batch.
+ * Only statements from batch created by that user are permitted.
+ * Eventually this may need to expand to basing this on whether user has access to that query and/or connection
+ *
+ * @param {Req} req
+ * @param {Res} res
+ */
+async function getBatchStatements(req, res) {
+  const { batch } = req;
+  return res.utils.data(batch.statements);
+}
+
+router.get(
+  '/api/batches/:batchId/statements',
+  mustBeAuthenticated,
+  batchToReq,
+  wrap(getBatchStatements)
+);
+
+/**
+ * Get batch statement
+ * Only statement from batch created by that user are permitted.
+ * Eventually this may need to expand to basing this on whether user has access to that query and/or connection
+ * @param {Req} req
+ * @param {Res} res
+ */
+async function getBatchStatement(req, res) {
+  const { params, batch } = req;
+
+  const statement = batch.statements.find((s) => s.id === params.statementId);
+  if (!statement) {
+    return res.utils.notFound();
+  }
+
+  return res.utils.data(statement);
+}
+
+router.get(
+  '/api/batches/:batchId/statements/:statementId',
+  mustBeAuthenticated,
+  batchToReq,
+  wrap(getBatchStatement)
+);
+
+/**
+ * Get batch statement
+ * Only statement from batch created by that user are permitted.
+ * Eventually this may need to expand to basing this on whether user has access to that query and/or connection
+ * @param {Req} req
+ * @param {Res} res
+ */
+async function getBatchStatementResults(req, res) {
+  const { config, params, batch } = req;
+
+  const statement = batch.statements.find((s) => s.id === params.statementId);
+  if (!statement) {
+    return res.utils.notFound();
+  }
+
+  const { resultPath } = statement;
+
+  // If no result path the query had no rows.
+  // Send empty array in case this is called.
+  if (!resultPath) {
+    return res.utils.data([]);
+  }
+
+  const fileData = await readFile(path.join(config.get('dbPath'), resultPath));
+  const { data, errors } = papa.parse(fileData);
+
+  // If there are errors this is unexpected and something the user cannot control
+  // Throw the first error message to return a 500
+  if (errors && errors.length) {
+    throw new Error(errors[0].message);
+  }
+
+  return res.utils.data(data);
+}
+
+router.get(
+  '/api/batches/:batchId/statements/:statementId/results',
+  mustBeAuthenticated,
+  batchToReq,
+  wrap(getBatchStatementResults)
+);
 
 module.exports = router;
