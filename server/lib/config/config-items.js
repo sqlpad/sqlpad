0ddcb582c410ccd0afe3413542d10a702eeeb90c
eladeyal <elad.eyal@intel.com>
2020-05-31 08:20:27 +0300

    remove duplicate

--- server/lib/config/config-items.js
+++ server/lib/config/config-items.js
@@ -96,11 +96,6 @@ const configItems = [
     default: '',
   },
   {
-    key: 'defaultConnectionId',
-    envVar: 'SQLPAD_DEFAULT_CONNECTION_ID',
-    default: '',
-  },
-  {
     key: 'googleClientId',
     envVar: 'GOOGLE_CLIENT_ID',
     default: '',
