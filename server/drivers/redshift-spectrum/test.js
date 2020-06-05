fc0d42e3d09be4d5a9c0b4f0ab831fba2b69d186
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-06-05 14:19:09 -0500

    Add redshift spectrum driver

---
+++ server/drivers/redshift-spectrum/test.js
@@ -0,0 +1,104 @@
+const assert = require('assert');
+const redshift = require('./index.js');
+
+const connection = {
+  name: 'test postgres',
+  driver: 'postgres',
+  host: 'localhost',
+  database: 'sqlpad',
+  username: 'sqlpad',
+  password: 'sqlpad',
+  maxRows: 100,
+};
+
+// redshift is postgres-compatible, but the schema query is different
+// A local postgres is used for testing, the schema query is skipped.
+describe('drivers/fake-redshift', function () {
+  before(async function () {
+    await redshift.runQuery('DROP TABLE IF EXISTS sqlpad_test;', connection);
+    await redshift.runQuery(
+      'CREATE TABLE sqlpad_test (id INT, name TEXT);',
+      connection
+    );
+  });
+
+  it('tests connection', function () {
+    return redshift.testConnection(connection);
+  });
+
+  it.skip('getSchema()', function () {
+    return redshift.getSchema(connection).then((schemaInfo) => {
+      const { sqlpad_test } = schemaInfo.public;
+      assert(sqlpad_test);
+      assert.strictEqual(sqlpad_test[0].table_schema, 'public');
+      assert.strictEqual(sqlpad_test[0].table_name, 'sqlpad_test');
+      assert.strictEqual(sqlpad_test[0].column_name, 'id');
+      assert.strictEqual(sqlpad_test[0].data_type, 'int4');
+      assert.strictEqual(sqlpad_test[0].column_description, null);
+    });
+  });
+
+  it('runQuery under limit', function () {
+    return redshift
+      .runQuery('SELECT * FROM generate_series(1, 10) gs;', connection)
+      .then((results) => {
+        assert(!results.incomplete, 'not incomplete');
+        assert.equal(results.rows.length, 10, 'row length');
+      });
+  });
+
+  it('runQuery over limit', function () {
+    return redshift
+      .runQuery('SELECT * FROM generate_series(1, 9000) gs;', connection)
+      .then((results) => {
+        assert(results.incomplete, 'incomplete');
+        assert.equal(results.rows.length, 100, 'row length');
+      });
+  });
+
+  it('Client cannot connect more than once', async function () {
+    const client = new redshift.Client(connection);
+    await client.connect();
+    await assert.rejects(client.connect());
+    await client.disconnect();
+  });
+
+  it('Client handles multiple disconnects', async function () {
+    const client = new redshift.Client(connection);
+    await client.connect();
+    await client.disconnect();
+    await client.disconnect();
+  });
+
+  it('Client handles multiple runQuery calls', async function () {
+    const client = new redshift.Client(connection);
+    await client.connect();
+
+    const results1 = await client.runQuery(
+      'SELECT * FROM generate_series(1, 10) g1'
+    );
+    assert.equal(results1.incomplete, false);
+    assert.equal(results1.rows.length, 10);
+    const results2 = await client.runQuery(
+      'SELECT * FROM generate_series(1, 10) g1'
+    );
+    assert.equal(results2.incomplete, false);
+    assert.equal(results2.rows.length, 10);
+
+    await client.disconnect();
+  });
+
+  it('Throws helpful error', async function () {
+    let error;
+    try {
+      await redshift.runQuery('SELECT * FROM fake_table', connection);
+    } catch (e) {
+      error = e;
+    }
+    assert(error);
+    assert(
+      error.message.includes('fake_table'),
+      'Error message has table reference'
+    );
+  });
+});
