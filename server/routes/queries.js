2b1026c8ad8b0711a0686da8eaa39cff8db60073
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-06-08 20:52:07 -0500

    Fix queries list API - parse chart JSON

--- server/routes/queries.js
+++ server/routes/queries.js
@@ -195,7 +195,7 @@ async function listQueries(req, res) {
     return {
       id: query.id,
       name: query.name,
-      chart: query.chart,
+      chart: JSON.parse(query.chart),
       queryText: query.query_text,
       createdBy: query.created_by,
       connection: {
