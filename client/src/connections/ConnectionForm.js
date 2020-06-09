0d1da6005f645a79aa45c218ffaf67a53bb07ef4
eladeyal-intel <elad.eyal@intel.com>
2020-06-09 20:56:44 +0300

    hide old error message while testing (#676)

--- client/src/connections/ConnectionForm.js
+++ client/src/connections/ConnectionForm.js
@@ -347,7 +347,7 @@ function ConnectionForm({ connectionId, onConnectionSaved }) {
 
         {renderDriverFields()}
       </div>
-      {testError && (
+      {!testing && testError && (
         <div>
           <ErrorBlock>{testError}</ErrorBlock>
         </div>
