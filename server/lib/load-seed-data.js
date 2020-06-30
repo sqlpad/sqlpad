fec54adc4de9b7faba14dce7ce2c4592e72beb8d
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-06-29 23:15:45 -0500

    Handle user_email for seed queries

--- server/lib/load-seed-data.js
+++ server/lib/load-seed-data.js
@@ -36,6 +36,31 @@ async function getItemDirectoryData(appLog, seedDataPath, dir) {
 }
 
 /**
+ * User references from seed data can be email or user id
+ * This tries to find the user referenced, and creates a disabled user if one is not found
+ * @param {import('../models')} models
+ * @param {string} emailOrId
+ */
+async function findOrCreateUserReference(models, emailOrId) {
+  let user = await models.users.findOneByEmail(emailOrId.toLowerCase());
+  if (!user) {
+    user = await models.users.findOneById(emailOrId);
+  }
+
+  // If user isn't found, we will add disabled user to reference
+  if (!user) {
+    user = await models.users.create({
+      id: emailOrId.trim(),
+      name: emailOrId.split('@')[0],
+      email: emailOrId.toLowerCase().trim(),
+      role: 'editor',
+      disabled: true,
+    });
+  }
+  return user;
+}
+
+/**
  * @param {*} appLog
  * @param {*} config
  * @param {import('../models')} models
@@ -53,7 +78,57 @@ async function loadSeedData(appLog, config, models) {
   const queries = await getItemDirectoryData(appLog, seedDataPath, 'queries');
   for (const query of queries) {
     const existing = await models.queries.findOneById(query.id);
-    const data = { ...existing, ...query };
+
+    // Find actual createdBy user.
+    // query.createdBy could be email or user id
+    let createdByUser = await findOrCreateUserReference(
+      models,
+      query.createdBy
+    );
+    let updatedByUser = createdByUser;
+    if (query.updatedBy) {
+      updatedByUser = await findOrCreateUserReference(models, query.updatedBy);
+    }
+
+    if (query.acl) {
+      for (const acl of query.acl) {
+        let fieldCount = 0;
+        if (acl.userId) {
+          fieldCount += 1;
+        }
+        if (acl.userEmail) {
+          fieldCount += 1;
+        }
+        if (acl.groupId) {
+          fieldCount += 1;
+        }
+        if (fieldCount > 1) {
+          throw new Error(
+            'Query seed import - only specify one of userId, userEmail, or groupId'
+          );
+        }
+
+        if (acl.userEmail) {
+          const aclUser = await findOrCreateUserReference(
+            models,
+            acl.userEmail
+          );
+          delete acl.userEmail;
+          acl.userId = aclUser.id;
+        }
+      }
+    }
+
+    const data = {
+      ...existing,
+      ...query,
+      createdBy: createdByUser.id,
+      updatedBy: updatedByUser.id,
+    };
+
+    // TODO - only update query if query data is different
+    // Aways updating is causing updatedAt to change, which is bringing these results to the top of UI
+    // This will require taking a subset of query data and performing a deep equal
     await models.upsertQuery(data);
   }
 
