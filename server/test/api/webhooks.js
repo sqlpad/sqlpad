4e17397b40204d7772364c147ec081bc21e9a68b
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-07-14 21:27:06 -0500

    Update webhooks.js

--- server/test/api/webhooks.js
+++ server/test/api/webhooks.js
@@ -53,6 +53,8 @@ describe('api/webhooks', function () {
     hookServer.server.close();
   });
 
+  // TODO FIXME XXX consider auto-user creation
+
   it('queryCreated', async function () {
     const hookServer = await TestUtils.makeHookServer();
     const utils = new TestUtils({
