e1486b28287cce2b3ce54cc5f00a1f1841c21013
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-09-27 17:10:00 -0500

    ts fixes for updateCompletion

--- client/src/utilities/updateCompletions.ts
+++ client/src/utilities/updateCompletions.ts
@@ -8,10 +8,46 @@ export default updateCompletions;
 // and you may find it helpful to print some vars out during dev
 const DEBUG_ON = false;
 
-function debug() {
-  // @ts-expect-error ts-migrate(2345) FIXME: Argument of type 'IArguments' is not assignable to... Remove this comment to see the full error message
-  if (DEBUG_ON) console.log.apply(null, arguments);
+function debug(...args: any) {
+  if (DEBUG_ON) console.log.apply(null, args);
 }
+
+type SchemaInfo = {
+  [schemaName: string]: {
+    [tableName: string]: [
+      {
+        column_name: string;
+        column_description: string;
+        data_type: string;
+      }
+    ];
+  };
+};
+
+type Completion = {
+  name: string;
+  value: string;
+  score: number;
+  meta: string;
+  schema?: string;
+  table?: string;
+};
+
+type MatchMap = {
+  schema: {
+    [schemaName: string]: Completion[];
+  };
+  table: {
+    [tableName: string]: Completion[];
+  };
+  schemaTable: {
+    [schemaDotTableName: string]: Completion[];
+  };
+};
+
+type DottedMatchMap = {
+  [key: string]: Array<Completion>;
+};
 
 /**
  * Updates global completions for all ace editors in use.
@@ -22,10 +58,8 @@ function debug() {
  * @todo scoped to an editor instance instead of all instances
  * @param {schemaInfoObject} schemaInfo
  */
-function updateCompletions(schemaInfo: any) {
-  // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 1.
+function updateCompletions(schemaInfo: SchemaInfo) {
   debug('updating completions');
-  // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 1.
   debug(schemaInfo);
 
   if (schemaInfo === null || schemaInfo === undefined) {
@@ -44,8 +78,8 @@ function updateCompletions(schemaInfo: any) {
   // for now we pre-assemble entire buckets of all schemas/tables/columns
   // these handle autocompletes with no dot
   // NOTE sqlpad is also firing autocomplete on every keypress instead of using live autocomplete
-  const schemaCompletions: any = [];
-  const tableCompletions: any = [];
+  const schemaCompletions: Completion[] = [];
+  const tableCompletions: Completion[] = [];
 
   // we also should create an index of dotted autocompletes.
   // given a precedingtoken as "sometable." or "someschema.table." we should be able to look up relevant completions
@@ -53,7 +87,7 @@ function updateCompletions(schemaInfo: any) {
   // SCHEMA
   // TABLE
   // SCHEMA.TABLE
-  const matchMaps = {
+  const matchMaps: MatchMap = {
     schema: {}, // will contain tables
     table: {},
     schemaTable: {},
@@ -67,17 +101,16 @@ function updateCompletions(schemaInfo: any) {
       meta: 'schema',
     });
     const SCHEMA = schema.toUpperCase();
-    // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
+
     if (!matchMaps.schema[SCHEMA]) matchMaps.schema[SCHEMA] = [];
 
     Object.keys(schemaInfo[schema]).forEach((table) => {
       const SCHEMA_TABLE = SCHEMA + '.' + table.toUpperCase();
       const TABLE = table.toUpperCase();
-      // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
+
       if (!matchMaps.table[TABLE]) matchMaps.table[TABLE] = [];
-      // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
+
       if (!matchMaps.schemaTable[SCHEMA_TABLE]) {
-        // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
         matchMaps.schemaTable[SCHEMA_TABLE] = [];
       }
       const tableCompletion = {
@@ -88,7 +121,6 @@ function updateCompletions(schemaInfo: any) {
         schema,
       };
       tableCompletions.push(tableCompletion);
-      // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
       matchMaps.schema[SCHEMA].push(tableCompletion);
 
       const columns = schemaInfo[schema][table];
@@ -101,9 +133,7 @@ function updateCompletions(schemaInfo: any) {
           schema,
           table,
         };
-        // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
         matchMaps.table[TABLE].push(columnCompletion);
-        // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
         matchMaps.schemaTable[SCHEMA_TABLE].push(columnCompletion);
       });
     });
@@ -120,59 +150,57 @@ function updateCompletions(schemaInfo: any) {
       callback: any
     ) {
       // figure out if there are any schemas/tables referenced in query
-      const allTokens = session
+      const allTokens: string[] = session
         .getValue()
         .split(/\s+/)
-        .map((t: any) => t.toUpperCase());
-      const relevantDottedMatches = {};
+        .map((t: string) => t.toUpperCase());
+      const relevantDottedMatches: DottedMatchMap = {};
       Object.keys(matchMaps.schemaTable).forEach((schemaTable) => {
         if (allTokens.indexOf(schemaTable) >= 0) {
-          // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
           relevantDottedMatches[schemaTable] =
-            // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
             matchMaps.schemaTable[schemaTable];
           // HACK - also add relevant matches for table only
-          // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
           const firstMatch = matchMaps.schemaTable[schemaTable][0];
-          const table = firstMatch.table.toUpperCase();
-          // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-          relevantDottedMatches[table] = matchMaps.table[table];
+          if (firstMatch.table) {
+            const table = firstMatch.table.toUpperCase();
+            relevantDottedMatches[table] = matchMaps.table[table];
+          }
         }
       });
       Object.keys(matchMaps.table).forEach((table) => {
         if (allTokens.indexOf(table) >= 0) {
-          // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
           relevantDottedMatches[table] = matchMaps.table[table];
           // HACK add schemaTable match for this table
           // we store schema at column match item, so look at first one and use that
-          // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
           const firstMatch = matchMaps.table[table][0];
-          const schemaTable =
-            firstMatch.schema.toUpperCase() +
-            '.' +
-            firstMatch.table.toUpperCase();
-          // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
-          relevantDottedMatches[schemaTable] = matchMaps.table[table];
+          if (firstMatch.schema && firstMatch.table) {
+            const schemaTable =
+              firstMatch.schema.toUpperCase() +
+              '.' +
+              firstMatch.table.toUpperCase();
+            relevantDottedMatches[schemaTable] = matchMaps.table[table];
+          }
         }
       });
-      // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 2.
       debug('matched found: ', Object.keys(relevantDottedMatches));
 
       // complete for schema and tables already referenced, plus their columns
-      let matches: any = [];
+      let matches: Array<Completion> = [];
+
       Object.keys(relevantDottedMatches).forEach((key) => {
-        // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
         matches = matches.concat(relevantDottedMatches[key]);
       });
-      const schemas = {};
-      const tables = {};
-      const wantedColumnCompletions: any = [];
-      // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'match' implicitly has an 'any' type.
+      const schemas: { [key: string]: string } = {};
+      const tables: { [key: string]: string } = {};
+      const wantedColumnCompletions: Array<Completion> = [];
+
       matches.forEach((match) => {
-        // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-        if (match.schema) schemas[match.schema] = match.schema;
-        // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-        if (match.table) tables[match.table] = match.schema;
+        if (match.schema) {
+          schemas[match.schema] = match.schema;
+        }
+        if (match.table && match.schema) {
+          tables[match.table] = match.schema;
+        }
       });
       Object.keys(schemas).forEach((schema) => {
         wantedColumnCompletions.push({
@@ -190,11 +218,8 @@ function updateCompletions(schemaInfo: any) {
           meta: 'table',
         };
         wantedColumnCompletions.push(tableCompletion);
-        // @ts-expect-error ts-migrate(7053) FIXME: No index signature with a parameter of type 'strin... Remove this comment to see the full error message
         const SCHEMA = tables[table].toUpperCase();
-        // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
         if (!relevantDottedMatches[SCHEMA]) relevantDottedMatches[SCHEMA] = [];
-        // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
         relevantDottedMatches[SCHEMA].push(tableCompletion);
       });
 
@@ -219,14 +244,12 @@ function updateCompletions(schemaInfo: any) {
         for (let i = lineTokens.length - 1; i >= 0; i--) {
           const token = lineTokens[i];
           if (columnWantedKeywords.indexOf(token) >= 0) {
-            // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 2.
             debug('WANT COLUMN BECAUSE FOUND: ', token);
             wanted = 'COLUMN';
             r = 0;
             break;
           }
           if (tableWantedKeywords.indexOf(token) >= 0) {
-            // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 2.
             debug('WANT TABLE BECAUSE FOUND: ', token);
             wanted = 'TABLE';
             r = 0;
@@ -234,7 +257,6 @@ function updateCompletions(schemaInfo: any) {
           }
         }
       }
-      // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 2.
       debug('WANTED: ', wanted);
 
       const currentLine = session.getDocument().getLine(pos.row);
@@ -246,30 +268,24 @@ function updateCompletions(schemaInfo: any) {
       const precedingToken = currentTokens[currentTokens.length - 1];
 
       // if preceding token has a . try to provide completions based on that object
-      // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 2.
       debug('PREFIX: "%s"', prefix);
-      // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 2.
       debug('PRECEDING CHAR: "%s"', precedingCharacter);
-      // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 2.
       debug('PRECEDING TOKEN: "%s"', precedingToken);
       if (precedingToken.indexOf('.') >= 0) {
         let dotTokens = precedingToken.split('.');
         dotTokens.pop();
         const DOT_MATCH = dotTokens.join('.').toUpperCase();
         debug(
-          // @ts-expect-error ts-migrate(2554) FIXME: Expected 0 arguments, but got 3.
           'Completing for "%s" even though we got "%s"',
           DOT_MATCH,
           precedingToken
         );
         if (wanted === 'TABLE') {
           // if we're in a table place, a completion should only be for tables, not columns
-          // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
           return callback(null, matchMaps.schema[DOT_MATCH]);
         }
         if (wanted === 'COLUMN') {
           // here we should see show matches for only the tables mentioned in query
-          // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
           return callback(null, relevantDottedMatches[DOT_MATCH]);
         }
       }
