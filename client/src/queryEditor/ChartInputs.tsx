a4e1d894328431c1a562907055fbbdbc688425dc
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-10-30 16:56:06 -0500

    Add warning

--- client/src/queryEditor/ChartInputs.tsx
+++ client/src/queryEditor/ChartInputs.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react';
+import React, { ChangeEvent, CSSProperties } from 'react';
 import Input from '../common/Input';
 import Select from '../common/Select';
 import { handleChartConfigurationFieldsChange } from '../stores/editor-actions';
@@ -10,15 +10,21 @@ import {
 } from '../stores/editor-store';
 import chartDefinitions from '../utilities/chartDefinitions';
 
-function cleanBoolean(value: any) {
+function cleanBoolean(value: string | boolean) {
+  if (typeof value === 'boolean') {
+    return value;
+  }
   if (typeof value === 'string') {
     if (value.toLowerCase() === 'true') {
-      value = true;
+      return true;
     } else if (value.toLowerCase() === 'false') {
-      value = false;
+      return false;
     }
   }
-  return value;
+
+  // This shouldn't happen, but if it does return false
+  console.warn('Unexpected value ', value);
+  return false;
 }
 
 const inputStyle: CSSProperties = {
@@ -39,89 +45,10 @@ function ChartInputs() {
     handleChartConfigurationFieldsChange(chartFieldId, queryResultField);
   };
 
-  const renderFormGroup = (inputDefinitionFields: any) => {
-    let resultColumnNames: string[] = [];
-    if (columns) {
-      resultColumnNames = columns.map((c) => c.name);
-    }
-
-    return inputDefinitionFields.map((field: any) => {
-      if (field.inputType === 'field-dropdown') {
-        const optionNodes = resultColumnNames.map((qrfield) => {
-          return (
-            <option key={qrfield} value={qrfield}>
-              {qrfield}
-            </option>
-          );
-        });
-        const selectedQueryResultField =
-          queryChartConfigurationFields[field.fieldId];
-        if (
-          selectedQueryResultField &&
-          resultColumnNames.indexOf(selectedQueryResultField) === -1
-        ) {
-          optionNodes.push(
-            <option
-              key={'selectedQueryResultField'}
-              value={selectedQueryResultField}
-            >
-              {selectedQueryResultField}
-            </option>
-          );
-        }
-        return (
-          <div style={inputStyle} key={field.fieldId}>
-            <label>{field.label}</label>
-            <Select
-              className="w-100"
-              value={selectedQueryResultField}
-              onChange={(event: any) =>
-                changeChartConfigurationField(field.fieldId, event.target.value)
-              }
-            >
-              <option value="" />
-              {optionNodes}
-            </Select>
-          </div>
-        );
-      } else if (field.inputType === 'checkbox') {
-        const checked =
-          cleanBoolean(queryChartConfigurationFields[field.fieldId]) || false;
-        return (
-          <div style={inputStyle} key={field.fieldId}>
-            <input
-              type="checkbox"
-              checked={checked}
-              id={field.fieldId}
-              name={field.fieldId}
-              onChange={(e) =>
-                changeChartConfigurationField(field.fieldId, e.target.checked)
-              }
-            />
-            <label htmlFor={field.fieldId} style={{ marginLeft: 8 }}>
-              {field.label}
-            </label>
-          </div>
-        );
-      } else if (field.inputType === 'textbox') {
-        const value = queryChartConfigurationFields[field.fieldId] || '';
-        return (
-          <div style={inputStyle} key={field.fieldId}>
-            <label>{field.label}</label>
-            <Input
-              value={value}
-              onChange={(e: any) =>
-                changeChartConfigurationField(field.fieldId, e.target.value)
-              }
-              className="w-100"
-            />
-          </div>
-        );
-      } else {
-        throw Error(`field.inputType ${field.inputType} not supported`);
-      }
-    });
-  };
+  let resultColumnNames: string[] = [];
+  if (columns) {
+    resultColumnNames = columns.map((c) => c.name);
+  }
 
   const chartDefinition = chartDefinitions.find(
     (def) => def.chartType === chartType
@@ -130,6 +57,83 @@ function ChartInputs() {
   if (!chartDefinition || !chartDefinition.fields) {
     return null;
   }
+
+  const content = chartDefinition.fields.map((field) => {
+    if (field.inputType === 'field-dropdown') {
+      const optionNodes = resultColumnNames.map((qrfield) => {
+        return (
+          <option key={qrfield} value={qrfield}>
+            {qrfield}
+          </option>
+        );
+      });
+      const selectedQueryResultField =
+        queryChartConfigurationFields[field.fieldId];
+      if (
+        selectedQueryResultField &&
+        resultColumnNames.indexOf(selectedQueryResultField) === -1
+      ) {
+        optionNodes.push(
+          <option
+            key={'selectedQueryResultField'}
+            value={selectedQueryResultField}
+          >
+            {selectedQueryResultField}
+          </option>
+        );
+      }
+      return (
+        <div style={inputStyle} key={field.fieldId}>
+          <label>{field.label}</label>
+          <Select
+            className="w-100"
+            value={selectedQueryResultField}
+            onChange={(event: ChangeEvent<HTMLSelectElement>) =>
+              changeChartConfigurationField(field.fieldId, event.target.value)
+            }
+          >
+            <option value="" />
+            {optionNodes}
+          </Select>
+        </div>
+      );
+    } else if (field.inputType === 'checkbox') {
+      const checked =
+        cleanBoolean(queryChartConfigurationFields[field.fieldId]) || false;
+      return (
+        <div style={inputStyle} key={field.fieldId}>
+          <input
+            type="checkbox"
+            checked={checked}
+            id={field.fieldId}
+            name={field.fieldId}
+            onChange={(e) =>
+              changeChartConfigurationField(field.fieldId, e.target.checked)
+            }
+          />
+          <label htmlFor={field.fieldId} style={{ marginLeft: 8 }}>
+            {field.label}
+          </label>
+        </div>
+      );
+    } else if (field.inputType === 'textbox') {
+      const value = queryChartConfigurationFields[field.fieldId] || '';
+      return (
+        <div style={inputStyle} key={field.fieldId}>
+          <label>{field.label}</label>
+          <Input
+            value={value}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              changeChartConfigurationField(field.fieldId, e.target.value)
+            }
+            className="w-100"
+          />
+        </div>
+      );
+    } else {
+      throw Error(`field.inputType ${field.inputType} not supported`);
+    }
+  });
 
   return (
     <div
@@ -143,14 +147,9 @@ function ChartInputs() {
         justifyContent: 'space-between',
       }}
     >
-      {renderFormGroup(chartDefinition.fields)}
+      {content}
     </div>
   );
 }
-
-ChartInputs.defaultProps = {
-  queryChartConfigurationFields: {},
-  queryResult: {},
-};
 
 export default ChartInputs;
