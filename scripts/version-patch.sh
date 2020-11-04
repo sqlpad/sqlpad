0137b2edd25fea080c9cce660d142d8944ba5bc7
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-11-04 12:08:38 -0600

    Require branch be up-to-date before release

--- scripts/version-patch.sh
+++ scripts/version-patch.sh
@@ -3,6 +3,20 @@
 # This script can be used for pushing patches not based on master branch
 # In most cases use scripts/version.sh to do builds, not this one
 
+# Determine if branch is up-to-date
+# If not exit the script
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Branch up-to-date"
+else
+    echo "Branch out of date"
+    exit 1;
+fi
+
+# Get relevant directories and ensure script is executed from root of directory
 SQLPAD_ROOT_DIR=$(pwd)
 SQLPAD_CLIENT_DIR=$(pwd)/client
 SQLPAD_SERVER_DIR=$(pwd)/server
@@ -19,17 +33,17 @@ fi
 VERSION=$1
 
 cd $SQLPAD_CLIENT_DIR
-npm --no-git-tag-version version $VERSION
+npm --no-git-tag-version version $VERSION || exit 1
 
 cd $SQLPAD_SERVER_DIR
-npm --no-git-tag-version version $VERSION
+npm --no-git-tag-version version $VERSION || exit 1
 
 cd $SQLPAD_ROOT_DIR
-npm --no-git-tag-version version $VERSION
+npm --no-git-tag-version version $VERSION || exit 1
 
-git commit -a -m "v$VERSION"
+git commit -a -m "v$VERSION" || exit 1
 
-git tag -a "v$VERSION" -m "v$VERSION"
+git tag -a "v$VERSION" -m "v$VERSION" || exit 1
 
 git push origin HEAD
 git push origin "v$VERSION"
--- scripts/version.sh
+++ scripts/version.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/env sh
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [[ "$BRANCH" != "master" ]]; then
+if [ $BRANCH != "master" ]; then
   echo 'Branch must be set to master';
   exit 1;
 fi
 
+# Determine if branch is up-to-date
+# If not exit the script
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Branch up-to-date"
+else
+    echo "Branch out of date"
+    exit 1;
+fi
+
+# Get relevant directories and ensure script is executed from root of directory
 SQLPAD_ROOT_DIR=$(pwd)
 SQLPAD_CLIENT_DIR=$(pwd)/client
 SQLPAD_SERVER_DIR=$(pwd)/server
 SCRIPTS_DIR=$(pwd)/scripts
 
-if [[ ! -d $SCRIPTS_DIR ]] || \
-   [[ ! -d $SQLPAD_CLIENT_DIR ]] || \
-   [[ ! -d $SQLPAD_SERVER_DIR ]]
+if [ ! -d $SCRIPTS_DIR ] || \
+   [ ! -d $SQLPAD_CLIENT_DIR ] || \
+   [ ! -d $SQLPAD_SERVER_DIR ]
 then
     echo This script must be executed from the sqlpad project directory
     exit 1
@@ -22,17 +36,17 @@ fi
 VERSION=$1
 
 cd $SQLPAD_CLIENT_DIR
-npm --no-git-tag-version version $VERSION
+npm --no-git-tag-version version $VERSION || exit 1
 
 cd $SQLPAD_SERVER_DIR
-npm --no-git-tag-version version $VERSION
+npm --no-git-tag-version version $VERSION || exit 1
 
 cd $SQLPAD_ROOT_DIR
-npm --no-git-tag-version version $VERSION
+npm --no-git-tag-version version $VERSION || exit 1
 
-git commit -a -m "v$VERSION"
+git commit -a -m "v$VERSION" || exit 1
 
-git tag -a "v$VERSION" -m "v$VERSION"
+git tag -a "v$VERSION" -m "v$VERSION" || exit 1
 
 git push origin master
 git push origin "v$VERSION"
