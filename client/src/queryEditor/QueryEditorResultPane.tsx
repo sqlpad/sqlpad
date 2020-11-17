e7174c5352a0d670be797d22dd6db35e4d396a88
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-11-16 20:37:23 -0600

    remove running indicator jitter

--- client/src/queryEditor/QueryEditorResultPane.tsx
+++ client/src/queryEditor/QueryEditorResultPane.tsx
@@ -5,7 +5,9 @@ import {
   useSessionBatch,
   useSessionIsRunning,
   useSessionSelectedStatementId,
+  useStatementStatus,
 } from '../stores/editor-store';
+import { api } from '../utilities/api';
 import QueryResultBatchHeader from './QueryResultBatchHeader';
 import QueryResultStatementHeader from './QueryResultStatementHeader';
 import StatementsTable from './StatementsTable';
@@ -13,22 +15,30 @@ import StatementsTable from './StatementsTable';
 function QueryEditorResultPane() {
   const selectedStatementId = useSessionSelectedStatementId();
   const isRunning = useSessionIsRunning();
+  const status = useStatementStatus(selectedStatementId);
 
   // Batch reference will change frequently during load
   // This component should be cheap to render though so this should be okay
   const batch = useSessionBatch();
-  const statementId = selectedStatementId;
+
+  // This API call is also made in QueryResultContainer
+  // Calls will be shared. This is here to access loading status
+  const { data } = api.useStatementResults(selectedStatementId, status);
 
   let paneContent = null;
-  if (statementId) {
+  // In order to prevent jitter on loading/runnig indicator all loading state needs to be figured at this level
+  // If running for a single statement, or multiple with a statement selected, show running animation until query results are loaded.
+  // If isRunning and selected statement, user ran a single query or ran multiple but selected a particular query to view
+  // If a statement is selected, and it is finished but no data yet, we're still loading the data
+  if (
+    (isRunning && selectedStatementId) ||
+    (selectedStatementId && status === 'finished' && !data)
+  ) {
+    paneContent = <QueryResultRunning />;
+  } else if (selectedStatementId) {
     paneContent = <QueryResultContainer statementId={selectedStatementId} />;
   } else if (batch?.statements) {
     paneContent = <StatementsTable statements={batch?.statements || []} />;
-  } else if (isRunning) {
-    // If a statementId isn't set yet, and there aren't statements,
-    // but a query is running, that means the initial batch creation hasn't happened yet
-    // For this case show a spinner, so there isn't an awkward delay
-    paneContent = <QueryResultRunning />;
   }
 
   return (
