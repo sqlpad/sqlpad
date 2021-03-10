d3054c77c36da0a15eab20b4f84ce2bce4e3891c
Rick Bergfalk <rick.bergfalk@gmail.com>
2021-03-10 00:04:36 -0600

    Add passport workarounds

--- server/routes/auth-oidc.js
+++ server/routes/auth-oidc.js
@@ -6,13 +6,51 @@ const router = require('express').Router();
  * @param {Req} req
  * @param {Res} res
  * @param {Function} next
+ *
+ * Work arounds in here because node-openid-client strategy
+ * doesn't do a workaround some strategies do.
+ *
+ * These workarounds should be fine for both openid-client and passport-openidconnect
+ * https://github.com/panva/node-openid-client/issues/146
+ * https://github.com/jaredhanson/passport/pull/680
  */
 function handleOidcCallback(req, res, next) {
   const baseUrl = req.config.get('baseUrl');
-  passport.authenticate('oidc', {
-    successRedirect: baseUrl + '/',
-    failureRedirect: baseUrl + '/signin',
-  })(req, res, next);
+
+  function redirectUser(err, user) {
+    if (err) {
+      res.redirect(`${baseUrl}/signin`);
+    }
+    if (!user) {
+      res.redirect(`${baseUrl}/signin`);
+    }
+    return req.logIn(user, (err) => {
+      if (err) {
+        res.redirect(`${baseUrl}/signin`);
+      }
+      res.redirect(`${baseUrl}`);
+    });
+  }
+
+  /**
+   * A custom passport authenticate handler to save session before redirect
+   * @param {*} err
+   * @param {*} user
+   * @param {*} info
+   */
+  function handleAuth(err, user) {
+    if (err) {
+      return redirectUser(err, user);
+    }
+    if (req.session && typeof req.session.save === 'function') {
+      return req.session.save((err) => {
+        redirectUser(err, user);
+      });
+    }
+    return redirectUser(err, user);
+  }
+
+  passport.authenticate('oidc', handleAuth)(req, res, next);
 }
 
 router.get('/auth/oidc', passport.authenticate('oidc'));
