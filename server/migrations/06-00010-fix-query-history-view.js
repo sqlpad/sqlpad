547a51667cb66a005b2554162bf2e378ca61308d
Kunal Nagpal <kunagpal@gmail.com>
2021-09-09 10:51:42 +0530

    Fixed query history view for MySQL 5.7 compatibility
    Moved from inline view creation to left join

--- server/migrations/06-00010-fix-query-history-view.js
+++ server/migrations/06-00010-fix-query-history-view.js
@@ -29,50 +29,46 @@ async function up(queryInterface, config, appLog, sequelizeDb) {
     castType = 'UNSIGNED';
   }
 
-  [
-    `
-      CREATE VIEW statement_summary AS (
-        SELECT
-          batch_id,
-          SUM(row_count) AS row_count,
-          MAX(CAST(incomplete AS ${castType})) AS incomplete
-        FROM
-          statements
-        GROUP BY
-          batch_id
-      )
-    ;`,
+  await sequelizeDb.query(
     `
       CREATE VIEW vw_query_history AS
-        SELECT
-          b.id,
-          b.query_id,
-          b.name AS query_name,
-          b.connection_id,
-          c.name AS connection_name,
-          b.status,
-          b.start_time,
-          b.stop_time,
-          b.duration_ms,
-          CASE 
-            WHEN b.selected_text = '' OR b.selected_text IS NULL THEN b.batch_text 
-            ELSE b.selected_text 
-            END AS query_text,
-          b.user_id,
-          u.email AS user_email,
-          ss.row_count,
-          ss.incomplete
-        FROM 
-          batches b
-          LEFT JOIN users u ON b.user_id = u.id
-          LEFT JOIN connections c ON b.connection_id = c.id
-          LEFT JOIN statement_summary ss ON b.id = ss.batch_id
-    ;`,
-  ].forEach(async (query) => {
-    await sequelizeDb.query(query, {
+      SELECT
+        b.id,
+        b.query_id,
+        b.name AS query_name,
+        b.connection_id,
+        c.name AS connection_name,
+        b.status,
+        b.start_time,
+        b.stop_time,
+        b.duration_ms,
+        CASE
+          WHEN b.selected_text = '' OR b.selected_text IS NULL THEN b.batch_text
+          ELSE b.selected_text
+          END AS query_text,
+        b.user_id,
+        u.email AS user_email,
+        ss.row_count,
+        ss.incomplete
+      FROM
+        batches b
+        LEFT JOIN users u ON b.user_id = u.id
+        LEFT JOIN connections c ON b.connection_id = c.id
+        LEFT JOIN (
+          SELECT
+            batch_id,
+            SUM(row_count) AS row_count,
+            MAX(CAST(incomplete AS ${castType})) AS incomplete
+          FROM
+            statements
+          GROUP BY
+            batch_id
+        ) ss ON b.id = ss.batch_id
+    `,
+    {
       type: Sequelize.QueryTypes.RAW,
-    });
-  });
+    }
+  );
 }
 
 module.exports = {
