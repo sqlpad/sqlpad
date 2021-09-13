37e78aa7e546724f8ef29e0f5843e07d3dbf403a
Kunal Nagpal <kunagpal@users.noreply.github.com>
2021-09-13 23:45:52 +0530

    Made migrations MySQL 5.7 safe (#1064)
    * Made migrations MySQL safe
    
    * Adjusted query history batching for MySQL 5.7 compatibility
    
    Moved from inline view creation to left join
    
    * Fixed query history view for MySQL 5.7 compatibility
    
    Moved from inline view creation to left join

--- server/migrations/04-00400-query-history-to-batch.js
+++ server/migrations/04-00400-query-history-to-batch.js
@@ -20,37 +20,36 @@ async function up(queryInterface, config, appLog, sequelizeDb) {
 
   await sequelizeDb.query(
     `
-      CREATE VIEW vw_query_history AS 
-        WITH statement_summary AS (
-          SELECT 
-            batch_id, 
-            SUM(row_count) AS row_count, 
+      CREATE VIEW vw_query_history AS
+      SELECT
+        b.id,
+        b.query_id,
+        b.name AS query_name,
+        b.connection_id,
+        c.name AS connection_name,
+        b.status,
+        b.start_time,
+        b.stop_time,
+        b.duration_ms,
+        b.selected_text AS query_text,
+        b.user_id,
+        u.email AS user_email,
+        ss.row_count,
+        ss.incomplete
+      FROM
+        batches b
+        LEFT JOIN users u ON b.user_id = u.id
+        LEFT JOIN connections c ON b.connection_id = c.id
+        LEFT JOIN (
+          SELECT
+            batch_id,
+            SUM(row_count) AS row_count,
             MAX(CAST(incomplete AS ${castType})) AS incomplete
-          FROM 
+          FROM
             statements
-          GROUP BY 
+          GROUP BY
             batch_id
-        )
-        SELECT 
-          b.id,
-          b.query_id,
-          b.name AS query_name,
-          b.connection_id,
-          c.name AS connection_name,
-          b.status,
-          b.start_time,
-          b.stop_time,
-          b.duration_ms,
-          b.selected_text AS query_text,
-          b.user_id,
-          u.email AS user_email,
-          ss.row_count,
-          ss.incomplete
-        FROM 
-          batches b
-          LEFT JOIN users u ON b.user_id = u.id
-          LEFT JOIN connections c ON b.connection_id = c.id
-          LEFT JOIN statement_summary ss ON b.id = ss.batch_id
+        ) ss ON b.id = ss.batch_id
     `,
     {
       type: Sequelize.QueryTypes.RAW,
