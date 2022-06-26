7d98a931ba752f5f87d89c876646b22d3b868950
Aaron Asch <aasch@athenahealth.com>
2022-06-26 06:01:30 +0000

    Support base URL overrides
    For when UI and API are hosted at different URL paths.

--- client/src/utilities/baseUrl.ts
+++ client/src/utilities/baseUrl.ts
@@ -1,8 +1,23 @@
 let _baseUrl = '';
 
+/**
+ * Sets and/or gets the base URL of the frontend static content
+ */
 export default function baseUrl(value?: string) {
   if (typeof value === 'string') {
     _baseUrl = value;
   }
   return _baseUrl;
+}
+
+let _apiBaseUrl = '';
+
+/**
+ * Sets and/or gets the base URL of the backend API
+ */
+export function apiBaseUrl(value?: string) {
+  if (typeof value === 'string') {
+    _apiBaseUrl = value;
+  }
+  return _apiBaseUrl;
 }
