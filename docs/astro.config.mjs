040ec9dde0e8263f0c2786b5172dffa74bb779a3
Rick Bergfalk <rick.bergfalk@gmail.com>
2022-08-12 23:54:31 -0500

    Dependency updates (#1153)
    * Update dependencies (minor and patch)
    
    * Update client and server deps
    
    * Update docs dependencies
    
    * Fix site for astro 1.0.0

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://getsqlpad.com',
 	integrations: [
 		// Enable Preact to support Preact JSX components.
 		preact(),
