46a84e11cd6d2e3a6792fc224b2d14cd3404d852
Rick Bergfalk <rick.bergfalk@gmail.com>
2023-03-18 00:42:09 -0500

    Remove request dep (#1183)
    * Update drill docker-compose version
    
    * Remove unused method
    
    * Remove request from package.json

--- server/drivers/drill/drill.js
+++ server/drivers/drill/drill.js
@@ -1,6 +1,4 @@
 const fetch = require('node-fetch');
-let request = require('request');
-let url = require('url');
 const appLog = require('../../lib/app-log');
 
 exports.version = '1.0';
@@ -17,31 +15,6 @@ let Client = (exports.Client = function (args) {
     this.protocol = 'https';
   }
 });
-
-Client.prototype.execute = function (queryString, callback) {
-  const href = url.format({
-    protocol: this.protocol,
-    hostname: 'localhost',
-    pathname: '/query.json',
-    port: 8047,
-  });
-  let headers = {
-    'Content-Type': 'application/json; charset=UTF-8',
-    'User-Name': this.user,
-    Accept: 'application/json',
-  };
-  let queryOptions = {
-    uri: href,
-    method: 'POST',
-    headers,
-    json: { queryType: 'SQL', query: queryString },
-  };
-  request(queryOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      callback(null, body);
-    } // TODO Add error handling
-  });
-};
 
 Client.prototype.getSchemata = function () {
   return this.query('SHOW DATABASES');
