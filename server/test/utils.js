77d518cf7758071c4b96f594581839dad8861e24
Rick Bergfalk <rick.bergfalk@gmail.com>
2023-06-18 14:47:18 -0500

    update rimraf

--- server/test/utils.js
+++ server/test/utils.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const { v4: uuidv4 } = require('uuid');
-const rimraf = require('rimraf');
+const { rimraf } = require('rimraf');
 const { mkdirp } = require('mkdirp');
 const path = require('path');
 const redis = require('redis');
@@ -20,8 +20,8 @@ const ensureConnectionAccess = require('../lib/ensure-connection-access');
 const ensureAdmin = require('../lib/ensure-admin');
 
 // At the start of any test run, clean out the root artifacts directory
-before(function (done) {
-  rimraf(path.join(__dirname, '/artifacts/*'), done);
+before(function () {
+  return rimraf(path.join(__dirname, '/artifacts/*'), { glob: true });
 });
 
 class TestUtils {
@@ -129,14 +129,7 @@ class TestUtils {
   prepDbDir() {
     const dbPath = this.config.get('dbPath');
     mkdirp.sync(dbPath);
-    return new Promise((resolve, reject) => {
-      return rimraf(path.join(dbPath, '*'), (err) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
-    });
+    return rimraf(path.join(dbPath, '*'), { glob: true });
   }
 
   async initDbs() {
